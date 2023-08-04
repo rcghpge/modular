8abb345aa4a19f32ed22619dc5047d86fb21eae8
Aditya Bhagwat <adityab@modular.com>
2023-08-04 09:09:25 -0700

    [API] Remove APIScenarioTest (#18647)
    These tests are now obsolete with the introduction of [p0 model testing
    [Internal link] as part of C API
    release qualification. Similar tests for Python wheel are soon to follow
    [Internal link]
    
    For scenarios other than testing on big p0 models e.g. stress testing
    mutli-threading and testing for memory leaks etc. we can and should
    write smaller unit tests.
    
    MAX_INTEGRATION_TESTS_REV_ID: 7d9e3a2ebf1c627ca231ba80cdbb6db686309384

--- max/tests/integration/API/python/pytests/conftest.py
+++
@@ -1,8 +0,0 @@
-# ===----------------------------------------------------------------------=== #
-#
-# This file is Modular Inc proprietary.
-#
-# ===----------------------------------------------------------------------=== #
-"""All fixtures for end-to-end testing"""
-
-pytest_plugins = ["model_fixtures"]
--- max/tests/integration/API/python/pytests/model_fixtures/__init__.py
+++
@@ -1,8 +0,0 @@
-# ===----------------------------------------------------------------------=== #
-#
-# This file is Modular Inc proprietary.
-#
-# ===----------------------------------------------------------------------=== #
-from .onnx import *
-from .tf import *
-from .utils import session_ctx
