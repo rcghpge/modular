d4794fb675bfa5174080a1e64e2112e5bd5b30ad
Aditya Bhagwat <adityab@modular.com>
2023-08-31 13:30:10 -0700

    [Packaging] Test p0 models in Python wheel release qualification  (#18250)
    Test p0 models on Python wheel as part of the wheel's release
    qualification process.
    
    MAX_INTEGRATION_TESTS_REV_ID: 6a382e36fe69d3513edc7078b9ae2ac169bf1809

--- max/tests/integration/API/python/test_p0_models.py
+++ max/tests/integration/API/python/test_p0_models.py
@@ -9,9 +9,9 @@
 
 import argparse
 import os
+import platform
 import shutil
 import subprocess
-import platform
 import sys
 import tarfile
 import tempfile
@@ -44,6 +44,13 @@ top_models = {
         "input_shapes": ["16384x8x1", "16384x256"],
         "output_names": ["output_0"],
     },
+    "dlrm-rm2": {
+        "s3_uri": "s3://modular-model-storage/dlrm/dlrm_rm2.tar.gz",
+        "saved_model_dir_name": "dlrm-rm2",
+        "input_names": ["ls_i", "xt"],
+        "input_shapes": ["2048x40x1", "2048x256"],
+        "output_names": ["output_0"],
+    },
     "dlrm-rm3": {
         "s3_uri": "s3://modular-model-storage/dlrm/dlrm-facebook-3.tar.gz",
         "saved_model_dir_name": "dlrm-rm3",
@@ -67,13 +74,6 @@ top_models = {
             "vision_model_output_pooler_output",
         ],
     },
-    "dlrm-rm2": {
-        "s3_uri": "s3://modular-model-storage/dlrm/dlrm_rm2.tar.gz",
-        "saved_model_dir_name": "dlrm-rm2",
-        "input_names": ["ls_i", "xt"],
-        "input_shapes": ["2048x40x1", "2048x256"],
-        "output_names": ["output_0"],
-    },
 }
 
 
@@ -91,10 +91,19 @@ def download_s3_object(s3_src: str, local_dst: Path):
         tar.extractall(local_dst)
 
 
-def setup_environment(package_library_path: Path, package_binary_path: Path):
+def remove_modular_env_variables():
     for key in os.environ:
         if "MODULAR" in key:
             del os.environ[key]
+
+    # TODO(https://github.com/modularml/modular/issues/17843)
+    assert "MODULAR_DERIVED_PATH" not in os.environ
+
+
+def setup_environment_for_c(
+    package_library_path: Path, package_binary_path: Path
+):
+    remove_modular_env_variables()
 
     if platform.system() == "Windows":
         os.environ["Path"] = f"{os.environ['Path']};{package_binary_path}"
@@ -103,16 +112,13 @@ def setup_environment(package_library_path: Path, package_binary_path: Path):
 
     os.environ["MODULAR_FRAMEWORK_ROOT"] = str(package_library_path)
 
-    # TODO(https://github.com/modularml/modular/issues/17843)
-    assert "MODULAR_DERIVED_PATH" not in os.environ
 
-
-def main(package_path: Path):
+def test_c_package(package_path: Path):
     package_library_path = package_path / "lib"
     package_binary_path = package_path / "bin"
     modular_api_executor_binary = package_binary_path / "modular-api-executor"
 
-    setup_environment(package_library_path, package_binary_path)
+    setup_environment_for_c(package_library_path, package_binary_path)
 
     with tempfile.TemporaryDirectory() as test_dir:
         test_dir_path = Path(test_dir)
@@ -161,12 +167,118 @@ def main(package_path: Path):
                 sys.exit(1)
 
 
+def _get_np_dtype(dtype):
+    import numpy as np
+
+    from modular import engine
+
+    mtype = engine.DType
+    if dtype == mtype.bool:
+        return np.bool_
+    elif dtype == mtype.int8:
+        return np.int8
+    elif dtype == mtype.int16:
+        return np.int16
+    elif dtype == mtype.int32:
+        return np.int32
+    elif dtype == mtype.int64:
+        return np.int64
+    elif dtype == mtype.uint8:
+        return np.uint8
+    elif dtype == mtype.uint16:
+        return np.uint16
+    elif dtype == mtype.uint32:
+        return np.uint32
+    elif dtype == mtype.uint64:
+        return np.uint64
+    elif dtype == mtype.float16:
+        return np.float16
+    elif dtype == mtype.float32:
+        return np.float32
+    elif dtype == mtype.float64:
+        return np.float64
+
+
+def generate_test_inputs(model):
+    import numpy as np
+
+    _batch_size = 1
+    input_specs = model.input_metadata
+    inputs = {}
+    for spec in input_specs:
+        input_shape_dyn = spec.shape
+        input_shape = [
+            _batch_size if not dim else dim for dim in input_shape_dyn
+        ]
+        input_type = spec.dtype
+        random_input = np.random.rand(*input_shape).astype(
+            _get_np_dtype(input_type)
+        )
+        inputs[spec.name] = random_input
+    return inputs
+
+
+def generate_clipvit_inputs_python():
+    import numpy as np
+
+    return {
+        "pixel_values": np.random.rand(1, 3, 224, 224).astype(np.float32),
+        "input_ids": np.random.rand(3, 12).astype(np.int32),
+        "attention_mask": np.random.rand(3, 12).astype(np.int32),
+    }
+
+
+def load_and_execute_on_random_input(model_path: Path):
+    from modular import engine
+
+    session = engine.InferenceSession()
+    print("\t Loading ...", end="")
+    model = session.load(model_path)
+    print("✅")
+    assert model is not None, "Loading failed ❌"
+    if "clip_vit" in str(model_path):
+        inputs = generate_clipvit_inputs_python()
+    else:
+        inputs = generate_test_inputs(model)
+    print("\t Executing ...", end="")
+    output = model.execute(**inputs)
+    print("✅")
+    assert output is not None, "Execution failed ❌"
+
+
+def test_python_wheel():
+    """Test the wheel which is assumed to be pip installed to the python
+    environment this script is executed in"""
+    remove_modular_env_variables()
+    from modular import engine
+
+    test_dir = tempfile.TemporaryDirectory()
+    test_dir_path = Path(test_dir.name)
+
+    session = engine.InferenceSession()
+    for key, value in top_models.items():
+        print("Model", key)
+        s3_uri = value["s3_uri"]
+        print("Downloading model from", s3_uri)
+        download_s3_object(s3_uri, test_dir_path)
+        model_path = test_dir_path / value["saved_model_dir_name"]
+        print("Loading and executing downloaded model ...")
+        load_and_execute_on_random_input(model_path)
+        print("Model executed successfully ✅")
+    test_dir.cleanup()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--package_path",
         help="Path to a local modular inference engine C package.",
-        required=True,
+        required=False,
     )
     args = parser.parse_args()
-    main(Path(args.package_path))
+    if args.package_path:
+        print("Testing p0 models on C package")
+        test_c_package(Path(args.package_path))
+    else:
+        print("Testing p0 models on Python wheel")
+        test_python_wheel()
