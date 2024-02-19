31cb25816a703a85214258c11eae4f7c46008367
Brendan Duke <bduke@modular.com>
2024-02-19 12:43:48 -0500

    [Packaging] Re-enable PT test on aarch64/20.04 (#32316)
    Re-enable PyTorch tests in test_p0_models.py.
    After #32315 this should now be working.
    
    MAX_INTEGRATION_TESTS_REV_ID: b1b646df00bf191eceaea6942fd29b8788e878c6

--- max/tests/integration/API/python/test_p0_models.py
+++ max/tests/integration/API/python/test_p0_models.py
@@ -332,19 +332,7 @@ def test_dir():
 
 @pytest.fixture(params=top_models.keys(), scope="module")
 def model_name(request):
-    model_name = request.param
-    model_meta = top_models[request.param]
-
-    if (
-        model_meta.framework == PYTORCH
-        and TEST_ENV_OS == "main"  # main is 20.04
-        and TEST_ENV_ARCH == "graviton"
-    ):
-        pytest.skip(
-            "PyTorch via python API not supported on aarch64 + Ubuntu 20.04"
-        )
-
-    return model_name
+    return request.param
 
 
 @pytest.fixture(scope="module")
