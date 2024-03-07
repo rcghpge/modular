5b0655c1381f018793fd136954f1e34b2ca84057
Zheng Hao Tan <zheng@modular.com>
2024-03-07 10:18:22 -0800

    [Python API] [MAX] Add initial version of repr for TorchInputSpec (#34014)
    This PR introduces `__repr__` and `__str__` for `TorchInputSpec` in
    Python.
    
    MAX_INTEGRATION_TESTS_REV_ID: 021dbfa7e4d46bee8bdc266cac1f93aa4eb925fe

--- max/tests/integration/API/python/pytests/test_engine.py
+++ max/tests/integration/API/python/pytests/test_engine.py
@@ -125,3 +125,25 @@ def test_dynamic_rank_spec():
         == "TensorSpec(shape=None, dtype=DType.float64, name=dynamic)"
     )
     assert str(input_spec) == "None x float64"
+
+
+def test_repr_torch_input_spec():
+    input_spec_with_shape = me.TorchInputSpec([20, 30], me.DType.float32)
+    assert input_spec_with_shape.shape == [20, 30]
+    assert input_spec_with_shape.dtype == me.DType.float32
+
+    assert (
+        repr(input_spec_with_shape)
+        == "TorchInputSpec(shape=[20, 30], dtype=DType.float32)"
+    )
+    assert str(input_spec_with_shape) == "20x30xfloat32"
+
+    input_spec_without_shape = me.TorchInputSpec(None, me.DType.float64)
+    assert input_spec_without_shape.shape == None
+    assert input_spec_without_shape.dtype == me.DType.float64
+
+    assert (
+        repr(input_spec_without_shape)
+        == "TorchInputSpec(shape=None, dtype=DType.float64)"
+    )
+    assert str(input_spec_without_shape) == "None x float64"
