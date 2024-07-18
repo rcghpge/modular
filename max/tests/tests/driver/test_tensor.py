669b80b85510d252ad69e386bece489574277563
Jakub Tucholski <jakub@modular.com>
2024-07-18 09:11:10 -0400

    [Python][Driver] Adding getters and setters for tensors (#43455)
    Implementation for getting and setting scalars in Python Driver Tensors.
    
    References MSDK-510
    
    MAX_TESTS_TESTS_ORIG_REV_ID: b5d6229fbbc39637bc61d61ab811207e513faa71

--- max/tests/tests/driver/test_tensor.py
+++ max/tests/tests/driver/test_tensor.py
@@ -5,10 +5,38 @@
 # ===----------------------------------------------------------------------=== #
 """Test max.driver Tensors."""
 import max.driver as md
+import pytest
 
 
 def test_tensor():
-    tensor = md.Tensor(md.dtype.float32, (3, 4, 5))
-    assert md.dtype.float32 == tensor.dtype
+    # Validate that metadata shows up correctly
+    tensor = md.Tensor(md.float32, (3, 4, 5))
+    assert md.float32 == tensor.dtype
+    assert "max.float32" == str(tensor.dtype)
     assert (3, 4, 5) == tensor.shape
     assert 3 == tensor.rank
+
+
+def test_get_and_set():
+    tensor = md.Tensor(md.int32, (3, 4, 5))
+    tensor[0, 1, 3] = 68
+    assert 68 == tensor[0, 1, 3]
+
+    # Setting negative indices
+    tensor[-1, -1, -1] = 72
+    assert 72 == tensor[2, 3, 4]
+
+    # Ensure we're not writing to the same memory location with each index
+    assert 68 == tensor[0, 1, 3]
+
+    # Cannot use non-integers when indexing
+    with pytest.raises(TypeError):
+        tensor[4.2, 2, 2] = 23
+
+    # Cannot do out-of-bounds indexing
+    with pytest.raises(IndexError):
+        tensor[5, 2, 2] = 23
+
+    # Cannot do out-of-bounds with negative indices
+    with pytest.raises(IndexError):
+        tensor[-4, -3, -3] = 42
