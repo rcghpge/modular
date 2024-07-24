942d356f6e24a6bede91e0a018d710bebf3fb012
Jakub Tucholski <jakub@modular.com>
2024-07-24 13:01:28 -0400

    [Driver][Python] Get returns zero-d tensor (#43697)
    This PR introduces the concept of zero-rank tensors into the Python API.
    Any access to an exact index in a tensor will return a zero-rank tensor
    wrapping a scalar. Users can then get the underlying scalar as a Python
    primitive by calling `.item()` on that tensor.
    
    This PR also splits out the `Tensor` object from the underlying
    `TensorStorage` object. This is intended to allow for supporting slices.
    
    References MSDK-510
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 5488aee4198cf44f7097963d769d378ff6663896

--- max/tests/tests/driver/test_tensor.py
+++ max/tests/tests/driver/test_tensor.py
@@ -20,18 +20,19 @@ def test_tensor():
 def test_get_and_set():
     tensor = md.Tensor(md.int32, (3, 4, 5))
     tensor[0, 1, 3] = 68
-    assert 68 == tensor[0, 1, 3]
+    # Get should return zero-d tensor
+    elt = tensor[0, 1, 3]
+    assert 0 == elt.rank
+    assert () == elt.shape
+    assert md.int32 == elt.dtype
+    assert 68 == elt.item()
 
     # Setting negative indices
     tensor[-1, -1, -1] = 72
-    assert 72 == tensor[2, 3, 4]
+    assert 72 == tensor[2, 3, 4].item()
 
     # Ensure we're not writing to the same memory location with each index
-    assert 68 == tensor[0, 1, 3]
-
-    # Cannot use non-integers when indexing
-    with pytest.raises(TypeError):
-        tensor[4.2, 2, 2] = 23
+    assert 68 == tensor[0, 1, 3].item()
 
     # Cannot do out-of-bounds indexing
     with pytest.raises(IndexError):
@@ -40,3 +41,13 @@ def test_get_and_set():
     # Cannot do out-of-bounds with negative indices
     with pytest.raises(IndexError):
         tensor[-4, -3, -3] = 42
+
+    # Indexes need to be equal to the tensor rank
+    with pytest.raises(ValueError):
+        tensor[2, 2] = 2
+    with pytest.raises(ValueError):
+        tensor[2, 2, 2, 2] = 2
+
+    # Cannot call item (without arguments) on a non-zero-rank tensor
+    with pytest.raises(ValueError):
+        tensor.item()
