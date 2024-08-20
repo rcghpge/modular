2772201909b682b5935c5c56dde2aad452bb916c
Jakub Tucholski <jakub@modular.com>
2024-08-20 19:30:16 -0400

    [Python][Driver] CUDA device (#45615)
    This creates a CUDA device using the Python Max Driver API. It also
    implements an abstract `Device` base class that's implemented by all
    `Device` objects.
    
    Closes MSDK-828
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 3d8973e5c1bf63367592dc7efcf65d4d86164e92

--- max/tests/tests/driver/test_device.py
+++ max/tests/tests/driver/test_device.py
@@ -4,9 +4,24 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+from os import getenv
+from subprocess import run
+
 import max.driver as md
+import pytest
 
 
 def test_cpu_device():
     cpu = md.CPU()
     assert "cpu" in str(cpu)
+
+
+def _cuda_available() -> bool:
+    output = run(getenv("MODULAR_IS_CUDA_AVAILABLE") or "is-cuda-available")
+    return output.returncode == 0
+
+
+@pytest.mark.skipif(not _cuda_available(), reason="Requires CUDA")
+def test_cuda_device():
+    cuda = md.CUDA()
+    assert "cuda" in str(cuda)
