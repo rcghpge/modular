848c2d307e26ee8544d51fb3fd07da2247d86631
Brendan Duke <bduke@modular.com>
2024-09-05 09:23:15 -0400

    [MGP] Lower external constants to alloc + HtoD (#46594)
    Lower mgp.buffer.constant.external ops to an mgp.buffer.alloc.static +
    mgp.buffer.host_to_device transfer.
    Use MOOpLoweringState::bufferTransferFromHost to emit the HtoD transfer.
    This also emits an `mgp.sync` and `mgp.extend.lifetime`, which aren't
    currently needed because the lifetime of the host external constants is
    untracked and owned outside the graph.
    However, this lifts the sync to the init block, which may help
    attributing time spent there.
    And in future if we transfer host buffer ownership to the graph, this
    will be needed anyway.
    If there is a performance hit, we can change this to lower to just
    an async `mgp.buffer.host_to_device`.
    
    Testing:
    - mo-to-primitives IR test.
    - Execution test via the graph API.
    MAX_INTEGRATION_TESTS_REV_ID: c347bae235cb0166957cf77699d0a19c1d02fbc6

--- max/tests/integration/API/python/gpu_tests/test_engine.py
+++ max/tests/integration/API/python/gpu_tests/test_engine.py
@@ -3,12 +3,16 @@
 # This file is Modular Inc proprietary.
 #
 # ===----------------------------------------------------------------------=== #
+from dataclasses import dataclass
 from math import isclose
 from pathlib import Path
 
 import numpy as np
 from max.driver import CPU, CUDA, Tensor
+from max.dtype import DType
 from max.engine import InferenceSession
+from max.graph import Graph, Value, TensorType
+from max.mlir.dialects import mo
 
 
 def test_load_on_gpu(gpu_session: InferenceSession, mo_model_path: Path):
@@ -26,3 +30,41 @@ def test_execute_gpu(gpu_session: InferenceSession, mo_model_path: Path):
     host_tensor = output_tensor.copy_to(CPU())
     for idx, elt in enumerate([4.0, 2.0, -5.0, 3.0, 6.0]):
         assert isclose(host_tensor[idx].item(), elt)
+
+
+@dataclass
+class Model:
+    num_elems: int
+
+    def __call__(self, input: Value) -> Value:
+        weights_tensor_type = TensorType(
+            DType.float32, (self.num_elems,)
+        ).to_mlir()
+        weights_tensor = Graph.current._add_op(
+            mo.constant_external,
+            result=weights_tensor_type,
+            name="foo",
+            align=np.dtype(np.float32).alignment,
+        )[0]
+
+        return input + weights_tensor
+
+
+def test_execute_external_weights_gpu(gpu_session: InferenceSession) -> None:
+    num_elems = 4096
+    weights = np.arange(num_elems, dtype=np.float32)
+
+    graph = Graph(
+        "external_weights",
+        Model(num_elems),
+        input_types=(TensorType(DType.float32, (num_elems,)),),
+    )
+    graph._mlir_op.verify()
+
+    compiled = gpu_session.load(graph, weights_registry={"foo": weights})
+    input_np = np.random.randn(num_elems).astype(np.float32)
+    output = compiled.execute(Tensor.from_numpy(input_np, device=CUDA()))[
+        0
+    ].copy_to(CPU())
+    for idx, elt in enumerate(input_np + weights):
+        assert isclose(output[idx].item(), elt)
