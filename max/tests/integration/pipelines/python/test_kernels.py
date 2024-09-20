41e0c655eddb7a5848fefc17d8567e37351ca6a0
Brendan Duke <bduke@modular.com>
2024-09-20 12:59:21 -0400

    Audit kernels.py to make sure that the function signatures match their corresponding custom kernels. This fixes one real bug where `kv_cache_length` should take a `ContiguousKVCacheCollection`, not a `ContiguousKVCache`.
    MAX_INTEGRATION_TESTS_REV_ID: 6944b02cdd660ce703f4219e32c24590c3668c98

---
+++ max/tests/integration/pipelines/python/test_kernels.py
@@ -0,0 +1,52 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from max.driver import CPU
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import Graph
+from nn.kernels import kv_cache_length
+from nn.kv_cache import (
+    ContiguousKVCacheCollectionType,
+    ContiguousKVCacheManager,
+)
+from nn.kv_cache_params import KVCacheParams
+
+
+def test_kv_cache_length() -> None:
+    """Tests that KV cache collections return the expected cache length."""
+    kv_params = KVCacheParams(
+        dtype=DType.bfloat16, n_kv_heads=8, head_dim=128, device=CPU()
+    )
+    session = InferenceSession()
+    kv_manager = ContiguousKVCacheManager(
+        params=kv_params,
+        max_batch_size=1,
+        max_seq_len=512,
+        num_layers=32,
+        session=session,
+        device=CPU(),
+    )
+
+    # Reserve a slot in the KV cache manager.
+    seq_id = kv_manager.claim(batch_size=1)[0]
+
+    # Set the cache lengths first by "stepping".
+    expected_cache_len = 42
+    kv_manager.step(valid_lengths={seq_id: expected_cache_len})
+
+    # Construct a KV cache collection with the given cache length.
+    kv_collection = kv_manager.fetch(seq_ids=[seq_id])
+    assert kv_collection is not None
+
+    graph = Graph(
+        "cache_length",
+        lambda kv_coll: kv_cache_length(kv_params, kv_coll),
+        input_types=[ContiguousKVCacheCollectionType()],
+    )
+
+    outputs = session.load(graph).execute(kv_collection)
+    assert outputs[0].item() == expected_cache_len
