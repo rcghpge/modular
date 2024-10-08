4eccf8f7353a9b737f82b793feb4e07e83df6988
Sean Paradiso <sparadiso@modular.com>
2024-10-08 18:49:31 -0400

    [Pipelines] extend tiny llama serving to be async (#48740)
    This doesn't do any actual heavy lifting right now, but I think it is a
    pattern we're going to want as we look to drill into batching behavior,
    testing timeouts on latches, etc.
    
    @gaziqbal / @mattgallivan obviously feel free to push back on this if
    you have other plans!
    
    MAX_INTEGRATION_TESTS_REV_ID: 7ed94664be92ce626fdbc7540718049e9cc369ba

--- max/tests/integration/pipelines/python/serve/test_tiny_llama_serving_gpu.py
+++ max/tests/integration/pipelines/python/serve/test_tiny_llama_serving_gpu.py
@@ -6,7 +6,9 @@
 """Test serving a Llama 3 model on the GPU."""
 
 import pytest
+from asgi_lifespan import LifespanManager
 from fastapi.testclient import TestClient
+from httpx import ASGITransport, AsyncClient
 from llama3 import SupportedEncodings
 from max.driver import CUDA
 from max.serve.mocks.mock_api_requests import simple_openai_request
@@ -14,6 +16,7 @@ from max.serve.schemas.openai import CreateChatCompletionResponse
 from .params import ModelParams
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "tinyllama_model",
     [
@@ -27,13 +30,32 @@ from .params import ModelParams
     ],
     indirect=True,
 )
-def test_tinyllama_serve_gpu(app):
-    with TestClient(app) as client:
-        raw_response = client.post(
-            "/v1/chat/completions", json=simple_openai_request()
-        )
-        # This is not a streamed completion - There is no [DONE] at the end.
-        response = CreateChatCompletionResponse.parse_raw(raw_response.json())
+async def test_tinyllama_serve_gpu(app):
+    # Arbitrary - just demonstrate we can submit multiple async
+    # requests and collect the results later
+    N_REQUESTS = 3
 
-        assert len(response.choices) == 1
-        assert response.choices[0].finish_reason == "stop"
+    async with LifespanManager(app) as manager:
+        async with AsyncClient(
+            transport=ASGITransport(app=manager.app), base_url="http://test"
+        ) as client:
+            tasks = [
+                client.post(
+                    "/v1/chat/completions", json=simple_openai_request()
+                )
+                for _ in range(N_REQUESTS)
+            ]
+
+            for task in tasks:
+                raw_response = await task
+
+                print(raw_response)
+
+                # This is not a streamed completion - There is no [DONE] at the end.
+                response = CreateChatCompletionResponse.parse_raw(
+                    raw_response.json()
+                )
+                print(response)
+
+                assert len(response.choices) == 1
+                assert response.choices[0].finish_reason == "stop"
