89a0a4bdda2bc1b0ca394cebb08ba63305da99e7
Brendan Duke <bduke@modular.com>
2024-10-16 15:28:58 -0400

    [SDK] Add max.profiler package (#49061)
    Add a max.profiler package.
    This currently contains Python bindings to the trace objecting in
    tracing.mojo.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 536e135ef713cf72992518e9476564547cb512fb

---
+++ max/tests/tests/profiler/test_profiling.py
@@ -0,0 +1,26 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from max.profiler import Trace, traced
+
+
+def test_profiling() -> None:
+    """Tests that profiling functions do not error."""
+    with Trace("foo"):
+        pass
+
+    @traced(message="baz", color="red")
+    def foo() -> None:
+        # The span is named "baz".
+        pass
+
+    @traced
+    def bar() -> None:
+        # The span is named "bar".
+        pass
+
+    foo()
+    bar()
