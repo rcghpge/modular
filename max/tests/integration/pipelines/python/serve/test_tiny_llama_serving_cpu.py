838cac2065012778c4b1945279e2b475e38a4edf
Rashid Kaleem <rashid@modular.com>
2024-10-17 14:30:36 -0500

    [Serving] Yield first token (#49193)
    Return the first token generated after the context encoding step as
    well.
    Closes SI-649
    
    ---------
    
    Signed-off-by: Rashid Kaleem <rashid@modular.com>
    MAX_INTEGRATION_TESTS_REV_ID: 8b810cc757b695c142aa9e78820c7faaafdd6e8e

--- max/tests/integration/pipelines/python/serve/test_tiny_llama_serving_cpu.py
+++ max/tests/integration/pipelines/python/serve/test_tiny_llama_serving_cpu.py
@@ -10,11 +10,15 @@ from async_asgi_testclient import TestClient
 from llama3 import SupportedEncodings
 from max.driver import CPU
 from max.serve.mocks.mock_api_requests import simple_openai_request
-from max.serve.schemas.openai import CreateChatCompletionResponse
+from max.serve.schemas.openai import (
+    CreateChatCompletionResponse,
+    CreateCompletionResponse,
+)
 
 from .params import ModelParams
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "tinyllama_model",
     [
@@ -30,7 +34,7 @@ from .params import ModelParams
 )
 async def test_tinyllama_serve_cpu(app):
     async with TestClient(app) as client:
-        raw_response = client.post(
+        raw_response = await client.post(
             "/v1/chat/completions", json=simple_openai_request()
         )
         # This is not a streamed completion - There is no [DONE] at the end.
@@ -38,3 +42,42 @@ async def test_tinyllama_serve_cpu(app):
 
         assert len(response.choices) == 1
         assert response.choices[0].finish_reason == "stop"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "tinyllama_model",
+    [
+        ModelParams(
+            weight_path="tiny_llama.gguf",
+            max_length=512,
+            max_new_tokens=10,
+            device=CPU(),
+            encoding=SupportedEncodings.float32,
+        )
+    ],
+    indirect=True,
+)
+async def test_tinyllama_serve_cpu_numtokens(app):
+    def _create_request(content, max_tokens):
+        return {
+            "model": "gpt-3.5-turbo",
+            "prompt": content,
+            "temperature": 0.7,
+            "max_tokens": max_tokens,
+        }
+
+    async with TestClient(app) as client:
+        responses = []
+        for i in range(3):
+            raw_response = await client.post(
+                "/v1/completions", json=_create_request("random request", i)
+            )
+            # This is not a streamed completion - There is no [DONE] at the end.
+            response = CreateCompletionResponse.parse_raw(raw_response.json())
+            responses.append(response.choices[0].text)
+            assert len(response.choices) == 1
+            assert response.choices[0].finish_reason == "stop"
+        # TODO Update to use llama3 decoder to count actual tokens returned.
+        for i in range(2):
+            assert responses[i + 1].startswith(responses[i])
