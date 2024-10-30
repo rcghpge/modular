311b22cc808827a02056c8c38081fa6ee5a3296b
Zheng Hao Tan <zheng@modular.com>
2024-10-30 14:03:32 -0700

    Initial stub for Llama 3.2 vision model (top) layer This PR introduces a mostly stubbed `VisionModel()` class that contains / calls into the different positional embedding layers and two other vision encoders (`VisionEncoder()` instances). We also introduce a new `VisionHyperparameters` dataclass to hold all configs for this vision pipeline. This can be consolidated into our more generic Llama 3 `Hyperparameters` in the future.
    MAX_INTEGRATION_TESTS_REV_ID: 3cae9f266568c97c45fd97dfc0a7f28309f61a28

---
+++ max/tests/integration/pipelines/python/llama3/test_vision_model.py
@@ -0,0 +1,141 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Runs Llama3.2 vision model layer.
+"""
+
+import numpy as np
+import pytest
+from llama3.vision.class_embedding import ClassEmbedding
+from llama3.vision.encoder import VisionEncoder, VisionEncoderLayer
+from llama3.vision.hyperparameters import VisionHyperparameters
+from llama3.vision.mlp import MLP
+from llama3.vision.model import VisionModel
+from llama3.vision.positional_embedding import (
+    PrecomputedAspectRatioEmbedding,
+    PrecomputedPositionEmbedding,
+)
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import Graph, TensorType
+from modular_graph_test import modular_graph_test
+from nn import Conv2D, Linear, LPLayerNorm
+
+
+def generate_test_vision_model() -> VisionModel:
+    """
+    This helper function generates a test vision model instance for testing purposes.
+    """
+    params = VisionHyperparameters()
+
+    # TODO(AIPIPE-131): Implement this.
+    patch_embedding = None
+    # TODO: Check if permutation is needed here.
+    # patch_embedding = Conv2D(
+    #     # in_channels=params.num_channels,
+    #     # out_channels=params.hidden_size,
+    #     # kernel_size=params.patch_size,
+    #     filter=patch_embedding_weight,
+    #     stride=(params.patch_size, params.patch_size),
+    #     padding=(0, 0, 0, 0),
+    #     bias=False,
+    # )
+
+    # TODO(AIPIPE-136): Implement this.
+    class_embedding = None
+    # class_embedding = nn.Parameter(
+    #     params.scale * torch.randn(params.hidden_size)
+    # )
+
+    gated_positional_embedding = PrecomputedPositionEmbedding(params)
+
+    pre_tile_positional_embedding = PrecomputedAspectRatioEmbedding(
+        params, is_gated=True
+    )
+    post_tile_positional_embedding = PrecomputedAspectRatioEmbedding(
+        params, is_gated=True
+    )
+
+    # The reference implementation does not specify an eps (so falls back to
+    # default). We do this anyway by specifying eps=1e-5.
+    layernorm_pre = None
+    layernorm_post = None
+    # layernorm_pre = LPLayerNorm(
+    #     vision_model_pre_weight, eps=params.norm_eps
+    # )
+    # layernorm_post = LPLayerNorm(
+    #     vision_model_post_weight, eps=params.norm_eps
+    # )
+
+    # encoders
+    transformer = None
+    # transformer = VisionEncoder(
+    #     [
+    #         VisionEncoderLayer(
+    #             mlp=MLP(Linear(mlp_fc1), Linear(mlp_fc2)),
+    #             input_layernorm=LPLayerNorm(encoder_layernorm_w1, eps),
+    #             post_attention_layernorm=LPLayerNorm(
+    #                 encoder_layernorm_w2, eps
+    #             ),
+    #             is_gated=False,
+    #             gate_attn=None,
+    #             gate_ffn=None,
+    #         )
+    #         for _ in range(num_hidden_layers)
+    #     ]
+    # )
+
+    global_transformer = None
+    # global_transformer = VisionEncoder(
+    #     [
+    #         VisionEncoderLayer(
+    #             mlp=MLP(Linear(mlp_fc1), Linear(mlp_fc2)),
+    #             input_layernorm=LPLayerNorm(encoder_layernorm_w1, eps),
+    #             post_attention_layernorm=LPLayerNorm(
+    #                 encoder_layernorm_w2, eps
+    #             ),
+    #             is_gated=True,
+    #             # TODO(AIPIPE-137): Implement this.
+    #             gate_attn=gate_attn,
+    #             # TODO(AIPIPE-137): Implement this.
+    #             gate_ffn=gate_ffn,
+    #         )
+    #         for _ in range(num_global_layers)
+    #     ]
+    # )
+
+    # self.post_init()  # TODO: Needed?
+    return VisionModel(
+        params=params,
+        patch_embedding=patch_embedding,
+        class_embedding=class_embedding,
+        gated_positional_embedding=gated_positional_embedding,
+        pre_tile_positional_embedding=pre_tile_positional_embedding,
+        post_tile_positional_embedding=post_tile_positional_embedding,
+        layernorm_pre=layernorm_pre,
+        layernorm_post=layernorm_post,
+        transformer=transformer,
+        global_transformer=global_transformer,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_type",
+    [
+        TensorType(DType.float32, ["dim"]),
+        TensorType(DType.float32, ["batch", "dim"]),
+        TensorType(DType.float32, ["x", "y", "z", "dim"]),
+    ],
+)
+def test_vision_model(
+    session: InferenceSession, input_type: TensorType
+) -> None:
+    dim = input_type.shape[-1]
+    mlp_fc1_type = TensorType(input_type.dtype, ["hidden_dim", dim])
+    mlp_fc2_type = TensorType(input_type.dtype, [dim, "hidden_dim"])
+    encoder_layernorm_w1_type = TensorType(input_type.dtype, [dim])
+    encoder_layernorm_w2_type = encoder_layernorm_w1_type
+
+    # TODO: Complete implementation here!
