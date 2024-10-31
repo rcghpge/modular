ccd728a4e76a37f8592d5dfb4c8ea52988971186
Sean Paradiso <sparadiso@modular.com>
2024-10-30 21:07:38 -0400

    [Pipelines] Skip weight downloads when using performance fake
    This simply avoids unnecessarily pulling weights for models when running
    the performance fake.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: d287b35b759153a86bc508d87d238cf29119431a

--- max/tests/tests/serve/test_openai_routes.py
+++ max/tests/tests/serve/test_openai_routes.py
@@ -5,6 +5,7 @@
 # ===----------------------------------------------------------------------=== #
 
 
+import json
 import functools
 from threading import Thread
 
@@ -109,3 +110,14 @@ def test_openai_echo_chat_completion_multi(app, model_name):
             threads[i].start()
         for t in threads:
             t.join()
+
+
+def test_vllm_response_deserialization():
+    vllm_response = """{"id":"chat-f33946bf8faf42849b11a4f948fc23f9","object":"chat.completion","created":1730306055,"model":"meta-llama/Meta-Llama-3-8B-Instruct","choices":[{"index":0,"message":{"role":"assistant","content":"Arrrr, listen close me hearty! Here be another one:\\n\\nWhy did the parrot go to the doctor?\\n\\nBecause it had a fowl temper! (get it? fowl, like a bird, but also a play on \\"foul\\" temper! ahh, shiver me timbers, I be laughin' me hook off!)","tool_calls":[]},"logprobs":null,"finish_reason":"stop","stop_reason":null}],"usage":{"prompt_tokens":20,"total_tokens":92,"completion_tokens":72},"prompt_logprobs":null}"""
+
+    CreateChatCompletionResponse.model_validate_json(vllm_response)
+
+
+def test_max_server_response():
+    response = """{"id":"7a0d00d-8f85-4a69-aa07-f51724787e3f","choices":[{"finish_reason":"stop","index":0,"message":{"content":"Arrrr, here be another one:nnWhy did the pirate quit his job?nnBecause he was sick o' all the arrrr-guments with his boss! (get it? arrrr-guments? ahh, never mind, matey, I'll just be walkin' the plank if I don't get a laugh out o' ye!)","refusal":"","tool_calls":null,"role":"assistant","function_call":null},"logprobs":{"content":[],"refusal":[]}}],"created":1730310250,"model":"","service_tier":null,"system_fingerprint":null,"object":"chat.completion","usage":null}"""
+    CreateChatCompletionResponse.model_validate_json(response)
