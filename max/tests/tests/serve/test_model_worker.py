72b5e8122913871611bfcc039d5e9f7a868a27ee
Gaz Iqbal <gaziqbal@gmail.com>
2024-11-06 09:42:16 -0800

    [Serving] Test coverage for exceptions or timeouts during model worker startup (#50390)
    Test would hang if the model worker process died during startup.
    This PR modifies the model worker initialization to catch exceptions and
    also adds a timeout (default of 5 mins for docker container).
    I think there is room to simplify all this more. And we can also add
    "Loading.." prints to show visible progress. This is just the first
    step.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 5728ca15b847f9af9836ef47f5147660a06607eb

---
+++ max/tests/tests/serve/test_model_worker.py
@@ -0,0 +1,66 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+# Unit tests for model_worker
+
+import pytest
+import time
+from max.pipelines.interfaces import (
+    TokenGenerator,
+)
+from max.serve.pipelines.echo_gen import EchoTokenGenerator
+from max.serve.pipelines.model_worker import start_model_worker
+
+
+@pytest.mark.asyncio
+async def test_model_worker_propagates_exception() -> None:
+    """Tests raising in the model worker context manager."""
+    with pytest.raises(AssertionError):
+        async with start_model_worker({"test": EchoTokenGenerator}):
+            raise AssertionError
+
+
+class MockInvalidTokenGenerator(TokenGenerator[str]):
+    ERROR_MESSAGE = "I am invalid"
+
+    def __init__(self):
+        raise ValueError(MockInvalidTokenGenerator.ERROR_MESSAGE)
+
+    def next_token(self, batch: dict[str, str]) -> dict[str, str]:
+        raise ValueError()
+
+    def release(self, context: str):
+        pass
+
+
+@pytest.mark.asyncio
+async def test_model_worker_propagates_construction_exception() -> None:
+    """Tests raising in the model worker task."""
+    with pytest.raises(
+        ValueError, match=MockInvalidTokenGenerator.ERROR_MESSAGE
+    ):
+        async with start_model_worker({"test": MockInvalidTokenGenerator}):
+            pass
+
+
+class MockSlowTokenGenerator(TokenGenerator[str]):
+    def __init__(self):
+        time.sleep(0.2)
+
+    def next_token(self, batch: dict[str, str]) -> dict[str, str]:
+        raise ValueError()
+
+    def release(self, context: str):
+        pass
+
+
+@pytest.mark.asyncio
+async def test_model_worker_start_timeout() -> None:
+    """Tests raising in the model worker task."""
+    with pytest.raises(TimeoutError):
+        async with start_model_worker(
+            {"test": MockSlowTokenGenerator}, timeout_secs=0.1
+        ):
+            pass
