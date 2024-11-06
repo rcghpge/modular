fe6e419a0bff081eb0744e6d0c32412ef6493ef5
kcaverly <kylebcaverly@gmail.com>
2024-11-06 13:40:52 -0500

    [Pipelines] Introduce general TextTokenizer
    MAX_INTEGRATION_TESTS_REV_ID: f3f48ce7f55ce1209ef750da28f5b4993bf55cd0

---
+++ max/tests/integration/pipelines/python/nn/test_tokenizer.py
@@ -0,0 +1,29 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import pytest
+from dataprocessing import TextContext
+from nn import TextTokenizer
+from max.pipelines import PipelineConfig, SupportedEncoding
+
+
+@pytest.mark.asyncio
+async def test_tokenizer__encode_and_decode():
+    encoding = SupportedEncoding.q4_k
+    tokenizer = TextTokenizer(
+        PipelineConfig(
+            architecture="llama",
+            version="3.1",
+            quantization_encoding=encoding,
+            huggingface_repo_id="modularai/llama-3.1",
+        )
+    )
+
+    test_string = "hi my name is"
+    context = TextContext(max_tokens=10, cache_seq_id=0, prompt=test_string)
+    encoded = await tokenizer.encode(test_string)
+    decoded = await tokenizer.decode(context, encoded, skip_special_tokens=True)
+    assert test_string == decoded
