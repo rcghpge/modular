aafb49c659256aaaee9e7bbe5f23139e452cd01d
Brendan Duke <bduke@modular.com>
2024-11-20 12:21:13 -0800

    [SDK] Move KL divergence to test_common (#51219)
    Create a new `test_common.distance_metrics` package and move KL
    divergence out so that it can be reused from there.
    
    MAX_INTEGRATION_TESTS_REV_ID: 4965b8824510f8e05dcf57ab583295e95f4d4a91

---
+++ max/tests/integration/pipelines/python/test_common/distance_metrics.py
@@ -0,0 +1,58 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Distance function definitions for use in testing infrastructure."""
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+def kl_divergence_verifier(
+    predicted: np.ndarray, expected: np.ndarray, description: str
+) -> None:
+    """Verifies predicted vs. expected vectors using KL divergence.
+
+    This is preferable to elementwise comparison for vector distances, because
+    elementwise comparison doesn't take into account the magnitude of the
+    vectors.
+    Furthermore since we are comparing logits, use the KL divergence as a
+    distance between expected and predicted probability distributions (over
+    next token classification).
+    """
+    if isinstance(predicted, np.int64):
+        # Assert that token ids generated match PyTorch exactly.
+        assert predicted == expected, description
+        return
+
+    eps_bf16 = torch.finfo(torch.bfloat16).eps
+    if isinstance(predicted, np.float32):
+        # Treat the "next logits" element specially until it is removed.
+        assert np.isclose(
+            predicted, expected, rtol=2 * eps_bf16, atol=2 * eps_bf16
+        )
+        return
+
+    logits_predicted = torch.from_numpy(predicted)
+    logits_expected = torch.from_numpy(expected)
+
+    # Convert logits to log-probabilities.
+    log_probs_expected = F.log_softmax(logits_expected, dim=-1)
+    log_probs_predicted = F.log_softmax(logits_predicted, dim=-1)
+
+    # Compute the KL divergence.
+    kl_divergence = F.kl_div(
+        log_probs_predicted,
+        log_probs_expected,
+        reduction="sum",
+        log_target=True,
+    )
+
+    # Note that this threshold was set experimentally so that the test passes
+    # with the existing Llama 3.1 implementation.
+    # Assert that the KL divergence between predicted and expected log
+    # probabilities is below 0.01.
+    threshold = 0.01
+    assert kl_divergence < threshold, description
