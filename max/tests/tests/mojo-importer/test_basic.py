3495afb4950447c4b0b48e1779010b3db627bc09
Connor Gray <code@connorgray.com>
2024-11-22 14:05:52 -0600

    [python-bindings] Add `max._mojo.mojo_importer` import hook supporting importing Mojo code from Python (#49361)
    This adds a [Python import
    hook](https://docs.python.org/3/reference/import.html#import-hooks) so
    that, given a project structure like:
    
    ```text
    my-project
    ├── main.py
    └── mojo_module.mojo
    ```
    
    Then from within main.py it's possible to import the Mojo code "as if"
    it was just another Python module:
    
    ```python
    # main.py
    
    import max._mojo.mojo_importer
    
    import mojo_module
    ```
    
    Under the hook, this invokes `mojo build --gen-py mojo_module.mojo` to
    compile the Mojo code to a dynamic library, which is then loaded as a
    [Python extension
    module](https://docs.python.org/3/extending/extending.html).
    
    ---------
    
    Co-authored-by: Stef Lindall <stef@modular.com>
    MAX_TESTS_TESTS_ORIG_REV_ID: 8e080f8c1ba76fbf008ea13ebcb6e08a79e82638

---
+++ max/tests/tests/mojo-importer/test_basic.py
@@ -0,0 +1,42 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import sys
+import os
+import unittest
+
+import max._mojo.mojo_importer
+
+# Put the current directory (containing mojo_module.mojo) on the Python module
+# lookup path.
+sys.path.insert(0, "")
+
+# TODO(MOCO-1375): Remove env var restriction on --gen-py use
+os.environ["MODULAR_MOJO_PYBIND"] = "enabled"
+
+# Imports from 'mojo_module.mojo'
+import mojo_module  # type: ignore
+
+
+class TestMojoPythonInterop(unittest.TestCase):
+    def test_pyinit(self):
+        self.assertTrue(mojo_module)
+
+    def test_pytype_reg_trivial(self):
+        self.assertEqual(mojo_module.Int.__name__, "Int")
+
+    def test_pytype_empty_init(self):
+        # Tests that calling the default constructor on a wrapped Mojo type
+        # is possible.
+        mojo_int = mojo_module.Int()
+
+        self.assertEqual(type(mojo_int), mojo_module.Int)
+
+        self.assertEqual(repr(mojo_int), "0")
+
+
+if __name__ == "__main__":
+    unittest.main()
