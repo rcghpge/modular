ec81bcfa5dc342b6ccfa67698ee31704f8a21183
akirchhoff-modular <github-wkmod@kirchhoff.digital>
2024-12-02 11:12:12 -0800

    [Pipelines] Monkey-patch no-timeout behavior into lm-eval (#51858)
    lm-eval often times out on A10 instances. The underlying reasons are not
    currently known (and should eventually be investigated), but for now, it
    makes more sense to remove the timeout. lm-eval currently has no option
    to remove the timeout. I have a patch locally that adds such an option,
    but Modular needs to sign the CLA before I can contribute it. In the
    mean time, here is a workaround that does not require any changes to
    lm-eval proper, by monkey-patching it from within our stub entry point.
    
    Part of AIPIPE-72
    
    MAX_INTEGRATION_TESTS_REV_ID: 860bff6404bae7fcdbac7894f1423a2258e03b05

--- max/tests/integration/pipelines/python/run_lm_eval.py
+++ max/tests/integration/pipelines/python/run_lm_eval.py
@@ -4,7 +4,28 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+import functools
+
 from lm_eval.__main__ import cli_evaluate
+
+
+def monkeypatch_api_timeout() -> None:
+    """Disable timeouts when running API models with num_concurrent>1."""
+
+    import aiohttp
+    from lm_eval.models import api_models
+
+    old_client_session = getattr(api_models, "ClientSession")
+
+    @functools.wraps(old_client_session)
+    def wrapped_client_session(*args, **kwargs):
+        kwargs["timeout"] = aiohttp.ClientTimeout(total=None)
+        return old_client_session(*args, **kwargs)
+
+    setattr(api_models, "ClientSession", wrapped_client_session)
+
+
+monkeypatch_api_timeout()
 
 if __name__ == "__main__":
     cli_evaluate()
