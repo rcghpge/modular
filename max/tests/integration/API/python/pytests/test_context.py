648af337043138e022477185552ba68bc26e2b54
kcaverly <kylebcaverly@gmail.com>
2024-12-06 14:49:49 -0500

    [Pipelines] Introduce num_steps in InputContext
    MAX_INTEGRATION_TESTS_REV_ID: 34e0ca8069313516cad746e5249ded3bb0dfa96a

---
+++ max/tests/integration/API/python/pytests/test_context.py
@@ -0,0 +1,44 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import numpy as np
+from max.pipelines import TextContext
+
+
+def test_context__current_length():
+    context = TextContext(
+        cache_seq_id=0,
+        prompt="this is a test prompt",
+        max_tokens=10,
+        next_tokens=np.array([0, 1, 2, 3]),
+    )
+
+    assert context.current_length == 4
+
+    context.update(np.array([4]), num_steps=None)
+    assert context.current_length == 5
+
+    # Currently, there are 5 tokens, we are saying
+    # here is the next one, and we've generated 3 tokens
+    # including that one, so increment the current length
+    # accordingly.
+    context.update(np.array([5]), num_steps=3)
+    assert context.current_length == 8
+
+
+def test_context__seq_len():
+    context = TextContext(
+        cache_seq_id=0,
+        prompt="this is a test prompt",
+        max_tokens=10,
+        next_tokens=np.array([0, 1, 2, 3]),
+    )
+
+    assert context.seq_len == 4
+    context.update(np.array([4]), num_steps=None)
+    assert context.seq_len == 1
+    context.update(np.array([5]), num_steps=5)
+    assert context.seq_len == 1
