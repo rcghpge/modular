a800e4e8e0ea17e4a851a83a4e9062bdfcc0c0a5
Zheng Hao Tan <zheng@modular.com>
2024-12-10 17:05:50 -0800

    [Pipelines] minor test_language_model.py fixes - part 2 Minor `test_language_model.py` fixes
    MAX_INTEGRATION_TESTS_REV_ID: e71984704038bd953445062faee994ce655dfc43

--- max/tests/integration/pipelines/python/llama3/vision/test_language_model.py
+++ max/tests/integration/pipelines/python/llama3/vision/test_language_model.py
@@ -7,30 +7,21 @@
 
 from __future__ import annotations
 
-import random
-from pathlib import Path
 from typing import Union
 
+import numpy as np
 import pytest
 import torch
 from llama_vision.cross_attention_decoder import (
     CrossAttentionDecoderLayer,
     CrossSdpaAttention,
 )
-from llama_vision.language_model import (
-    CausalLanguageModel,
-    TextModel,
-    instantiate_language_model,
-)
-from max.driver import CPU
+from llama_vision.language_model import CausalLanguageModel, TextModel
+from max.driver import CPU, Tensor
 from max.dtype import DType
 from max.engine import InferenceSession
-from max.graph import Graph, TensorType, Weight, ops
-from max.graph.weights import SafetensorWeights
-from max.pipelines import PipelineConfig, SupportedEncoding
+from max.graph import Graph, TensorType, Weight
 from max.pipelines.kv_cache import KVCacheParams, load_kv_manager
-
-# from test_common.distance_metrics import is_euclidean_distance_close
 from nn import (
     MLP,
     AttentionWithRopeQKV,
@@ -43,30 +34,6 @@ from nn import (
 from transformers import MllamaForCausalLM
 from transformers.models.mllama.configuration_mllama import MllamaTextConfig
 from transformers.testing_utils import torch_device
-
-# copied from https://github.com/huggingface/transformers/blob/main/tests/test_modeling_common.py
-global_rng = random.Random()
-
-
-def ids_tensor(shape, vocab_size, rng=None, name=None) -> torch.Tensor:
-    #  Creates a random int32 tensor of the shape within the vocab size
-    if rng is None:
-        rng = global_rng
-
-    total_dims = 1
-    for dim in shape:
-        total_dims *= dim
-
-    values = []
-    for _ in range(total_dims):
-        values.append(rng.randint(0, vocab_size - 1))
-
-    return (
-        torch.tensor(data=values, dtype=torch.long, device=torch_device)
-        .view(shape)
-        .contiguous()
-    )
-
 
 def weight(
     name: str, weights_array: torch.Tensor, weights_registry: dict
@@ -286,15 +253,14 @@ def self_attention_decoder_layer(
 
 def language_model_given_pytorch_model(
     pytorch_model,
-    num_attention_heads,
-    hidden_size,
-    num_key_value_heads,
-    rms_norm_eps,
-    num_hidden_layers,
-    cross_attention_layers,
+    num_attention_heads: int,
+    hidden_size: int,
+    rms_norm_eps: float,
+    num_hidden_layers: int,
+    cross_attention_layers: list[int],
     max_seq_len: int,
     rope_theta: float,
-    kv_params,
+    kv_params: KVCacheParams,
     weights_registry: dict,
     dtype: DType,
 ):
@@ -365,88 +331,18 @@ def language_model_given_pytorch_model(
     )
 
 
-def generate_test_language_model() -> Graph:
-    """
-    This helper function generates a test vision model instance for testing purposes.
-    """
-
-    pipeline_config = PipelineConfig(
-        architecture="MllamaForConditionalGeneration",
-        huggingface_repo_id="meta-llama/Llama-3.2-11B-Vision-Instruct",
-        quantization_encoding=SupportedEncoding.bfloat16,
-        weight_path=[
-            Path("model-00001-of-00005.safetensors"),
-            Path("model-00002-of-00005.safetensors"),
-            Path("model-00003-of-00005.safetensors"),
-            Path("model-00004-of-00005.safetensors"),
-            Path("model-00005-of-00005.safetensors"),
-        ],
-    )
-    text_config = pipeline_config.huggingface_config.text_config
-
-    weights = pipeline_config.load_weights()
-    assert isinstance(
-        weights, SafetensorWeights
-    ), "only safetensor weights supported currently"
-
-    kv_params = KVCacheParams(
-        dtype=pipeline_config.dtype,
-        n_kv_heads=text_config.num_key_value_heads,
-        head_dim=text_config.hidden_size // text_config.num_key_value_heads,
-    )
-    with Graph("test_llama_vision") as graph:
-        print("Building vision model...")
-        language_model = instantiate_language_model(
-            dtype=pipeline_config.dtype,
-            hidden_size=text_config.hidden_size,
-            n_heads=text_config.num_attention_heads,
-            rope_theta=text_config.rope_theta,
-            max_seq_len=text_config.max_length,
-            num_hidden_layers=text_config.num_hidden_layers,
-            cross_attention_layers=text_config.cross_attention_layers,
-            vocab_size=text_config.vocab_size,
-            rms_norm_eps=text_config.rms_norm_eps,
-            num_key_value_heads=text_config.num_key_value_heads,
-            intermediate_size=text_config.intermediate_size,
-            kv_params=kv_params,
-            weights=weights,
-        )
-
-        graph.output(
-            ops.constant(
-                language_model.kv_params.n_kv_heads, dtype=DType.int32
-            ),
-        )
-
-        return graph
-
-
-@pytest.mark.skip("requires internet and is very large")
-def test_build_language_model():
-    """
-    This test is not meant to be run in CI.
-    It will require the internet and download over 20gb of weights.
-    It is primarily meant to be run as a double check function that the vision model continues to build.
-    """
-
-    vision_model = generate_test_language_model()
-    assert isinstance(vision_model, Graph)
-
-
 @pytest.mark.parametrize(
     # num_vision_tokens = image_dim**2 // patch_dim**2 + 1 (cls token)
-    "batch_size,seq_len,num_tiles,num_vision_tokens,max_length",
+    "seq_lens,num_tiles,num_vision_tokens",
     [
-        (1, 7, 4, 1025, 512),
+        ([7, 512], 4, 1025),
     ],
 )
 def test_llama_language_model(
     session: InferenceSession,
-    batch_size: int,
-    seq_len: int,
+    seq_lens: list[int],
     num_tiles: int,
     num_vision_tokens: int,
-    max_length: int,
 ):
     config_dict = {
         "bos_token_id": 128000,
@@ -457,10 +353,10 @@ def test_llama_language_model(
         "hidden_size": 1024,  # original: 4096,
         "initializer_range": 0.02,
         "intermediate_size": 14336,
-        "max_position_embeddings": 512,  # original: 131072
+        "max_position_embeddings": 8192,  # original: 131072
         "model_type": "mllama_text_model",
         "num_attention_heads": 8,  # original: 32
-        "num_hidden_layers": 5,  # original: 40
+        "num_hidden_layers": 4,  # original: 40
         "num_key_value_heads": 8,
         "pad_token_id": 128004,
         "rms_norm_eps": 1e-05,
@@ -468,7 +364,7 @@ def test_llama_language_model(
             "factor": 8.0,
             "high_freq_factor": 4.0,
             "low_freq_factor": 1.0,
-            "original_max_position_embeddings": 8192,
+            "original_max_position_embeddings": 511,  # original: 8192
             "rope_type": "llama3",
         },
         "rope_theta": 500000.0,
@@ -481,7 +377,6 @@ def test_llama_language_model(
     config = MllamaTextConfig(**config_dict)
 
     pytorch_model = MllamaForCausalLM(config=config)
-    pytorch_model.to(torch_device)
 
     dtype = DType.float32
 
@@ -497,11 +392,10 @@ def test_llama_language_model(
         pytorch_model,
         hidden_size=config.hidden_size,
         num_attention_heads=config.num_attention_heads,
-        num_key_value_heads=config.num_key_value_heads,
         rms_norm_eps=config.rms_norm_eps,
         num_hidden_layers=config.num_hidden_layers,
         cross_attention_layers=config.cross_attention_layers,
-        max_seq_len=max_length,
+        max_seq_len=config.max_position_embeddings,
         rope_theta=config.rope_theta,
         kv_params=kv_params,
         weights_registry=weights_registry,
@@ -510,27 +404,23 @@ def test_llama_language_model(
 
     session = InferenceSession(devices=[CPU()])
 
+    batch_size = len(seq_lens)
     kv_manager = load_kv_manager(
         params=kv_params,
         max_cache_batch_size=batch_size,  # verify this.
-        max_seq_len=max_length,
+        max_seq_len=config.max_position_embeddings,
         num_layers=config.num_hidden_layers,
         session=session,
         devices=[CPU()],
     )
 
-    # seq_ids = kv_manager.claim(n=batch_size)
-    # kv_cache_inputs = tuple(
-    #     [element for tup in kv_manager.fetch(seq_ids) for element in tup]
-    # )
-
-    pytorch_input_ids = (
-        ids_tensor(
-            [batch_size, seq_len],
-            config.vocab_size - 1,
-        )
-        + 1
+    pytorch_input_ids = torch.randint(
+        0,
+        config.vocab_size - 1,
+        [batch_size, max(seq_lens)],
+        dtype=torch.long,
     )
+
     pytorch_attention_mask = pytorch_input_ids.ne(1).to(
         dtype=torch.int64, device=torch_device
     )
@@ -538,63 +428,72 @@ def test_llama_language_model(
     # Define graph API input types
     input_ids_type = TensorType(
         DType.int64,
-        # Rank of 1 for ragged graph API input tensor.
-        shape=[batch_size, seq_len],
+        shape=[batch_size, max(seq_lens)],
     )
     cross_attention_states_type = TensorType(
         dtype,
         shape=[batch_size * num_tiles * num_vision_tokens, config.hidden_size],
     )
-    input_row_offsets_type = TensorType(
+    hidden_input_row_offsets_type = TensorType(
         DType.uint32,
         [batch_size + 1],
     )
+    cross_input_row_offsets_type = hidden_input_row_offsets_type
 
     input_types = [
+        element for tup in kv_manager.input_symbols() for element in tup
+    ] + [
         input_ids_type,
+        hidden_input_row_offsets_type,
         cross_attention_states_type,
-        # Pass 2 sets of offsets for hidden and cross attn states.
-        input_row_offsets_type,
-        input_row_offsets_type,
-    ] + [element for tup in kv_manager.input_symbols() for element in tup]
+        cross_input_row_offsets_type,
+    ]
     with Graph("test_language_model", input_types=input_types) as graph:
         (
-            graph_input_ids,
-            graph_cross_attention_states,
-            graph_hidden_input_row_offsets,
-            graph_cross_input_row_offsets,
             *graph_kv_cache_inputs,
+            graph_input_ids,
+            graph_hidden_input_row_offsets,
+            graph_cross_attention_states,
+            graph_cross_input_row_offsets,
         ) = graph.inputs
 
         logits = graph_api_model(
-            kv_cache_inputs=graph_kv_cache_inputs,
+            kv_cache_inputs=tuple(graph_kv_cache_inputs),
             input_ids=graph_input_ids,
-            cross_attention_states=graph_cross_attention_states,
             hidden_input_row_offsets=graph_hidden_input_row_offsets,
+            cross_attention_states=graph_cross_attention_states,
             cross_input_row_offsets=graph_cross_input_row_offsets,
         )
         graph.output(logits)
 
     compiled = session.load(graph, weights_registry=weights_registry)
 
-    # prompt_lens = [30]
-    # assert len(prompt_lens) == batch_size
-    # input_row_offsets = Tensor(
-    #     [batch_size + 1],
-    #     DType.uint32,
-    # )
-    # running_sum = 0
-    # for i in range(batch_size):
-    #     input_row_offsets[i] = running_sum
-    #     running_sum += prompt_lens[i]
-    # input_row_offsets[batch_size] = running_sum
+    seq_ids = kv_manager.claim(n=batch_size)
+    cache_lengths = {s: seq_lens[i] for i, s in enumerate(seq_ids)}
+    kv_cache_inputs = kv_manager.fetch(cache_lengths)[0]
+
+    torch_dtype = torch.float32
+    # Initialize model inputs.
+    cross_attention_states = torch.randn(
+        cross_attention_states_type.shape.static_dims, dtype=torch_dtype
+    )
+    hidden_input_row_offsets = torch.tensor(
+        [0, *np.cumsum(seq_lens)], dtype=torch.uint32
+    )
+    cross_input_row_offsets = torch.tensor(
+        [i for i in range(batch_size + 1)],
+        dtype=torch.uint32,
+    )
 
     # predicted = compiled.execute(
-    #     input_ids, attention_mask, input_row_offsets, *kv_cache_inputs
+    #     *kv_cache_inputs,
+    #     pytorch_input_ids.to(dtype=torch.int64),
+    #     hidden_input_row_offsets,
+    #     cross_attention_states,
+    #     cross_input_row_offsets,
     # )[0]
+    # assert isinstance(predicted, Tensor)
 
-    # pytorch_model.eval()
-    # with torch.autocast(device_type="cuda", dtype=torch.float32):
     expected = pytorch_model(
         input_ids=pytorch_input_ids,
         attention_mask=pytorch_attention_mask,
@@ -603,7 +502,9 @@ def test_llama_language_model(
     assert not (torch.isnan(expected).any().item())
     expected = expected.detach().numpy()
 
-    # # Compare the outputs.
+    # Compare the outputs.
+    # np.testing.assert_array_equal(predicted.to_numpy().shape, expected.shape)
+
     # assert is_euclidean_distance_close(
     #     result=predicted.to_numpy(), expected=expected, rtol=1e-4
     # )
