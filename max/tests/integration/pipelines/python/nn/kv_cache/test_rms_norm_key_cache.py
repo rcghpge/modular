91d8db438bb1937017652a578591783028a49101
Brendan Duke <bduke@modular.com>
2024-12-11 22:40:41 -0500

    [SDK] Add rms_norm_key_cache op and test
    Add an `rms_norm_key_cache` custom op to the graph API with an
    integration test.
    This op uses the RMS norm output lambda to compute RMS norm in place in
    the `valid_lengths` section of the key cache, which is the section of
    the key cache that has been written but not "committed yet" (cache
    lengths don't increment until after the whole forward pass).
    
    MAX_INTEGRATION_TESTS_REV_ID: 582eb81e56f9f1f7cb79ce41350bae083d27ceaf

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/test_rms_norm_key_cache.py
@@ -0,0 +1,122 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from dataclasses import dataclass
+
+import numpy as np
+import pytest
+from max.driver import CPU, Tensor
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import Graph, TensorType, TensorValue
+from max.pipelines.kv_cache import (
+    ContinuousBatchingKVCacheManager,
+    FetchContinuousBatchingKVCacheCollection,
+    KVCacheParams,
+    KVCacheStrategy,
+)
+from nn.kernels import rms_norm_key_cache
+
+
+@dataclass(frozen=True)
+class RMSNormKeyCacheModel:
+    """Model containing a single matmul KV ragged op."""
+
+    fetch_layer: FetchContinuousBatchingKVCacheCollection
+    """Layer for fetching a kv cache collection."""
+
+    kv_params: KVCacheParams
+    """Hyperparameters describing this instance of the KV cache."""
+
+    layer_idx: int
+    """Layer index of the KV cache collection."""
+
+    def __call__(
+        self,
+        gamma: TensorValue,
+        total_seq_len: TensorValue,
+        input_row_offsets: TensorValue,
+        *fetch_args: TensorValue,
+    ) -> None:
+        """Stages a graph consisting of a matmul KV cache ragged custom op.
+
+        This contains both the matmul KV cache ragged custom op and a "fetch"
+        op to get a KVCacheCollection.
+        """
+        rms_norm_key_cache(
+            self.kv_params,
+            self.fetch_layer(*fetch_args),
+            gamma=gamma,
+            epsilon=1e-5,
+            layer_idx=self.layer_idx,
+            total_seq_len=total_seq_len,
+            input_row_offsets=input_row_offsets,
+        )
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        DType.float32,
+    ],
+)
+def test_rms_norm_key_cache(session: InferenceSession, dtype: DType) -> None:
+    seq_lens = [10, 4]
+    batch_size = 2
+    max_seq_len = 16
+    kv_params = KVCacheParams(
+        dtype=dtype,
+        n_kv_heads=8,
+        head_dim=128,
+        cache_strategy=KVCacheStrategy.CONTINUOUS,
+    )
+    kv_manager = ContinuousBatchingKVCacheManager(
+        kv_params,
+        max_cache_batch_size=batch_size,
+        max_seq_len=max_seq_len,
+        num_layers=1,
+        devices=[CPU()],
+        session=session,
+    )
+    fetch_layer = FetchContinuousBatchingKVCacheCollection(kv_params)
+
+    # Stage the fetch op + custom matmul KV cache ragged op graph.
+    gamma_type = TensorType(DType.float32, shape=[kv_params.head_dim])
+    input_row_offsets_type = TensorType(DType.uint32, ["batch_size_plus_1"])
+    total_seq_len_type = TensorType(DType.uint32, shape=[])
+    graph = Graph(
+        "matmul_kv_cache_ragged",
+        forward=RMSNormKeyCacheModel(fetch_layer, kv_params, layer_idx=0),
+        input_types=[
+            gamma_type,
+            total_seq_len_type,
+            input_row_offsets_type,
+            *kv_manager.input_symbols()[0],
+        ],
+    )
+
+    # Compile and init the model.
+    model = session.load(graph)
+
+    # Claim seq_ids in cache.
+    seq_ids = kv_manager.claim(n=batch_size)
+
+    cache_lengths = {s: seq_lens[i] for i, s in enumerate(seq_ids)}
+    fetch_args = kv_manager.fetch(cache_lengths)[0]
+    # First set KV blocks to all ones so that RMSNorm changes them.
+    kv_blocks = fetch_args[0]
+    all_ones = np.ones(kv_blocks.shape, dtype=kv_blocks.dtype.to_numpy())
+
+    # Copy so that we can reuse `all_ones` in a comparison later.
+    fetch_args = (Tensor.from_numpy(all_ones.copy()), *fetch_args[1:])
+
+    gamma = np.random.randn(kv_params.head_dim).astype(np.float32)
+    input_row_offsets = np.array([0, *np.cumsum(seq_lens)], dtype=np.uint32)
+    total_seq_len = np.array(sum(input_row_offsets), dtype=np.uint32)
+    model(gamma, total_seq_len, input_row_offsets, *fetch_args)
+
+    # Check that the RMSNorm wrote output to the KV cache.
+    assert (fetch_args[0].to_numpy() != all_ones).any()
