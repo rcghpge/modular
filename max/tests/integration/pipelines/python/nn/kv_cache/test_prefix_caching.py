506f1a09360ce568f4b230b23884b20890316026
Brian Zhang <30697412+atomicapple0@users.noreply.github.com>
2024-12-22 14:30:55 -0800

    [KVCache] Extend paged attention KV cache manager with prefix caching (#53190)
    During `manager.fetch`, we pass in a token list or prompt. Prior to this
    change, these prompt tokens are assumed to not have a KV cache entry.
    With prefix caching, we will look each token up in the prefix cache and
    add the corresponding blocks to the sequence's set of committed blocks.
    Now, we need to shorten the prompt to only include the tokens that were
    not already found in the prefix cache.
    
    One edge case is when all prompt tokens are found in the prefix cache.
    In this case, we could naively return an empty prompt as each token is
    already cached. However, this does not work as the model expects a
    prompt of length at least 1. Thus, our prefix cache lookup attempts to
    match on `prompt[:-1]` instead of the entire prompt.
    
    Currently the implementation of `fetch` mutates the input
    `seq_ids_and_prompt` to shorten the prompt. This is not a particularly
    elegant api. We will come up with a better api in the future.
    
    This PR does not support evictions yet. Once a block is committed and
    inserted into the trie, it will not be released even after the original
    sequence is released. This will allow future sequences to reuse the
    computed KV entries, but also means that we never free the blocks even
    when we are running out of room.
    
    This PR only supports prefix caching when paged KVCache's page size is
    1. This restriction will be lifted in the future.
    
    Run it:
    ```
    br //SDK/public/max-repo/pipelines/python:pipelines -- llama3 --quantization-encoding bfloat16 \
        --use-gpu --prompt "The meaning of life is" \
        --cache-strategy paged --kv-cache-page-size 1 --enable-prefix-caching
    ```
    
    In the above example, we reduce the KV cache usage by half when we run
    the same prompt twice (warmup + generation).
    
    Previously for the first pass, we use blocks 0-511. For the second pass,
    we use blocks 512-1023.
    
    With prefix caching, we only use blocks 0-511 for both passes.
    
    Fixes AIPIPE-217
    
    MAX_INTEGRATION_TESTS_REV_ID: bdaa058659930995d331674ce16e758b41b188f2

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/test_prefix_caching.py
@@ -0,0 +1,214 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import numpy as np
+import pytest
+from max.driver import CPU
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.pipelines.kv_cache import (
+    KVCacheManager,
+    KVCacheParams,
+    KVCacheStrategy,
+    load_kv_manager,
+)
+
+FAKE_TOKEN = 999
+
+
+def get_blocks_from_kv_tuple(kv_tuple) -> list[int]:
+    return kv_tuple[2].to(CPU()).to_numpy()[0].tolist()
+
+
+def get_uncommitted_and_committed_block_counts(
+    kv_tuple,
+) -> tuple[list[int], list[int]]:
+    lengths = kv_tuple[3].to(CPU()).to_numpy()[0]
+    return lengths[0], lengths[1]
+
+
+def create_paged_manager(num_blocks: int) -> KVCacheManager:
+    # Setting kv_heads, head_dim, and num_layers to 1 so it is easy to compute
+    # memory usage. Now we know each block is 1 byte.
+    NUM_KV_HEADS = 1
+    HEAD_DIM = 1
+    NUM_LAYERS = 1
+    PAGE_SIZE = 1
+
+    kv_params = KVCacheParams(
+        dtype=DType.float32,
+        n_kv_heads=NUM_KV_HEADS,
+        head_dim=HEAD_DIM,
+        cache_strategy=KVCacheStrategy.PAGED,
+        enable_prefix_caching=True,
+    )
+
+    session = InferenceSession()
+
+    available_cache_memory = (
+        2
+        * NUM_LAYERS
+        * NUM_KV_HEADS
+        * HEAD_DIM
+        * num_blocks
+        * kv_params.dtype.size_in_bytes
+    )
+    kv_manager = load_kv_manager(
+        params=kv_params,
+        max_cache_batch_size=9999,
+        max_seq_len=512,
+        num_layers=NUM_LAYERS,
+        devices=[CPU()],
+        session=session,
+        available_cache_memory=available_cache_memory,
+        page_size=PAGE_SIZE,
+    )
+
+    assert len(kv_manager.available_blocks) == num_blocks  # type: ignore
+    return kv_manager
+
+
+@pytest.mark.asyncio
+async def test_prefix_caching() -> None:
+    kv_manager = create_paged_manager(num_blocks=128)
+
+    # Reserve a slot in the KV cache manager.
+    seq_id_1 = 1
+    initial_prompt_1 = [10, 11, 12, 13, 14]
+    kv_manager.external_claim([seq_id_1])
+
+    # Seq 1: Prefill 10 - 14
+    seq_ids_and_prompts = {seq_id_1: np.array(initial_prompt_1)}
+    kv_tuple_list = kv_manager.fetch(seq_ids_and_prompts)
+    assert get_uncommitted_and_committed_block_counts(kv_tuple_list[0]) == (
+        len(initial_prompt_1),
+        0,
+    )
+    kv_manager.step(seq_ids_and_prompts)
+
+    # Check that we got new blocks
+    assert get_blocks_from_kv_tuple(kv_tuple_list[0]) == [0, 1, 2, 3, 4]
+
+    # Seq 1: Token gen 15 - 18
+    for i, tok in enumerate([15, 16, 17, 18]):
+        seq_ids_and_prompts = {seq_id_1: np.array([tok])}
+        kv_tuple_list = kv_manager.fetch(seq_ids_and_prompts)
+        assert get_uncommitted_and_committed_block_counts(kv_tuple_list[0]) == (
+            1,
+            5 + i,
+        )
+        kv_manager.step(seq_ids_and_prompts)
+
+    # Seq 2: Claim
+    seq_id_2 = 2
+    initial_prompt_2 = [10, 11, 12, 13]
+    kv_manager.external_claim([seq_id_2])
+
+    # Seq 2: Prefill 10 - 13
+    seq_ids_and_prompts = {seq_id_2: np.array(initial_prompt_2)}
+    kv_tuple_list = kv_manager.fetch(seq_ids_and_prompts)
+    assert get_uncommitted_and_committed_block_counts(kv_tuple_list[0]) == (
+        1,
+        len(initial_prompt_2) - 1,
+    )
+    kv_manager.step(seq_ids_and_prompts)
+
+    # Check that we got cached blocks, except for last token in prompt
+    assert get_blocks_from_kv_tuple(kv_tuple_list[0])[:3] == [0, 1, 2]
+    assert get_blocks_from_kv_tuple(kv_tuple_list[0])[3] != [3]
+
+    # Seq 2: Token gen 14 - 17
+    for i, tok in enumerate([14, 15, 99, 100]):
+        seq_ids_and_prompts = {seq_id_2: np.array([tok])}
+        kv_tuple_list = kv_manager.fetch(seq_ids_and_prompts)
+        assert get_uncommitted_and_committed_block_counts(kv_tuple_list[0]) == (
+            1,
+            len(initial_prompt_2) + i,
+        )
+        assert get_blocks_from_kv_tuple(kv_tuple_list[0])[:4] == [0, 1, 2, 3]
+        kv_manager.step(seq_ids_and_prompts)
+
+    # Validate final trie
+    assert kv_manager.radix_trie.pretty_format() == [  # type: ignore
+        "[10, 11, 12]",
+        "--[13]",
+        "----[14]",
+        "------[15]",
+        "--------[16]",
+        "----------[17]",
+        "------------[18]",
+        "--------[99]",
+        "----------[100]",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_prefix_caching_with_repeating_prompt() -> None:
+    kv_manager = create_paged_manager(num_blocks=128)
+
+    available_blocks = 128
+
+    # Try to assign and release more than 128 blocks.
+    for seq_id in range(1000):
+        kv_manager.external_claim([seq_id])
+        # We reuse the same prompt each time, allowing for prefix sharing.
+        prompt = np.array([100, 101, 102, 103, 104])
+        seq_ids_and_prompts = {seq_id: prompt}
+        _ = kv_manager.fetch(seq_ids_and_prompts)
+
+        if seq_id == 0:
+            # During first fetch, we do not get a cache hit so we use 5 blocks.
+            available_blocks -= 5
+        else:
+            # During later fetches, we get a cache hit so we use 1 block.
+            available_blocks -= 1
+        assert len(kv_manager.available_blocks) == available_blocks  # type: ignore
+
+        kv_manager.step(seq_ids_and_prompts)
+
+        if seq_id != 0:
+            # During later fetches, we will just release the block we wrote to
+            # since a different block already exists for the same token.
+            available_blocks += 1
+        assert len(kv_manager.available_blocks) == available_blocks  # type: ignore
+
+        kv_manager.release(seq_id)
+
+
+@pytest.mark.asyncio
+async def test_prefix_caching_with_no_release() -> None:
+    kv_manager = create_paged_manager(num_blocks=128)
+
+    # Try to assign and release more than 128 blocks.
+    # We expect to run out of blocks here.
+    with pytest.raises(RuntimeError):
+        for seq_id in range(1000):
+            kv_manager.external_claim([seq_id])
+            prompt = np.random.randint(0, 8, size=16)
+            seq_ids_and_prompts = {seq_id: prompt}
+            _ = kv_manager.fetch(seq_ids_and_prompts)
+            kv_manager.step(seq_ids_and_prompts)
+
+            # We intentionally do not release the sequence here!
+
+
+@pytest.mark.asyncio
+async def test_prefix_caching_with_random_prompt() -> None:
+    kv_manager = create_paged_manager(num_blocks=128)
+
+    # We expect to run out of blocks here.
+    with pytest.raises(RuntimeError):
+        # Try to assign and release more than 128 blocks.
+        for seq_id in range(1000):
+            kv_manager.external_claim([seq_id])
+            # Picking random prompts.
+            prompt = np.random.randint(0, 8, size=16)
+            seq_ids_and_prompts = {seq_id: prompt}
+            # This fetch will eventually trigger evictions from the tree. However,
+            # this is not yet supported.
+            _ = kv_manager.fetch(seq_ids_and_prompts)
+            kv_manager.step(seq_ids_and_prompts)
+            kv_manager.release(seq_id)
