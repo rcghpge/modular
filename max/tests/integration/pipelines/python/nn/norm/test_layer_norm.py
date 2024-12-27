cec10ab352cedb725e6488aaf5ff504b33f5ccce
Brendan Duke <bduke@modular.com>
2024-12-27 10:13:13 -0500

    [SDK] Accept beta argument in LPLayerNorm
    Accept a beta argument in LPLayerNorm.
    This is needed in a follow up for Llama vision correctness where
    previously the beta parameter was initialized to zero instead of the
    trained bias weight.
    As a drive by change field types from `TensorValueLike` to `Weight`.
    
    This was tested by applying the layer hook method from #50988 to the MAX
    and PyTorch Llama vision models and comparing layer outputs.
    
    As a drive by, enable the layer norm unit test, update it to take a
    bias, and move it off of `modular_graph_test`.
    
    MAX_INTEGRATION_TESTS_REV_ID: 7249cb4b9cb766e3f376dea4000d1c70afac6afb

--- max/tests/integration/pipelines/python/nn/norm/test_layer_norm.py
+++ max/tests/integration/pipelines/python/nn/norm/test_layer_norm.py
@@ -4,59 +4,115 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+from __future__ import annotations
+
+import max.driver as md
 import numpy as np
+import numpy.typing as npt
 import pytest
+import torch
 import torch.nn.functional as F
-from hypothesis import example
+from distance_metrics import is_euclidean_distance_close
 from max.dtype import DType
-from max.graph import Graph, TensorType
-from modular_graph_test import are_all_tensor_values, modular_graph_test
+from max.engine import InferenceSession
+from max.graph import Graph, TensorType, Weight
+from ndarray_from_tensor_type import ndarray_from_tensor_type
 from nn import LPLayerNorm
 
 
-def torch_layer_norm(x, weight):
-    return F.layer_norm(x, weight.shape, weight=weight, eps=1e-6)
+def torch_layer_norm(
+    x: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor
+) -> torch.Tensor:
+    return F.layer_norm(x, weight.shape, weight=weight, bias=bias, eps=1e-6)
 
 
-@pytest.mark.xfail(True, reason="AIPIPE-153")
+def _test_layer_norm(
+    inputs: npt.NDArray[np.floating],
+    weight: npt.NDArray[np.floating],
+    bias: npt.NDArray[np.floating],
+    input_type: TensorType,
+    session: InferenceSession,
+) -> None:
+    # Phase 1: op staging.
+    graph = Graph(
+        "layer_norm",
+        forward=LPLayerNorm(
+            weight=Weight(
+                name="weight",
+                dtype=DType.from_numpy(weight.dtype),
+                shape=weight.shape,
+            ),
+            bias=Weight(
+                name="bias",
+                dtype=DType.from_numpy(bias.dtype),
+                shape=bias.shape,
+            ),
+        ),
+        input_types=[input_type],
+    )
+
+    # Phase 2: model compilation and weight initialization.
+    model = session.load(
+        graph, weights_registry={"weight": weight, "bias": bias}
+    )
+
+    # Phase 3: execution.
+    predicted = model(inputs)[0]
+    assert isinstance(predicted, md.Tensor)
+
+    expected = (
+        torch_layer_norm(
+            torch.from_numpy(inputs),
+            torch.from_numpy(weight),
+            torch.from_numpy(bias),
+        )
+        .detach()
+        .numpy()
+    )
+
+    # Compared the outputs.
+    assert is_euclidean_distance_close(
+        predicted.to_numpy(), expected, rtol=1e-4
+    )
+
+
 @pytest.mark.parametrize(
     "input_type",
     [
-        TensorType(DType.float32, ["dim"]),
         TensorType(DType.float32, ["batch", "dim"]),
         TensorType(DType.float32, ["x", "y", "z", "dim"]),
     ],
 )
-def test_layer_norm(session, input_type):
-    # Initialize Graph
-    dim = input_type.shape[-1]
-    weight_type = TensorType(input_type.dtype, [dim])
-    with Graph("layer_norm", input_types=[input_type, weight_type]) as graph:
-        assert are_all_tensor_values(graph.inputs)
-        x, weight = graph.inputs
-        graph.output(LPLayerNorm(weight)(x))
+def test_layer_norm_random(
+    session: InferenceSession, input_type: TensorType
+) -> None:
+    """Compares the output of a single layer norm op against torch."""
+    # Generate random inputs with the given tensor types.
+    weight_bias_shape = TensorType(input_type.dtype, [input_type.shape[-1]])
+    inputs, weight, bias = ndarray_from_tensor_type(
+        input_type, weight_bias_shape, weight_bias_shape
+    )
 
-        @example(
-            (
-                np.array([0.0, 1.0], dtype=np.float32),
-                np.array([1.0, 0.0], dtype=np.float32),
-            )
-        )
-        @modular_graph_test(session, graph)
-        def test_correctness(execute, inputs, torch_inputs):
-            result = execute(inputs)
-            expected = torch_layer_norm(*torch_inputs).detach().numpy()
-            assert result.shape == expected.shape
-            # In some cases Torch produces NaN while we produce zero.  Fudge
-            # the Torch results to zero for locations where it produces NaN and
-            # we don't.
-            expected[np.isnan(expected) & ~np.isnan(result)] = 0
-            ACCURACY_RTOL = 1e-2
-            ACCURACY_ATOL = 1e-8
-            np.testing.assert_allclose(
-                result,
-                expected,
-                atol=ACCURACY_ATOL,
-                rtol=ACCURACY_RTOL,
-                equal_nan=True,
-            )
+    _test_layer_norm(inputs, weight, bias, input_type, session)
+
+
+@pytest.mark.parametrize(
+    "input_type,inputs,weight,bias",
+    [
+        (
+            TensorType(DType.float32, ["dim"]),
+            np.array([1.0, 0.0], dtype=np.float32),
+            np.array([0.0, 1.0], dtype=np.float32),
+            np.array([0.0, 0.0], dtype=np.float32),
+        ),
+    ],
+)
+def test_layer_norm_aipipe_153(
+    session: InferenceSession,
+    input_type: TensorType,
+    inputs: npt.NDArray[np.floating],
+    weight: npt.NDArray[np.floating],
+    bias: npt.NDArray[np.floating],
+) -> None:
+    """Regression test for AIPIPE-153 layer norm corner case."""
+    _test_layer_norm(inputs, weight, bias, input_type, session)
