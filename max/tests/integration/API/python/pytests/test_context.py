02baf4fe50a3ace99511dabbbadffac8055048a0
Brian Zhang <30697412+atomicapple0@users.noreply.github.com>
2025-01-14 12:24:39 -0800

    [Pipelines] Add test for `TextContext.trim_prompt` (#53877)
    See
    [Internal link]
    
    This test is fairly trivial and does not test a real use case.
    
    We expect the `trim_prompt` code path will be more rigorously tested
    after prefix caching is stabilized and added to CI.
    
    MAX_INTEGRATION_TESTS_REV_ID: 6486d939facdb8fb365addff025eebb9f567ccd2

--- max/tests/integration/API/python/pytests/test_context.py
+++ max/tests/integration/API/python/pytests/test_context.py
@@ -5,6 +5,7 @@
 # ===----------------------------------------------------------------------=== #
 
 import numpy as np
+import pytest
 from max.pipelines import TextContext
 
 
@@ -42,3 +43,32 @@ def test_context__seq_len():
     assert context.seq_len == 1
     context.update(5, num_steps=5)
     assert context.seq_len == 1
+
+
+def test_context__trim_prompt():
+    context = TextContext(
+        cache_seq_id=0,
+        prompt="this is a test prompt",
+        max_length=10,
+        next_tokens=np.array([0, 1, 2, 3]),
+    )
+
+    # Can't trim more tokens than the context has.
+    with pytest.raises(AssertionError):
+        context.trim_prompt(999)
+
+    # Trimming 0 tokens does nothing.
+    context.trim_prompt(0)
+    assert (context.next_tokens == np.array([0, 1, 2, 3])).all()
+    assert context.active_length == 4
+    assert context.current_length == 4
+
+    # Trimming 2 tokens should remove the first 2 tokens of prompt.
+    context.trim_prompt(2)
+    assert (context.next_tokens == np.array([2, 3])).all()
+    assert context.active_length == 2
+    assert context.current_length == 4  # does not change
+
+    # Can't trim prompt to 0 tokens.
+    with pytest.raises(AssertionError):
+        context.trim_prompt(2)
