7ce27ea89859a81db63246d99942ca910d873c5c
abdul dakkak <adakkak@modular.com>
2025-01-25 17:55:32 -0800

    [SDK] Simplify the is_float in DType to not know about float dtypes (#54219)
    The problem with the current approach is that one has to enumerate all
    the float dtypes which becomes a bit anoying as we add new dtypes. This
    just uses the masks (which is also efficient) to check if the value is
    floating or not.
    
    MAX_INTEGRATION_TESTS_REV_ID: bd94b109c582ee3f1a6ac6bfe0e7ede4abf74e52

---
+++ max/tests/integration/API/python/pytests/test_dtype.py
@@ -0,0 +1,63 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Test the max.graph dtype operations."""
+
+from hypothesis import given
+from hypothesis import strategies as st
+from max.dtype import DType
+
+int_dtype = st.sampled_from(
+    [
+        DType.int8,
+        DType.int16,
+        DType.int32,
+        DType.int64,
+        DType.uint8,
+        DType.uint16,
+        DType.uint32,
+        DType.uint64,
+    ]
+)
+
+float_dtype = st.sampled_from(
+    [
+        DType.f8e4m3,
+        DType.f8e4m3fnuz,
+        DType.f8e5m2,
+        DType.f8e5m2fnuz,
+        DType.bfloat16,
+        DType.float16,
+        DType.float32,
+        DType.float64,
+    ]
+)
+
+
+@given(dtype=int_dtype | float_dtype)
+def test_numpy_roundtrip(dtype: DType):
+    # There is no float8 / bf16 in numpy, so we cannot roundtrip float8 / bf16
+    if dtype in [
+        DType.f8e4m3,
+        DType.f8e4m3fnuz,
+        DType.f8e5m2,
+        DType.f8e5m2fnuz,
+        DType.bfloat16,
+    ]:
+        return
+    np_dtype = dtype.to_numpy()
+    assert dtype == DType.from_numpy(np_dtype)
+
+
+@given(int_dtype=int_dtype, float_dtype=float_dtype)
+def test_is_integral(int_dtype: DType, float_dtype: DType):
+    assert int_dtype.is_integral()
+    assert not float_dtype.is_integral()
+
+
+@given(int_dtype=int_dtype, float_dtype=float_dtype)
+def test_is_float(int_dtype: DType, float_dtype: DType):
+    assert not int_dtype.is_float()
+    assert float_dtype.is_float()
