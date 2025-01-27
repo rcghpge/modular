8088a57627e39f7c8da295edcaf053a73e57b6df
Aisha Mohamed <42137324+aishahasmoh@users.noreply.github.com>
2025-01-27 10:53:33 -0600

    [Pipelines] Implement the Conv1D layer
    Used Conv2D with a dummy height dimension to implement Conv1D.
    
    MAX_INTEGRATION_TESTS_REV_ID: c4af89788d8cf2f14c0456cb3144ad3d61cedd9a

---
+++ max/tests/integration/pipelines/python/nn/test_conv.py
@@ -0,0 +1,90 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+import numpy as np
+import torch
+import torch.nn as nn
+from max.driver import Tensor
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import Graph, TensorType, Weight
+from nn import Conv1D
+
+ACCURACY_RTOL = 1e-4
+ACCURACY_ATOL = 1e-6
+
+
+def test_conv1d() -> None:
+    batch_size = 3
+    in_channels = 128
+    length = 3000
+    hidden_size = 1280  # out_channels. Has nothing to do with input size
+    kernel_size = 3
+    stride = 1  # try with 2
+    padding = 1
+
+    # batch_size, in_channels, seq_length = 3000
+    input_sequence = torch.rand(size=(batch_size, in_channels, length)).to(
+        torch.float32
+    )
+
+    conv = nn.Conv1d(
+        in_channels=in_channels,
+        out_channels=hidden_size,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        bias=False,
+    )
+
+    conv.weight.data = nn.Parameter(torch.rand(size=conv.weight.data.shape))
+
+    # out_length = ((length + 2 * padding - (kernel_size - 1) - 1) / stride) + 1
+    # out_length = ((3000 + 2 * 1 - (3 - 1) - 1) / 1) + 1 = 3000
+    with torch.no_grad():
+        # permute output from (batch_size, hidden_size, out_length) to (batch_size, out_length, hidden_size).
+        torch_conv_result = torch.permute(conv(input_sequence), (0, 2, 1))
+
+    # Permute (batch_size, n_channels, seq_length) inputs to (batch_size, seq_length, n_channels) for our Graph API.
+    graph_api_inputs = torch.permute(input_sequence, (0, 2, 1)).contiguous()
+    # Permute filters from (hidden_size, in_channels, kernel_size) to (kernel_size, in_channels, hidden_size).
+    weights_registry = {}
+    weights_registry["conv1d_weight"] = torch.permute(
+        conv.weight.data, (2, 1, 0)
+    ).contiguous()
+
+    graph_api_filters = Weight(
+        name="conv1d_weight",
+        dtype=DType.from_numpy(weights_registry["conv1d_weight"].numpy().dtype),
+        shape=weights_registry["conv1d_weight"].shape,
+    )
+
+    # out_channels=hidden_size and kernel_size=kernel_size are inferred from kernel.
+    session = InferenceSession()
+    graph = Graph(
+        "conv1d",
+        Conv1D(
+            graph_api_filters,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        ),
+        input_types=(
+            TensorType(DType.float32, (batch_size, length, in_channels)),
+        ),
+    )
+
+    compiled = session.load(graph, weights_registry=weights_registry)
+
+    graph_api_conv_result = compiled.execute(graph_api_inputs)[0]
+    assert isinstance(graph_api_conv_result, Tensor)
+
+    np.testing.assert_allclose(
+        graph_api_conv_result.to_numpy(),
+        torch_conv_result.detach().numpy(),
+        equal_nan=True,
+        rtol=ACCURACY_RTOL,
+        atol=ACCURACY_ATOL,
+    )
