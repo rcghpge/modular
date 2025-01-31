eabd68da6929ca196e9f941a0545176e31856edd
kcaverly <kylebcaverly@gmail.com>
2025-01-31 11:27:08 -0500

    [Pipelines] Add Support for XGrammar Compilers and Matcher within TextTokenizer
    This creates a Grammar matcher provided a json schema, and leverages
    jump ahead decoding to forward fill any available tokens prior to
    context encoding. There are a few perf concerns with enabling this due
    to the type hint of `Sequence[int]` that we may want to verify before
    merging.
    
    Additionally, currently xgrammar is only supported on x86_64 machines.
    As a result, we only have it added with our GPU dependencies, and will
    remain GPU only for now.
    
    MAX_INTEGRATION_TESTS_REV_ID: 55a899f7869a7c157e6fe775add3a0bb31c128b1

---
+++ max/tests/integration/API/python/gpu_tests/test_tokenizer_gpu.py
@@ -0,0 +1,67 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+import asyncio
+
+from max.driver import DeviceSpec
+from max.pipelines import (
+    PipelineConfig,
+    TextTokenizer,
+    TokenGeneratorRequest,
+    TokenGeneratorRequestMessage,
+    TokenGeneratorResponseFormat,
+)
+
+
+def test_text_tokenizer_with_constrained_decoding():
+    pipeline_config = PipelineConfig(
+        huggingface_repo_id="HuggingFaceTB/SmolLM-135M",
+        device_specs=[DeviceSpec.accelerator(id=0)],
+        enable_constrained_decoding=True,
+    )
+
+    tokenizer = TextTokenizer(pipeline_config)
+
+    prompt = """
+    Please provide a json response, with the person's name and age extracted from the excerpt.
+    For example, provided an excerpt 'Joe Biden is 100 years old.' return with {"name": "Joe Biden", "age": 100}.
+
+    Please extract the person's name and age from the following excerpt:
+    'Donald Trump is 102 years old.'
+
+    """
+
+    request = TokenGeneratorRequest(
+        id="request_with_tools",
+        index=0,
+        model_name=pipeline_config.huggingface_repo_id,
+        messages=[
+            TokenGeneratorRequestMessage(
+                role="user",
+                content=prompt,
+            )
+        ],
+        response_format=TokenGeneratorResponseFormat(
+            type="json_schema",
+            json_schema={
+                "title": "Person",
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                    },
+                    "age": {
+                        "type": "integer",
+                    },
+                },
+                "required": ["name", "age"],
+            },
+        ),
+    )
+
+    context = asyncio.run(tokenizer.new_context(request))
+
+    assert context.matcher
+    assert isinstance(context.prompt, str)
