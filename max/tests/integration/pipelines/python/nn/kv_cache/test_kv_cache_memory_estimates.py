21283f8e7b269b0373202e76299e8d7453112002
Austin Doolittle <austin@modular.com>
2025-02-04 12:44:32 -0800

    [KVCache] fix paged cache memory size truncation (#55154)
    We previously truncated based on max length without considering
    page_size, which resulted in under-representing the amount of memory
    necessary to store the cache. This adds unit tests to verify the
    behavior works as expected and patches the issue.
    
    MAX_INTEGRATION_TESTS_REV_ID: ec8b6014e758e674746b848c6853db35d0a279db

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/test_kv_cache_memory_estimates.py
@@ -0,0 +1,61 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from max.driver import Device
+from max.dtype import DType
+from max.pipelines.kv_cache import KVCacheParams, KVCacheStrategy
+from max.pipelines.kv_cache.paged_cache import PagedKVCacheManager
+
+
+def test_paged_kv_cache_memory_estimates_truncated():
+    kv_params = KVCacheParams(
+        dtype=DType.float32,
+        n_kv_heads=8,
+        head_dim=128,
+        cache_strategy=KVCacheStrategy.PAGED,
+        page_size=128,
+    )
+    available_cache_memory = 10 * 1024 * 1024 * 1024
+    max_seq_len = 1024
+    max_batch_size = 1
+
+    estimated_cache_memory = PagedKVCacheManager.estimated_memory_size(
+        kv_params,
+        max_batch_size,
+        max_seq_len,
+        1,
+        available_cache_memory,
+        [Device.cpu(0)],
+    )
+
+    expected_value = 1024 * 8 * 128 * 2 * 4
+    assert estimated_cache_memory == expected_value
+
+
+def test_paged_kv_cache_memory_estimates_not_truncated():
+    kv_params = KVCacheParams(
+        dtype=DType.float32,
+        n_kv_heads=8,
+        head_dim=128,
+        cache_strategy=KVCacheStrategy.PAGED,
+        page_size=128,
+    )
+    available_cache_memory = (
+        8000000  # notably less than the calculated size of the cache.
+    )
+    max_seq_len = 1024
+    max_batch_size = 1
+
+    estimated_cache_memory = PagedKVCacheManager.estimated_memory_size(
+        kv_params,
+        max_batch_size,
+        max_seq_len,
+        1,
+        available_cache_memory,
+        [Device.cpu(0)],
+    )
+
+    assert estimated_cache_memory == available_cache_memory
