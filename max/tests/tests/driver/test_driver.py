19e9e0da5996680afc7223f78258eaf9e470982c
Brendan Duke <bduke@modular.com>
2025-02-07 09:41:06 -0500

    [SDK] Expose device.synchronize in Python
    Expose `max.driver.device.synchronize`, which is needed to ensure
    multi-device operations synchronize.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 8fe7d5f3960dc11e476e1eca60f40df8a4cfc417

--- max/tests/tests/driver/test_driver.py
+++ max/tests/tests/driver/test_driver.py
@@ -10,3 +10,8 @@ import max.driver as md
 
 def test_version():
     assert md.__version__, "expected version to have value"
+
+
+def test_synchronize() -> None:
+    """Tests that the `synchronize` method works -- no-op on CPU."""
+    md.CPU().synchronize()
