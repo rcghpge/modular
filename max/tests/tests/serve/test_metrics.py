4009cf245f35c758a65f336af6526d6c17048fca
Matt Terry <matt.terry@gmail.com>
2025-02-10 11:05:34 -0800

    [Serving] Refactor metric collection to make async recording easy (#55298)
    Provide way to separate the measurement of telemetry from the recording
    of it. This introduces MaxMeasurement which bundles the metric, the
    measurement, the time when it happens. With this data collected, we can
    defer / translate the recording of this data.
    
    The PR refactors our existing telemetry recording sync/async to use this
    abstraction. A followup that moves recording to a separate process will
    come shortly.
    
    This refactors where metric definitions are stored (now a global
    container) since metric definitions are global. OTEL will yell at you if
    you define multiple metrics with the same name. It collects metric &
    logging configuration into a shared place (common) separate from metric
    definition. This helps you ensure that you configure OTEL before
    creating metrics and protects against stateful metric objects.
    
    OTEL's internal structure is gross & they seem to actively obscure the
    functionality that allow you to set the observation time of a metric
    along with metric & value. I appologies in advance for the typing
    shenanigans and all the _interal imports. If it is any consolation, OTEL
    hasn't touched the metric code in 3 years, so things should be fairly
    stable.
    
    Contributes to MAXSERV-857
    
    MAX_TESTS_TESTS_ORIG_REV_ID: f6edb81621fa7faa0fd239a3d563908e1f1f4057

---
+++ max/tests/tests/serve/test_metrics.py
@@ -0,0 +1,64 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+import pickle
+from unittest import mock
+
+import pytest
+from max.serve.config import Settings
+from max.serve.telemetry import common, metrics
+from opentelemetry.metrics import get_meter_provider  # type: ignore
+
+_meter = get_meter_provider().get_meter("testing")
+
+
+def test_correct_metric_names():
+    for name, inst in metrics.SERVE_METRICS.items():
+        assert name == inst._name
+
+
+def test_max_measurement():
+    m = metrics.MaxMeasurement("maxserve.itl", 1)
+    m.commit()
+
+
+def test_serialization():
+    measurements = [
+        metrics.MaxMeasurement("maxserve.itl", 1),
+        metrics.MaxMeasurement("maxserve.itl", -3.4),
+        metrics.MaxMeasurement("maxserve.itl", 1, attributes={"att1": "val"}),
+    ]
+    for m in measurements:
+        b = pickle.dumps(m)
+        m2 = pickle.loads(b)
+
+        assert m.instrument_name == m2.instrument_name
+        assert m.value == m2.value
+        assert m.attributes == m2.attributes
+        assert m.time_unix_nano == m2.time_unix_nano
+
+
+def test_reject_unknown_metric():
+    m = metrics.MaxMeasurement("bogus", 1)
+    with pytest.raises(metrics.UnknownMetric):
+        m.commit()
+
+
+def test_instrument_called():
+    common.configure_metrics(Settings())
+    itl = metrics.SERVE_METRICS["maxserve.itl"]
+    assert itl._real_instrument is not None
+    with mock.patch.object(
+        itl._real_instrument, "_measurement_consumer"
+    ) as mock_consumer:
+        # make _real_instrument None & verify that the measurement does _not_ get consumed
+        with mock.patch.object(itl, "_real_instrument", None):
+            metrics.MaxMeasurement("maxserve.itl", 1).commit()
+            assert mock_consumer.consume_measurement.call_count == 0
+
+        # put things back together and verify that it does get consumed
+        metrics.MaxMeasurement("maxserve.itl", 1).commit()
+        # make sure the consumer got called
+        assert mock_consumer.consume_measurement.call_count == 1
