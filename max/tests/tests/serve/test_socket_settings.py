be2f88e4c02eaaf54c1805bc18860571e298b433
Steven Truong <5817749+steventr@users.noreply.github.com>
2025-02-13 09:58:07 -0700

    [Serve] Validate server settings (port) before model load (#55773)
    This PR makes it so that serve throws an error earlier when trying to
    start up on an occupied port -- preventing doing unnecessary work (model
    loading, compiling) before finding the port is taken.
    
    Closes CLIN-41
    
    ---------
    
    Co-authored-by: Zheng Hao Tan <zheng@modular.com>
    MAX_TESTS_TESTS_ORIG_REV_ID: 502078d65a79751296dfff526d9785bed53fb9e7

---
+++ max/tests/tests/serve/test_socket_settings.py
@@ -0,0 +1,21 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+import socket
+
+import pytest
+from max.serve.config import Settings
+
+
+def test_setting_throws_occupied_port():
+    # Ensure port occupied
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.bind(("", 8000))
+        except socket.error as e:
+            # It's okay if it's occupied already for some reason.
+            pass
+        with pytest.raises(ValueError):
+            socket_settings = Settings(host="0.0.0.0", port=8000)
