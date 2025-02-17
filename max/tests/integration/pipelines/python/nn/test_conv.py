f998404543c8477fb617a30494aa257cb88d54a5
Aisha Mohamed <42137324+aishahasmoh@users.noreply.github.com>
2025-02-17 05:54:04 -0600

    [GraphAPI] Implement Conv3D Layer (#56024)
    [GraphAPI] Implement Conv3D Layer
    
    MAX_INTEGRATION_TESTS_REV_ID: 28281716b31d63358da808814fc85dfea6ad9924

--- max/tests/integration/pipelines/python/nn/test_conv.py
+++ max/tests/integration/pipelines/python/nn/test_conv.py
@@ -10,7 +10,7 @@ from max.driver import Tensor
 from max.dtype import DType
 from max.engine import InferenceSession
 from max.graph import Graph, TensorType, Weight
-from max.pipelines.nn import Conv1D
+from max.pipelines.nn import Conv1D, Conv3D
 
 ACCURACY_RTOL = 1e-4
 ACCURACY_ATOL = 1e-6
@@ -79,6 +79,87 @@ def test_conv1d() -> None:
         ),
         input_types=(
             TensorType(DType.float32, (batch_size, length, in_channels)),
+        ),
+    )
+
+    compiled = session.load(graph, weights_registry=weights_registry)
+
+    graph_api_conv_result = compiled.execute(graph_api_inputs)[0]
+    assert isinstance(graph_api_conv_result, Tensor)
+
+    np.testing.assert_allclose(
+        graph_api_conv_result.to_numpy(),
+        torch_conv_result.detach().numpy(),
+        equal_nan=True,
+        rtol=ACCURACY_RTOL,
+        atol=ACCURACY_ATOL,
+    )
+
+
+def test_conv3d() -> None:
+    in_channels = 3
+    out_channels = 1280
+    kernel_size = (2, 14, 14)
+    stride = (2, 14, 14)
+
+    # input params
+    batch_size = 3
+    depth = 32
+    height = 112
+    width = 112
+
+    # batch_size, in_channels, depth, height, width
+    input_sequence = torch.rand(
+        size=(batch_size, in_channels, depth, height, width)
+    ).to(torch.float32)
+
+    conv = nn.Conv3d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        bias=False,
+    )
+
+    conv.weight.data = nn.Parameter(torch.rand(size=conv.weight.data.shape))
+    print("Filter shape = ", conv.weight.data.shape)
+    print(
+        "Should be = (out_channels, in_channels, depth, height, width) = ",
+        (out_channels, in_channels, depth, height, width),
+    )
+
+    with torch.no_grad():
+        # permute output from (batch_size, out_channels, depth, height, width) to (batch_size, depth, height, width, out_channels).
+        torch_conv_result = torch.permute(conv(input_sequence), (0, 2, 3, 4, 1))
+
+    # Permute (batch_size, in_channels, depth, height, width) inputs to (batch_size, depth, height, width, in_channels) for our Graph API.
+    graph_api_inputs = torch.permute(
+        input_sequence, (0, 2, 3, 4, 1)
+    ).contiguous()
+    # Permute filters from (out_channels, in_channels, depth, height, width) to (depth, height, width, in_channels / num_groups, out_channels).
+    weights_registry = {}
+    weights_registry["conv3d_weight"] = torch.permute(
+        conv.weight.data, (2, 3, 4, 1, 0)
+    ).contiguous()
+
+    graph_api_filters = Weight(
+        name="conv3d_weight",
+        dtype=DType.from_numpy(weights_registry["conv3d_weight"].numpy().dtype),
+        shape=weights_registry["conv3d_weight"].shape,
+    )
+
+    session = InferenceSession()
+    graph = Graph(
+        "conv3d",
+        Conv3D(
+            graph_api_filters,
+            bias=None,
+            stride=stride,
+        ),
+        input_types=(
+            TensorType(
+                DType.float32, (batch_size, depth, height, width, in_channels)
+            ),
         ),
     )
 
