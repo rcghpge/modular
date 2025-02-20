70b4cb3ea69ce563ac02bed01879cf9b6ec7b9d0
Steven Truong <5817749+steventr@users.noreply.github.com>
2025-02-20 15:32:36 -0700

    [Serve] Add a version route to MAX Serve (#56310)
    Add a '/version' endpoint
    
    Resolves MAXSERV-766
    
    MAX_TESTS_TESTS_ORIG_REV_ID: e1319585fb32fd18125ecf4fd3b0aeb073c6ba91

--- max/tests/tests/serve/test_routes.py
+++ max/tests/tests/serve/test_routes.py
@@ -4,9 +4,45 @@
 #
 # ===----------------------------------------------------------------------=== #
 
-
+import pytest
+from fastapi.testclient import TestClient
+from max.serve.api_server import ServingTokenGeneratorSettings, fastapi_app
+from max.serve.config import APIType, Settings
+from max.serve.pipelines.echo_gen import (
+    EchoPipelineTokenizer,
+    EchoTokenGenerator,
+)
+from max.serve.pipelines.llm import (
+    TokenGeneratorPipelineConfig,
+)
 from max.serve.router import openai_routes
 from max.serve.schemas.openai import CreateCompletionRequest  # type: ignore
+
+
+@pytest.fixture
+def app():
+    settings = Settings(
+        api_types=[APIType.KSERVE], MAX_SERVE_USE_HEARTBEAT=False
+    )
+
+    pipeline_settings = ServingTokenGeneratorSettings(
+        model_name="echo",
+        model_factory=EchoTokenGenerator,
+        pipeline_config=TokenGeneratorPipelineConfig.dynamic_homogenous(
+            batch_size=1
+        ),
+        tokenizer=EchoPipelineTokenizer(),
+    )
+    return fastapi_app(settings, pipeline_settings)
+
+
+# In Bazel, we throw PackageNotFoundError since we're not running as a proper package.
+def test_version_endpoint_exists(app):
+    with TestClient(app) as client:
+        response = client.get("/version")
+        assert response.status_code == 200
+        assert response.json()
+        assert response.json()["version"]
 
 
 def test_prompts():
