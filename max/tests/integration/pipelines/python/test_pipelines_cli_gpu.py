29c6e4b8db45b549b565fef09a8c21344f0c6d21
kcaverly <kylebcaverly@gmail.com>
2025-03-03 09:46:09 -0500

    [Pipelines] Fix default value for bitmask in null scenario
    There was a bug in the default values for the bitmask, in the scenario
    in which the server was enabled for structured output, but no json
    schema
    was passed. This fixes this bug, allowing both structured and
    non-structured
    queries interspersed.
    
    MAX_INTEGRATION_TESTS_REV_ID: 49514fd65bdb323af1b9df7a0a3e648a053d0fd2

--- max/tests/integration/pipelines/python/test_pipelines_cli_gpu.py
+++ max/tests/integration/pipelines/python/test_pipelines_cli_gpu.py
@@ -7,6 +7,7 @@
 import hf_repo_lock
 import pytest
 from max.entrypoints import pipelines
+from max.pipelines import PIPELINE_REGISTRY
 
 
 def test_pipelines_cli__smollm_bfloat16(capsys):
@@ -24,6 +25,35 @@ def test_pipelines_cli__smollm_bfloat16(capsys):
                 hf_repo_lock.revision_for_hf_repo(
                     "HuggingFaceTB/SmolLM2-135M-Instruct"
                 ),
+            ]
+        )
+    captured = capsys.readouterr()
+    # With an Instruct model, and as generate does not have prompt
+    # templating for chat, this will just complete the question before resolving.
+    assert "?" in captured.out
+
+
+def test_pipelines_cli__smollm_bfloat16_with_structured_output_enabled(capsys):
+    PIPELINE_REGISTRY.reset()
+    with pytest.raises(SystemExit):
+        pipelines.main(
+            [
+                "generate",
+                "--model-path",
+                "HuggingFaceTB/SmolLM2-135M-Instruct",
+                "--prompt",
+                "Why is the sky blue",
+                "--trust-remote-code",
+                "--device-memory-utilization=0.1",
+                "--huggingface-revision",
+                hf_repo_lock.revision_for_hf_repo(
+                    "HuggingFaceTB/SmolLM2-135M-Instruct"
+                ),
+                # We are enabling structured output here, to ensure that
+                # enabling structured output server wide, without a JSON
+                # schema provided does not change the outputs of the base
+                # chat experience.
+                "--enable-structured-output",
             ]
         )
     captured = capsys.readouterr()
