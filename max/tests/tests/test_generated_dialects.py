d9de46d074a6fd0502fbe2790e043f80fd968526
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-03-11 16:55:38 -0400

    [SDK][NFC] Bind python functions agaist M::DType instead of M_Dtype (#57458)
    This starts to untangle the mess that is `max._core.dtype.DType`.
    
    First of all, currently there are two worlds of types:
    `M_Dtype` converts to and from `max._core.dtype.DType`.
    `M::DType` converts to and from `int`.
    
    Now, `M_Dtype` and `int` are out of the equation, and `M::DType` maps to
    and from `max._core.dtype.DType`, though with a minor indirection
    through a type caster. The type caster is needed because we can't bind
    `M::DType` directly since it's not an enum (nanobind restriction), so
    it's just a very thin layer that uses the underlying `M::DType::Cases`
    instead.
    
    Towards MAXPLAT-88
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 33659a7f3ba1e70f8367e6ea0002cd7079748352

--- max/tests/tests/test_generated_dialects.py
+++ max/tests/tests/test_generated_dialects.py
@@ -7,9 +7,10 @@
 
 # TODO(MAXPLAT-75): Generate mypy stubs
 from max._core.dialects import mo  # type: ignore
+from max._core.dtype import DType
 
 
 def test_mo_attr(mlir_context):
     # TODO(GEX-1848): use max.dtype.DType
     attr = mo.DTypeAttr(mlir_context, 1)
-    assert attr.dtype == 1
+    assert attr.dtype == DType.bool
