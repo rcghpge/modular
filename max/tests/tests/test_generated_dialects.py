97c4ec1210f3e72abf20036b639c668165bff0de
Stef Lindall <stef@modular.com>
2025-03-20 14:42:00 -0700

    Primarily adds the `builtin` dialect bindings, while adding a number of quality of live improvements to make them work and allow testing them. - Allow binding interfaces methods with overloads - Add __repr__ - Add Attribute and Type subclasses - De-duplicate interface methods - Register TypeID <-> type_info hooks - Type casters for mlir::Type and llvm::StringAttr, and attribute - interfaces - Type hooks for mlir::Attributes - Tests for various builtin dialect things
    MAX_TESTS_TESTS_ORIG_REV_ID: 76f82e49739d7e9bbe2f73820463394458345af3

--- max/tests/tests/test_generated_dialects.py
+++ max/tests/tests/test_generated_dialects.py
@@ -5,31 +5,69 @@
 # ===----------------------------------------------------------------------=== #
 """Tests attribute factories."""
 
+import functools
+
 # TODO(MAXPLAT-75): Generate mypy stubs
-from max._core.dialects import mo, mosh  # type: ignore
+from max import mlir
+from max._core import Type
+from max._core.dialects import builtin, mo, mosh  # type: ignore
 from max._core.dtype import DType
 
 
 def test_mo_attr(mlir_context):
-    # TODO(GEX-1848): use max.dtype.DType
-    attr = mo.DTypeAttr(mlir_context, 1)
+    attr = mo.DTypeAttr(mlir_context, DType.bool)
     assert attr.dtype == DType.bool
+    assert attr == mo.DTypeAttr(mlir_context, DType.bool)
+    assert attr != mo.DTypeAttr(mlir_context, DType.int8)
 
 
 def test_mosh(mlir_context):
     shape_type = mosh.ShapeType(mlir_context)
     assert isinstance(shape_type, mosh.ShapeType)
-    # TODO(MAXPLAT-67)
-    # assert isinstance(shape_type, mlir.Type)
+    assert isinstance(shape_type, Type)
+    assert shape_type == mosh.ShapeType(mlir_context)
 
 
 def test_mosh_shapeattr(mlir_context):
     shape_type = mosh.ShapeType(mlir_context)
-    attr = mosh.MKGENShapeAttr([1, 2, 3], shape_type)
-    # Next: validate `attr.values`!
+    attr = mosh.ShapeAttr([1, 2, 3], shape_type)
+    dims = attr.values
+    index_type = builtin.IntegerType(
+        mlir_context, 64, builtin.SignednessSemantics.signed
+    )
+    uint8_type = builtin.IntegerType(
+        mlir_context, 8, builtin.SignednessSemantics.unsigned
+    )
+    Index = functools.partial(builtin.IntegerAttr, index_type)
+    UInt8 = functools.partial(builtin.IntegerAttr, uint8_type)
+    expected = [Index(1), Index(2), Index(3)]
+    assert attr.values == expected
+    assert attr.values != []
+    assert attr.values != [Index(1), Index(1), Index(1)]
+    assert attr.values != [UInt8(1), UInt8(2), UInt8(3)]
 
 
 def test_mosh_shapeattr_empty(mlir_context):
     shape_type = mosh.ShapeType(mlir_context)
-    attr = mosh.MKGENShapeAttr([], shape_type)
-    # assert list(attr.values) == []
+    attr = mosh.ShapeAttr([], shape_type)
+    assert list(attr.values) == []
+
+
+def test_mosh_shapeattr__no_active_context():
+    with mlir.Context() as ctx:
+        shape_type = mosh.ShapeType(ctx)
+    attr = mosh.ShapeAttr([], shape_type)
+    assert attr.values == []
+    assert attr.type == shape_type
+
+
+def test_builtin_integerattr(mlir_context):
+    int_type = builtin.IntegerType(
+        mlir_context, 1, builtin.SignednessSemantics.unsigned
+    )
+    int_attr = builtin.IntegerAttr(int_type, 1)
+    assert int_attr.type == int_type
+
+
+def test_builtin_moduleop(mlir_context):
+    op = builtin.ModuleOp(mlir.Location.current)
