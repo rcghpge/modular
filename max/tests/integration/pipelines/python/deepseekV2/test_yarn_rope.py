64cb2af55194d0f980d4a2a9fa13848f454bd02f
Kate Caldwell <85645843+katelyncaldwell@users.noreply.github.com>
2025-03-24 16:13:05 -0700

    [Pipelines] Yarn Rope refactor.  (#58338)
    MAX_INTEGRATION_TESTS_REV_ID: a6a4d19b7841ca19e7cdd5b311c64525d6c29358

--- max/tests/integration/pipelines/python/deepseekV2/test_yarn_rope.py
+++ max/tests/integration/pipelines/python/deepseekV2/test_yarn_rope.py
@@ -9,8 +9,9 @@ from max._core.engine import PrintStyle
 from max.dtype import DType
 from max.engine import InferenceSession
 from max.graph import Graph, Shape, TensorType
-from max.pipelines.architectures.deepseekV2.layers.yarn_rope import (
-    YarnRotaryEmbedding,
+from max.nn.rotary_embedding import (
+    DeepseekYarnRopeScalingParams,
+    DeepseekYarnRotaryEmbedding,
 )
 from torch.utils.dlpack import from_dlpack
 from torch_reference.configuration_deepseek import (
@@ -48,18 +49,34 @@ def generate_torch_outputs(
     torch_output = layer(input_tensor_rope, seq_len=input_tensor_rope.shape[2])
     torch_cos = torch_output[0].to(torch.bfloat16)
     torch_sin = torch_output[1].to(torch.bfloat16)
-    return torch_cos, torch_sin
+    return torch.stack([torch_cos, torch_sin], axis=0)
 
 
 def generate_max_outputs(
     config: DeepseekV2Config,
     input_tensor_rope: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
+    scaling_params = DeepseekYarnRopeScalingParams(
+        scaling_factor=config.rope_scaling["factor"],
+        original_max_position_embeddings=config.rope_scaling[
+            "original_max_position_embeddings"
+        ],
+        beta_fast=config.rope_scaling["beta_fast"],
+        beta_slow=config.rope_scaling["beta_slow"],
+        mscale=config.rope_scaling["mscale"],
+        mscale_all_dim=config.rope_scaling["mscale_all_dim"],
+    )
     session = InferenceSession()
     session.set_debug_print_options(style=PrintStyle.COMPACT)
     graph = Graph(
         "YarnRope",
-        YarnRotaryEmbedding(config.qk_rope_head_dim),
+        DeepseekYarnRotaryEmbedding(
+            config.qk_rope_head_dim,
+            theta=config.rope_theta,
+            n_heads=config.num_attention_heads,
+            max_seq_len=config.max_position_embeddings,
+            scaling_params=scaling_params,
+        ),
         input_types=(
             TensorType(
                 DType.bfloat16,
@@ -70,29 +87,20 @@ def generate_max_outputs(
 
     compiled = session.load(graph)
     max_output = compiled.execute(input_tensor_rope)
-    max_cos = from_dlpack(max_output[0]).to(torch.bfloat16)
-    max_sin = from_dlpack(max_output[1]).to(torch.bfloat16)
-    return max_cos, max_sin
+    return from_dlpack(max_output[0]).to(torch.bfloat16)
 
 
 def test_yarn_rope(
     config: DeepseekV2Config,
     input_tensor_rope: torch.Tensor,
 ) -> None:
-    torch_cos, torch_sin = generate_torch_outputs(config, input_tensor_rope)
-    max_cos, max_sin = generate_max_outputs(config, input_tensor_rope)
+    torch_output = generate_torch_outputs(config, input_tensor_rope)
+    max_output = generate_max_outputs(config, input_tensor_rope)
 
     # TODO (MODELS-396): These tolerances are likely too permissive. This should be revisited and adjusted if needed when the model is E2E validated.
     torch.testing.assert_close(
-        torch_cos,
-        max_cos,
-        rtol=2e-2,
-        atol=2e-2,
-    )
-
-    torch.testing.assert_close(
-        torch_sin,
-        max_sin,
+        torch_output,
+        max_output,
         rtol=2e-2,
         atol=2e-2,
     )
