97a7f3fd93cc69a85743b731ec45a46877df595d
akirchhoff-modular <akirchhoff@modular.com>
2025-04-10 14:26:23 -0700

    [Pipelines] Add Llama3 to dataset evaluation (#59435)
    Requested by Dan.
    
    MAX_INTEGRATION_TESTS_REV_ID: 481b52b7c4ac8347b7a9ec5a27be235598a5f78a

---
+++ max/tests/integration/pipelines/python/dataset_eval_configs/meta-llama/Meta-Llama-3-8B-Instruct.sh
@@ -0,0 +1,13 @@
+##===----------------------------------------------------------------------===##
+#
+# This file is Modular Inc proprietary.
+#
+##===----------------------------------------------------------------------===##
+
+batch_size=100
+max_length=4096
+extra_pipelines_args=(--enable-echo)
+extra_lm_eval_args=(
+  --apply_chat_template
+  --fewshot_as_multiturn
+)
