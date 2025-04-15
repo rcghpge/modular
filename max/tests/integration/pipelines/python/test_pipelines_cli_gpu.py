057d91678c633596f7687739a04a5417ce93d50f
kcaverly <kylebcaverly@gmail.com>
2025-04-15 11:12:15 -0400

    [Pipelines] Add smoke test for Speculative Decoding
    MAX_INTEGRATION_TESTS_REV_ID: ac673768865ed51ad2d0b10a8385bb3207ef9a8e

--- max/tests/integration/pipelines/python/test_pipelines_cli_gpu.py
+++ max/tests/integration/pipelines/python/test_pipelines_cli_gpu.py
@@ -20,7 +20,7 @@ def test_pipelines_cli__smollm_bfloat16(capsys):
                 "Why is the sky blue",
                 "--trust-remote-code",
                 "--device-memory-utilization=0.1",
-                "--huggingface-revision",
+                "--huggingface-model-revision",
                 hf_repo_lock.revision_for_hf_repo(
                     "HuggingFaceTB/SmolLM2-135M-Instruct"
                 ),
@@ -43,7 +43,7 @@ def test_pipelines_cli__smollm_bfloat16_with_structured_output_enabled(capsys):
                 "Why is the sky blue",
                 "--trust-remote-code",
                 "--device-memory-utilization=0.1",
-                "--huggingface-revision",
+                "--huggingface-model-revision",
                 hf_repo_lock.revision_for_hf_repo(
                     "HuggingFaceTB/SmolLM2-135M-Instruct"
                 ),
@@ -58,3 +58,25 @@ def test_pipelines_cli__smollm_bfloat16_with_structured_output_enabled(capsys):
     # With an Instruct model, and as generate does not have prompt
     # templating for chat, this will just complete the question before resolving.
     assert "?" in captured.out
+
+
+def test_pipelines_cli__speculative_decoding(capsys):
+    with pytest.raises(SystemExit):
+        pipelines.main(
+            [
+                "generate",
+                "--model-path",
+                "HuggingFaceTB/SmolLM2-360M-Instruct",
+                "--draft-model-path",
+                "HuggingFaceTB/SmolLM2-135M-Instruct",
+                "--prompt",
+                "hey, hows it going?",
+                "--device-memory-utilization=0.3",
+                "--max-num-steps=5",
+                "--no-enable-chunked-prefill",
+            ]
+        )
+
+    captured = capsys.readouterr()
+
+    assert "I was just thinking about our last chat" in captured.out
