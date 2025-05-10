727fa8acea0f27ddc460b5c02aa3a63e88bbf73a
Stef Lindall <stef@modular.com>
2025-05-09 17:21:15 -0700

    [SDK] Add `max.support.notebooks` and `%%mojo` notebook magic
    `import max.support.notebooks` will register the `%%mojo` notebook
    magic.
    
    - Use as `%%mojo` to compile and run a cell via `mojo run`
    - Provide more arguments eg. `%%mojo package -o custom_ops.mojopkg` for
    interactive package compilation and loading in a notebook.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 9d41423b667578e3db036d33b20384e9c1f1d349

---
+++ max/tests/tests/support/test_notebook.py
@@ -0,0 +1,49 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from pathlib import Path
+
+import pytest
+from IPython.testing.globalipapp import start_ipython
+from IPython.utils.capture import capture_output
+from max.support.paths import MojoCompilationError
+
+
+@pytest.fixture(scope="session")
+def ipython():
+    ipython = start_ipython()
+    import max.support.notebook  # noqa
+
+    yield ipython
+
+
+def test_mojo_run_print(ipython):
+    with capture_output() as captured:
+        ipython.run_cell_magic(
+            magic_name="mojo",
+            line="",
+            cell="""
+def main():
+    print(1)
+""",
+        )
+    assert captured.stdout.strip() == "1"
+
+
+def test_compile_error(ipython):
+    with pytest.raises(MojoCompilationError):
+        ipython.run_cell_magic(
+            magic_name="mojo", line="", cell='''var i: Int = "hello"'''
+        )
+
+
+def test_mojo_package(ipython):
+    ipython.run_cell_magic(
+        magic_name="mojo",
+        line="package -o hello.mojopkg",
+        cell='''def hello() -> String: return "hello"''',
+    )
+    assert Path("hello.mojopkg").is_file()
