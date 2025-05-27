d483b463f3aa3e2d028ccbd3d44929781cf70999
Brian Zhang <bez@modular.com>
2025-05-27 13:28:26 -0400

    [python-bindings] Create _core_mojo module (#62511)
    Completes MSTDL-1370
    
    See:
    [Internal link]
    
    Seems like we decided on:
    - Decision 1: Placement of Mojo Sources
      - Put Mojo sources in `SDK/lib/API/python/max/_core_mojo`
    - Decision 2: When to compile Mojo to .so
      - Choice 3: Fully Lazy (add import caching in future if needed)
    
    Testing:
    ```
    bt //SDK/lib/API/python/tests/_core_mojo:tests
    ```
    MAX_TESTS_TESTS_ORIG_REV_ID: 25ec625b3672c019ef92bb25651ba412f1ccf607

---
+++ max/tests/tests/_core_mojo/test_block_hasher.py
@@ -0,0 +1,32 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+
+import numpy as np
+from max._core_mojo import block_hasher
+
+
+def test_block_hasher():
+    block_size = 128
+    num_tokens = 3000
+    tokens = np.arange(num_tokens, dtype=np.int32)  # Use int32 for tokens
+
+    hashes = block_hasher(tokens, block_size)
+
+    assert isinstance(hashes, list)
+    assert isinstance(hashes[0], int)
+    assert len(hashes) == num_tokens // block_size
+
+    # It is very unlikely (but not impossible) that a valid hasher will return 0
+    # Usually a 0 is indicative of a bug of some sort.
+    assert 0 not in hashes
+
+    # It is unlikely (but not impossible) that a hasher will return the same value twice.
+    # Usually a duplicate is indicative of a bug of some sort.
+    seen = set()
+    for h in hashes:
+        assert h not in seen
+        seen.add(h)
