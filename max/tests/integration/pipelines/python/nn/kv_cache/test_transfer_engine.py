c7489165dd8b8a6da5e4e9843ff174b066170eaa
Brian Zhang <bez@modular.com>
2025-05-27 12:06:12 -0400

    [NIXL] Implement KVCache transfer engine abstraction above NIXL using UCX / TCP
    Created a new `KVTransferEngine` class that abstracts ugly details of
    NIXL.
    
    Limitations:
    - each agent has exactly one contiguous buffer of pages to handle (may
    not be true with tiered KVCache / spec decoding).
    - only DRAM->DRAM transfers via TCP are tested
    - GPU transfers are untested
    - Non-sync polling operations are unimplemented
    - Limited cleanup on engine destruction
    - Use torch dlpack hack to get data_ptr
    - transfers only accept single src / dst pair, instead of a list
    MAX_INTEGRATION_TESTS_REV_ID: fa1ea2dc5b53ae41006b1cfd8bd6d1849f43e3dc

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/test_transfer_engine.py
@@ -0,0 +1,165 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+
+from queue import Queue
+from threading import Thread
+
+import numpy as np
+import pytest
+from max.driver import CPU, Accelerator
+from max.driver.tensor import Tensor
+from max.dtype import DType
+from max.nn.kv_cache import (
+    KVTransferEngine,
+    KVTransferEngineMetadata,
+    XferReqData,
+)
+
+
+def transfer_routine_sender(
+    engine: KVTransferEngine, remote: KVTransferEngineMetadata, queue: Queue
+):
+    xfer_req = engine.initiate_send_xfer(remote, src_idx=2, dst_idx=1)
+    queue.put(xfer_req)
+    engine.send_xfer_sync(xfer_req)
+
+
+def transfer_routine_receiver(engine: KVTransferEngine, queue: Queue):
+    xfer_req = queue.get()
+    engine.recv_xfer_sync(xfer_req)
+
+
+def test_send_recv_basic():
+    device = CPU()
+    total_num_pages = 3
+    elts_per_page = 3
+    num_elts = total_num_pages * elts_per_page
+
+    blocks_1 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int8) + 10).to(
+        device
+    )
+    blocks_2 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int8) + 80).to(
+        device
+    )
+
+    engine_1 = KVTransferEngine(
+        "engine_1", blocks_1, total_num_pages, listen_port=8043
+    )
+    engine_2 = KVTransferEngine(
+        "engine_2", blocks_2, total_num_pages, listen_port=8053
+    )
+
+    engine_1.connect(engine_2.metadata)
+    engine_2.connect(engine_1.metadata)
+
+    queue: Queue[XferReqData] = Queue()
+    thread_1 = Thread(
+        target=transfer_routine_sender,
+        args=(engine_1, engine_2.metadata, queue),
+    )
+    thread_2 = Thread(target=transfer_routine_receiver, args=(engine_2, queue))
+
+    # This is done via threads so wait_send_complete and wait_recv_complete
+    # can progress in parallel. Doing this with single thread may cause hangs.
+    thread_1.start()
+    thread_2.start()
+
+    thread_1.join()
+    thread_2.join()
+
+    expected_blocks_1 = np.array(
+        [10, 11, 12, 13, 14, 15, 16, 17, 18],
+    )
+    expected_blocks_2 = np.array(
+        [80, 81, 82, 16, 17, 18, 86, 87, 88],
+    )
+    assert np.array_equal(blocks_1.to_numpy(), expected_blocks_1)
+    assert np.array_equal(blocks_2.to_numpy(), expected_blocks_2)
+
+
+def test_constructor():
+    tensor = Tensor(DType.int8, (10, 10), device=CPU())
+
+    # ok
+    _ = KVTransferEngine(
+        "abc",
+        tensor,
+        total_num_pages=2,
+        listen_port=8043,
+    )
+
+    # device is not CPU
+    with pytest.raises(ValueError):
+        _ = KVTransferEngine(
+            "abc",
+            tensor.to(Accelerator()),
+            total_num_pages=2,
+            listen_port=8044,
+        )
+
+    # total_num_pages is 0
+    with pytest.raises(ValueError):
+        _ = KVTransferEngine(
+            "abc",
+            tensor,
+            total_num_pages=0,
+            listen_port=8045,
+        )
+
+    # bytes is not divisible by total_num_pages
+    with pytest.raises(ValueError):
+        _ = KVTransferEngine(
+            "abc",
+            tensor,
+            total_num_pages=3,
+            listen_port=8046,
+        )
+
+
+def test_initiate_send_xfer():
+    device = CPU()
+    total_num_pages = 3
+    elts_per_page = 3
+    num_elts = total_num_pages * elts_per_page
+
+    blocks_1 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int8) + 10).to(
+        device
+    )
+    blocks_2 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int8) + 80).to(
+        device
+    )
+
+    engine_1 = KVTransferEngine(
+        "engine_1", blocks_1, total_num_pages, listen_port=8043
+    )
+    engine_2 = KVTransferEngine(
+        "engine_2", blocks_2, total_num_pages, listen_port=8053
+    )
+
+    engine_1.connect(engine_2.metadata)
+    engine_2.connect(engine_1.metadata)
+
+    # ok
+    _ = engine_1.initiate_send_xfer(engine_2.metadata, src_idx=2, dst_idx=1)
+
+    # oob src_idx
+    with pytest.raises(ValueError):
+        _ = engine_1.initiate_send_xfer(
+            engine_2.metadata, src_idx=100, dst_idx=1
+        )
+
+    # oob dst_idx
+    with pytest.raises(ValueError):
+        _ = engine_1.initiate_send_xfer(
+            engine_2.metadata, src_idx=2, dst_idx=100
+        )
+
+    # oob dst_idx
+    with pytest.raises(ValueError):
+        _ = engine_1.initiate_send_xfer(
+            engine_2.metadata, src_idx=2, dst_idx=-1
+        )
