5437f60b405e945c12862793ad3d6c9ae3a53af8
Brian Zhang <bez@modular.com>
2025-06-03 11:41:34 -0700

    [NIXL] Rewrite `NixlTestUcx.cpp` in Python using NIXL bindings (#62890)
    This uses the raw NIXL python bindings instead of calling into the
    higher level TransferEngine abstraction like in
    `test_transfer_engine.py`.
    
    This is nearly a direct port of the existing C++ test.
    
    This is helpful as I am trying to figure out why
    `test_transfer_engine.py` is flaky and `NixlTestUcx.cpp` is not.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 99469d2daf383a10ed6d4e192719c67a3973d38f

--- max/tests/tests/nixl/test_nixl_gpu.py
+++ max/tests/tests/nixl/test_nixl_gpu.py
@@ -5,19 +5,28 @@
 # ===----------------------------------------------------------------------=== #
 """Tests basic NIXL functionality"""
 
+import time
 from typing import cast
 
 import numpy as np
 import pytest
+import torch
 from max._core.nixl import (
     Agent,
     AgentConfig,
     MemoryType,
     RegistrationDescriptorList,
     Status,
+    TransferDescriptorList,
+    TransferOpType,
 )
 from max.driver import CPU, Accelerator, Device, Tensor
 from numpy.typing import ArrayLike
+
+
+def _get_tensor_base_addr(tensor: Tensor) -> int:
+    """Get the base address of a tensor."""
+    return torch.from_dlpack(tensor).data_ptr()
 
 
 def create_agent(
@@ -48,16 +57,18 @@ def test_agent_registration():
 
 
 def test_remote_agent_registration():
-    agent_1, _ = create_agent("test_agent_1", 8047)
-    agent_2, _ = create_agent("test_agent_2", 8057)
+    agent_1, _ = create_agent("agent_1", 8047)
+    agent_2, _ = create_agent("agent_2", 8057)
 
     # Get Agent Metadata from Agents
     agent_1_md = agent_1.get_local_metadata()
     agent_2_md = agent_2.get_local_metadata()
 
     # Register Agent with Pair
-    agent_2.load_remote_metadata(agent_1_md)
-    agent_1.load_remote_metadata(agent_2_md)
+    remote_bytes_1 = agent_2.load_remote_metadata(agent_1_md)
+    assert remote_bytes_1.decode() == "agent_1"
+    remote_bytes_2 = agent_1.load_remote_metadata(agent_2_md)
+    assert remote_bytes_2.decode() == "agent_2"
 
 
 @pytest.mark.parametrize("device", [CPU(), Accelerator()])
@@ -90,3 +101,155 @@ def test_memory_registration(device: Device):
         backends=[ucx_backend],
     )
     assert status == Status.SUCCESS
+
+
+@pytest.mark.parametrize("device", [CPU()])
+def test_memory_transfer(device: Device):
+    buffer_size_1 = 1024
+    buffer_magic_val_1 = 99
+    buffer_size_2 = 2048
+    buffer_magic_val_2 = 42
+    agent_1_name = "agent_1"
+    agent_2_name = "agent_2"
+
+    buffer_1 = Tensor.from_numpy(
+        np.full((buffer_size_1,), buffer_magic_val_1, dtype=np.int8)
+    ).to(device)
+    buffer_2 = Tensor.from_numpy(
+        np.full((buffer_size_2,), buffer_magic_val_2, dtype=np.int8)
+    ).to(device)
+
+    memory_type = MemoryType.DRAM if device.is_host else MemoryType.VRAM
+
+    # Create Agents
+    agent_1, ucx_backend_1 = create_agent(agent_1_name, listen_port=8037)
+    agent_2, ucx_backend_2 = create_agent(agent_2_name, listen_port=8042)
+
+    # Register Memory
+    reg_dlist_1 = RegistrationDescriptorList(
+        type=memory_type, descs=[cast(ArrayLike, buffer_1)], sorted=True
+    )
+    agent_1.register_memory(descs=reg_dlist_1, backends=[ucx_backend_1])
+
+    reg_dlist_2 = RegistrationDescriptorList(
+        type=memory_type, descs=[cast(ArrayLike, buffer_2)], sorted=True
+    )
+    agent_2.register_memory(descs=reg_dlist_2, backends=[ucx_backend_2])
+
+    # Get Agent Metadata from Agents
+    agent_1_md = agent_1.get_local_metadata()
+    agent_2_md = agent_2.get_local_metadata()
+
+    # Register Agent with Pair
+    remote_bytes_2 = agent_2.load_remote_metadata(agent_1_md)
+    assert remote_bytes_2.decode() == agent_1_name
+    remote_bytes_1 = agent_1.load_remote_metadata(agent_2_md)
+    assert remote_bytes_1.decode() == agent_2_name
+
+    # Compute buffer regions to transfer
+    # The offsets and transfer sizes are arbitrarily chosen for this test.
+    dst_offset = buffer_size_1 // 4
+    src_offset = 0
+    bytes_to_copy = buffer_size_1 // 2
+    src_base = _get_tensor_base_addr(buffer_1) + src_offset
+    dst_base = _get_tensor_base_addr(buffer_2) + dst_offset
+
+    # Create xfer request
+    xfer_dlist_src = TransferDescriptorList(
+        type=memory_type,
+        descs=[(src_base, bytes_to_copy, memory_type.value)],
+    )
+    xfer_dlist_dst = TransferDescriptorList(
+        type=memory_type,
+        descs=[(dst_base, bytes_to_copy, memory_type.value)],
+    )
+    secret_message = "mojo is so fast 🔥"
+    xfer_req = agent_1.create_transfer_request(
+        operation=TransferOpType.WRITE,
+        local_descs=xfer_dlist_src,
+        remote_descs=xfer_dlist_dst,
+        remote_agent=agent_2_name,
+        notif_msg=secret_message,
+    )
+
+    status = agent_1.post_transfer_request(xfer_req)
+    assert status in [Status.SUCCESS, Status.IN_PROG], (
+        "Failed to post transfer request"
+    )
+
+    check_status = Status.IN_PROG
+    notif_received = False
+    start_time = time.time()
+    timeout = 5.0
+    notif_map: dict[str, list[bytes]] = {}
+
+    while (check_status != Status.SUCCESS or not notif_received) and (
+        time.time() - start_time
+    ) < timeout:
+        if check_status != Status.SUCCESS:
+            check_status = agent_1.get_transfer_status(xfer_req)
+            assert check_status in [Status.SUCCESS, Status.IN_PROG], (
+                "Received unexpected transfer status"
+            )
+
+        if not notif_received:
+            notif_map = agent_2.get_notifs()
+            if agent_1_name in notif_map and len(notif_map[agent_1_name]) > 0:
+                notif_received = True
+
+        # Yield/Sleep briefly to avoid busy-waiting
+        time.sleep(0.001)
+
+    assert check_status == Status.SUCCESS, (
+        f"Transfer did not complete in {timeout} seconds"
+    )
+    assert agent_1_name in notif_map, "Notification map missing agent 1"
+    assert len(notif_map[agent_1_name]) == 1, (
+        "Incorrect number of notifications received"
+    )
+    received_message = notif_map[agent_1_name][0].decode()
+    assert received_message == secret_message, "Notification message mismatch"
+
+    def s2ms(s: float) -> float:
+        return s * 1000
+
+    # Should take only a couple ms
+    print(f"Transfer completed in {s2ms(time.time() - start_time):.2f} ms")
+    print(f"Agent 2 received notification from Agent 1: '{received_message}'")
+
+    # Verify transferred data
+
+    # Buffer 1 should be unchanged
+    buffer_1_expected = np.full(
+        (buffer_size_1,), buffer_magic_val_1, dtype=np.int8
+    )
+    assert np.array_equal(buffer_1.to_numpy(), buffer_1_expected), (
+        "Transferred data does not match expected data"
+    )
+
+    # Buffer 2 should be mostly 42's except for some region of 99's
+    buffer_2_expected = np.full(
+        (buffer_size_2,), buffer_magic_val_2, dtype=np.int8
+    )
+    buffer_2_expected[dst_offset : dst_offset + bytes_to_copy] = (
+        buffer_magic_val_1
+    )
+    assert np.array_equal(buffer_2.to_numpy(), buffer_2_expected), (
+        "Transferred data does not match expected data"
+    )
+
+    # Cleanup
+    status = agent_1.release_transfer_request(xfer_req)
+    assert status == Status.SUCCESS, "Failed to release transfer request"
+
+    status = agent_2.deregister_memory(reg_dlist_2)
+    assert status == Status.SUCCESS, "Failed to deregister memory"
+
+    status = agent_1.invalidate_remote_metadata(agent_2_name)
+    assert status == Status.SUCCESS, "Failed to invalidate remote metadata"
+
+    status = agent_2.invalidate_remote_metadata(agent_1_name)
+    assert status == Status.SUCCESS, "Failed to invalidate remote metadata"
+
+    status = agent_1.deregister_memory(reg_dlist_1)
+    assert status == Status.SUCCESS, "Failed to deregister memory"
