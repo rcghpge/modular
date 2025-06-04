86171248745151133167604ff48b9940ac4ce725
Austin Doolittle <austin@modular.com>
2025-06-04 18:25:49 -0400

    [Kernels] Add scatter_set_constant op
    We have a Scatter op, but it copies the whole input tensor to the
    output. We'd much rather make these modifications in place for such a
    sparse set of changes, this is a simpler op that fills in specific
    coordinates with a static value. Currently only supports 2D inputs.
    
    MAX_INTEGRATION_TESTS_REV_ID: 7082181eb9b8525d7b9d4169c3a9b90089e8672a

---
+++ max/tests/integration/pipelines/python/nn/test_scatter_set_constant_gpu.py
@@ -0,0 +1,70 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Test the scatter_set_constant kernel."""
+
+import numpy as np
+import pytest
+from max.driver import CPU, Accelerator, Device, Tensor
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import BufferType, DeviceRef, Graph, TensorType
+from max.nn.kernels import scatter_set_constant
+
+
+@pytest.mark.parametrize("device", [CPU(), Accelerator()])
+def test_scatter_set_constant_2d(device: Device):
+    vocab_size = 3
+    data = Tensor.from_numpy(np.zeros((3, 3), dtype=np.float32)).to(device)
+    indices = Tensor.from_numpy(
+        np.array([[0, 1], [1, 1], [1, 2], [2, 0], [2, 2]], dtype=np.int32)
+    ).to(device)
+    fill_val = 5.0
+
+    with Graph(
+        "scatter_set_constant_graph",
+        input_types=[
+            BufferType(
+                DType.float32,
+                ("seq_len", vocab_size),
+                device=DeviceRef.from_device(device),
+            ),
+            TensorType(
+                DType.int32,
+                ("num_indices", 2),
+                device=DeviceRef.from_device(device),
+            ),
+        ],
+    ) as graph:
+        data_val = graph.inputs[0].buffer
+        indices_val = graph.inputs[1].tensor
+        scatter_set_constant(data_val, indices_val, fill_val)
+        graph.output(data_val)
+
+    session = InferenceSession(devices=[device])
+    model = session.load(graph)
+
+    model(data, indices)
+
+    expected_output = np.array(
+        [
+            [
+                0,
+                5,
+                0,
+            ],
+            [
+                0,
+                5,
+                5,
+            ],
+            [
+                5,
+                0,
+                5,
+            ],
+        ]
+    ).astype(np.float32)
+    np.testing.assert_array_equal(data.to_numpy(), expected_output)
