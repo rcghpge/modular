365dc56e7016bfafb5201d7d5409802e2bbb0cd1
Brian Zhang <bez@modular.com>
2025-06-05 10:42:56 -0700

    [NIXL] Fix mysterious UCX warnings in NIXL transfer test
    On main, the tranfer engine tests are passing and the transfers are run
    successfully.
    
    However, there are concerning warnings printed on the console sometimes.
    
    These can be discovered by running the tests multiple times via
    `--runs_per_test 10`:
    ```
    bt //SDK/integration-test/pipelines/python/nn:tests_transfer_engine --test_output=streamed --test_arg=-svv --cache_test_results=no --runs_per_test 10
    ...
    [1749067638.748586] [workspace-bez-a100-695bd5d7c7-cjk86:490793:0]       event_set.c:176  UCX  ERROR epoll_ctl(event_fd=65, DEL, fd=80) failed: Bad file descriptor
    ...
    [1749067638.748642] [workspace-bez-a100-695bd5d7c7-cjk86:490793:0]           async.c:585  UCX  WARN  failed to remove async handler 0x144d9440 [id=80 ref 1] ???() : Input/output error
    [1749067638.748646] [workspace-bez-a100-695bd5d7c7-cjk86:490793:0]            sock.c:63   UCX  WARN  failed to close fd 80: Bad file descriptor
    ...
    ============================== 4 passed in 4.64s ===============================
    ...
    ```
    
    I believe that this is because there are instances of UCX in the same
    pytest session.
    
    By splitting the tests into isolated targets, these issues disappear:
    ```
    bt //SDK/integration-test/pipelines/python/nn/kv_cache/transfer_engine:test_send_recv_gpu --test_output=streamed --test_arg=-svv --cache_test_results=no --runs_per_test 10
    ```
    
    MAX_INTEGRATION_TESTS_REV_ID: ccbd07ad190d552a86b5cc1b5328920600551244

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/transfer_engine/test_send_recv_gpu.py
@@ -0,0 +1,93 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+# NIXL transfers can fail when multiple transfers are in the same bazel pytest target.
+# This appears as a invalid file descriptor error.
+# As such, this transfer test is alone in this file.
+
+from queue import Queue
+from threading import Thread
+
+import numpy as np
+import pytest
+from common import get_unique_port
+from max.driver import CPU, Accelerator, Device
+from max.driver.tensor import Tensor
+from max.nn.kv_cache import (
+    KVTransferEngine,
+    KVTransferEngineMetadata,
+    XferReqData,
+)
+
+
+def transfer_routine_sender(
+    engine: KVTransferEngine,
+    remote: KVTransferEngineMetadata,
+    queue: Queue,
+    src_idxs: list[int],
+    dst_idxs: list[int],
+):
+    xfer_req = engine.initiate_send_xfer(remote, src_idxs, dst_idxs)
+    queue.put(xfer_req)
+    engine.send_xfer_sync(xfer_req)
+
+
+def transfer_routine_receiver(engine: KVTransferEngine, queue: Queue):
+    xfer_req = queue.get()
+    engine.recv_xfer_sync(xfer_req)
+
+
+@pytest.mark.parametrize("device", [CPU(), Accelerator()])
+def test_send_recv_basic(device: Device):
+    total_num_pages = 3
+    elts_per_page = 3
+    num_elts = total_num_pages * elts_per_page
+
+    blocks_1 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int16) + 10).to(
+        device
+    )
+    blocks_2 = Tensor.from_numpy(np.arange(num_elts, dtype=np.int16) + 80).to(
+        device
+    )
+
+    engine_1 = KVTransferEngine(
+        "engine_1", blocks_1, total_num_pages, listen_port=get_unique_port()
+    )
+    engine_2 = KVTransferEngine(
+        "engine_2", blocks_2, total_num_pages, listen_port=get_unique_port()
+    )
+
+    engine_1.connect(engine_2.metadata)
+    engine_2.connect(engine_1.metadata)
+
+    queue: Queue[XferReqData] = Queue()
+    src_idxs = [2, 2]
+    dst_idxs = [1, 0]
+    thread_1 = Thread(
+        target=transfer_routine_sender,
+        args=(engine_1, engine_2.metadata, queue, src_idxs, dst_idxs),
+    )
+    thread_2 = Thread(target=transfer_routine_receiver, args=(engine_2, queue))
+
+    # This is done via threads so wait_send_complete and wait_recv_complete
+    # can progress in parallel. Doing this with single thread may cause hangs.
+    thread_1.start()
+    thread_2.start()
+
+    thread_1.join()
+    thread_2.join()
+
+    expected_blocks_1 = np.array(
+        [10, 11, 12, 13, 14, 15, 16, 17, 18],
+    )
+    expected_blocks_2 = np.array(
+        [16, 17, 18, 16, 17, 18, 86, 87, 88],
+    )
+    assert np.array_equal(blocks_1.to_numpy(), expected_blocks_1)
+    assert np.array_equal(blocks_2.to_numpy(), expected_blocks_2)
+
+    engine_2.cleanup()
+    engine_1.cleanup()
