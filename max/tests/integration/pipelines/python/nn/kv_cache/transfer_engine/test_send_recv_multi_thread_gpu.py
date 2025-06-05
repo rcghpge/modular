bdf0ea7c23c56c017efcc3c46d9c4b4e1b1ac6d9
Brian Zhang <bez@modular.com>
2025-06-05 12:14:32 -0700

    [NIXL] Add a fully multi-threaded NIXL send/recv test (#63126)
    Unlike the other test, engine init and connection are done in the
    spawned threads.
    
    It currently passes but skips engine cleanup :|
    
    When engine cleanup is enabled, it still passes but prints a mysterious
    warning:
    
    ```
    (autovenv) ubuntu@workspace-bez-a100-6bffb6f887-4mx69:~/modular$ bt //SDK/integration-test/pipelines/python/nn/kv_cache/transfer_engine:test_send_recv_multi_thread_gpu  --test_output=streamed --test_arg=-svv --cache_test_results=no --runs_per_test 10
    ...
    Created backend: ucx
    Created backend: ucx
    Loading remote metadata for agent: engine_2
    Loading remote metadata for agent: engine_1
    MD listener is listening on port 8002...
    MD listener is listening on port 8003...
    [1749150207.000943] [workspace-bez-a100-6bffb6f887-4mx69:241266:0]           flush.c:58   UCX  ERROR req 0x7fdc8011f280: error during flush: Endpoint timeout
    PASSED
    ```
    
    MAX_INTEGRATION_TESTS_REV_ID: 2c95ee6541cc90bd317855151433e09a28cd8221

---
+++ max/tests/integration/pipelines/python/nn/kv_cache/transfer_engine/test_send_recv_multi_thread_gpu.py
@@ -0,0 +1,107 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+
+from queue import Queue
+from threading import Thread
+
+import numpy as np
+import pytest
+from common import get_unique_port
+from max.driver import Accelerator, Device
+from max.driver.tensor import Tensor
+from max.nn.kv_cache import (
+    KVTransferEngine,
+    KVTransferEngineMetadata,
+    XferReqData,
+)
+
+
+@pytest.mark.parametrize("device", [Accelerator()])
+def test_send_recv_basic(device: Device):
+    # Queues for communication between threads
+    sender_md_queue: Queue[KVTransferEngineMetadata] = Queue()
+    receiver_md_queue: Queue[KVTransferEngineMetadata] = Queue()
+    xfer_queue: Queue[XferReqData] = Queue()
+
+    # Transfer parameters
+    total_num_pages = 3
+    src_idxs = [2, 2]
+    dst_idxs = [1, 0]
+
+    def transfer_routine_sender():
+        blocks_np = np.array(
+            [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        )
+        blocks = Tensor.from_numpy(blocks_np).to(device)
+
+        # Create engine
+        engine = KVTransferEngine(
+            "engine_1", blocks, total_num_pages, listen_port=get_unique_port()
+        )
+
+        # Connect with peer
+        sender_md_queue.put(engine.metadata)
+        remote_md = receiver_md_queue.get()
+        engine.connect(remote_md)
+
+        # Perform transfer
+        xfer_req = engine.initiate_send_xfer(remote_md, src_idxs, dst_idxs)
+        xfer_queue.put(xfer_req)
+        engine.send_xfer_sync(xfer_req)
+
+        # Verify results
+        expected_blocks = np.array(
+            [10, 11, 12, 13, 14, 15, 16, 17, 18],
+        )
+        assert np.array_equal(blocks.to_numpy(), expected_blocks)
+
+        # Release resources is skipped since it causes the following error:
+        # `flush.c:58   UCX  ERROR req 0x7f274411a280: error during flush: Endpoint timeout`
+
+        # TODO(E2EOPT-299) Reenable cleanup
+        # engine.cleanup()
+
+    def transfer_routine_receiver():
+        blocks_np = np.array(
+            [80, 81, 82, 83, 84, 85, 86, 87, 88],
+        )
+        blocks = Tensor.from_numpy(blocks_np).to(device)
+
+        # Create engine
+        engine = KVTransferEngine(
+            "engine_2", blocks, total_num_pages, listen_port=get_unique_port()
+        )
+
+        # Connect with peer
+        receiver_md_queue.put(engine.metadata)
+        remote_md = sender_md_queue.get()
+        engine.connect(remote_md)
+
+        # Perform transfer
+        xfer_req = xfer_queue.get()
+        engine.recv_xfer_sync(xfer_req)
+
+        # Verify results
+        expected_blocks = np.array(
+            [16, 17, 18, 16, 17, 18, 86, 87, 88],
+        )
+        assert np.array_equal(blocks.to_numpy(), expected_blocks)
+
+        # Release resources is skipped since it causes the following error:
+        # `flush.c:58   UCX  ERROR req 0x7f274411a280: error during flush: Endpoint timeout`
+
+        # TODO(E2EOPT-299) Reenable cleanup
+        # engine.cleanup()
+
+    thread_1 = Thread(target=transfer_routine_sender)
+    thread_2 = Thread(target=transfer_routine_receiver)
+
+    thread_1.start()
+    thread_2.start()
+
+    thread_1.join()
+    thread_2.join()
