c2c5765b914a489bd3e53faff245d6f18b391bca
Spenser Bauman <spenser@modular.com>
2025-06-05 18:22:39 -0400

    [SDK] Remove deadline for rms_norm pipeline tests (#63133)
    The testpoints under this directory timeout for me semi-consistently in
    CI recently. The only recourse for that is to rerun the test until it
    passes.
    
    This change drops the deadline from the the norm related tests points,
    similar to what we do for the core Graph API tests.
    
    Closes: MODELS-589
    
    ---------
    
    Co-authored-by: Joe Loser <joe@modular.com>
    MAX_INTEGRATION_TESTS_REV_ID: a7db7bc0a77746b7e59e2d79179415c6ee41b171

--- max/tests/integration/pipelines/python/nn/norm/conftest.py
+++ max/tests/integration/pipelines/python/nn/norm/conftest.py
@@ -5,8 +5,17 @@
 # ===----------------------------------------------------------------------=== #
 
 import pytest
+from hypothesis import settings
 from max.driver import Accelerator
 from max.engine import InferenceSession
+
+# When running in CI, graph tests can take around 300ms for a single run.
+# These seem to be due to CI running under very high cpu usage.
+# A similar effect can be achieved locally be running with each test multiple times `--runs_per_test=3`.
+# They all launch at the same time leading to exceptionally heavy cpu usage.
+# We have reasonable test suite timeouts. Use those instead of hypothesis deadlines.
+settings.register_profile("norm_tests", deadline=None)
+settings.load_profile("norm_tests")
 
 
 @pytest.fixture(scope="session")
