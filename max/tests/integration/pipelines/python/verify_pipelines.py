563bc4ea9e7e009a8e78f0f4a456f989ea0d117f
Brendan Duke <bduke@modular.com>
2025-06-11 13:52:57 -0400

    [CI] Explain meaning of KL div in verify summary (#63481)
    Explain the meaning of the "KL Div" column in the summary for logits
    verification runs.
    
    MAX_INTEGRATION_TESTS_REV_ID: f95b7af3f52674db7a298fcc909172a0f4358e00

--- max/tests/integration/pipelines/python/verify_pipelines.py
+++ max/tests/integration/pipelines/python/verify_pipelines.py
@@ -135,6 +135,9 @@ def dump_results(
 
     if any_logit:
         to.write("\n\n## LLMs\n")
+        to.write(
+            "NOTE: KL Div here is the average over each prompt. _Not_ the max used for pass/fail checks."
+        )
         to.write("| Status | Model | KL Div |\n")
         to.write("|:------:|:---------|:------:|\n")
 
