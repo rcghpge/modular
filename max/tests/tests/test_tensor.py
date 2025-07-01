004e105add459703c2343bf5a8c66f538be352b5
Stef Lindall <stef@modular.com>
2025-06-30 18:57:34 -0700

    [SDK] Experimental: MAX Tensor (#63555)
    Based on the [Single graph + copy on
    [Internal link]
    design proposal.
    
    - Adds a `max.experimental` namespace
    - Adds `max.experimental.tensor.Tensor` basic implementation
    - Adds `max.experimental.tensor.functional` which can wrap `ops` from
    the graph ops library to operate on `Tensor`s
    - Implements `Tensor.__add__` using `functional(ops.add)`
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 1a1592f9291890ef09050869ee4c6c5f47497980

---
+++ max/tests/tests/test_tensor.py
@@ -0,0 +1,74 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Tests `max.Tensor` basic behaviors."""
+
+import asyncio
+
+import pytest
+from max.driver import CPU, Accelerator, accelerator_count
+from max.driver import Tensor as DriverTensor
+from max.dtype import DType
+from max.experimental.tensor import Tensor, TensorType
+from max.graph import DeviceRef
+
+DEVICE = Accelerator() if accelerator_count() else CPU()
+
+
+def test_tensor_basic():
+    expected_type = TensorType(
+        DType.float32, [5, 5], DeviceRef.from_device(DEVICE)
+    )
+    a_data = DriverTensor.zeros([5, 5], DType.float32, DEVICE)
+    a = Tensor(storage=a_data)
+    assert a.type == expected_type
+    assert a.driver_tensor.to(CPU())[0, 0].item() == 0.0
+    b = a + 1
+    assert isinstance(b, Tensor)
+    assert not b.real
+    assert b.type == a.type == expected_type
+
+    asyncio.run(b.realize)
+    assert b.real
+    assert b.driver_tensor.to(CPU())[0, 0].item() == 1.0
+
+
+def test_tensor_with_intermediate():
+    expected_type = TensorType(
+        DType.float32, [5, 5], DeviceRef.from_device(DEVICE)
+    )
+    a_data = DriverTensor.zeros([5, 5], DType.float32, DEVICE)
+    a = Tensor(storage=a_data)
+    assert a.type == expected_type
+    assert a.driver_tensor.to(CPU())[0, 0].item() == 0.0
+    b = a + a + 1
+    assert isinstance(b, Tensor)
+    assert not b.real
+    assert b.type == a.type == expected_type
+
+    asyncio.run(b.realize)
+    assert b.real
+    assert b.driver_tensor.to(CPU())[0, 0].item() == 1.0
+
+
+def test_compilation_failure():
+    a_data = DriverTensor.zeros([5, 5], DType.float8_e4m3fn, CPU())
+    b_data = DriverTensor.zeros([5, 5], DType.float32, CPU())
+    a = Tensor(storage=a_data)
+    a_plus_1 = a + 1
+    assert isinstance(a_plus_1, Tensor)
+
+    with pytest.raises(Exception):
+        asyncio.run(a_plus_1.realize)
+
+    del a, a_plus_1
+
+    b = Tensor(storage=b_data)
+    # TODO(MAXPLAT-317): We should be pruning these but we're not.
+    # c = b + 1
+    # asyncio.run(c.realize)
+    # assert c.real
+
+    _ = b + 1
