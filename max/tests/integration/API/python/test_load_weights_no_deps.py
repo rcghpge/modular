5769512bf64bc4e7cd45b1398319e913ebd43670
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-07-15 14:59:13 -0400

    [SDK] Don't import weight loaders if their dependency isn't available (#65330)
    MAX_INTEGRATION_TESTS_REV_ID: 90a69634ad1964b3ae99aef062caad2ecba1f6e3

---
+++ max/tests/integration/API/python/test_load_weights_no_deps.py
@@ -0,0 +1,37 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Test to make sure we get a proper error message when we try to load a weight file from a ."""
+
+from os import getenv
+from pathlib import Path
+
+import pytest
+from max.graph.weights import GGUFWeights, PytorchWeights
+
+
+@pytest.fixture
+def graph_testdata() -> Path:
+    """Returns the path to the Modular .derived directory."""
+    path = getenv("GRAPH_TESTDATA")
+    assert path is not None
+    return Path(path)
+
+
+def test_load_gguf(graph_testdata) -> None:
+    """Tests adding an external weight to a graph."""
+
+    with pytest.raises(ImportError) as info:
+        weights = GGUFWeights(graph_testdata / "example_data.gguf")
+    # Also test the error message, we want to make sure we hit our custom error message
+    assert str(info.value) == "Unable to load gguf file, gguf not installed"
+
+
+def test_load_pytorch(graph_testdata) -> None:
+    """Tests adding an external weight to a graph."""
+
+    with pytest.raises(ImportError) as info:
+        weights = PytorchWeights(graph_testdata / "example_data.pt")
+    assert str(info.value) == "Unable to load pytorch file, torch not installed"
