7a61f8a208c3ef20df8822edbc4d5114a208ac4e
Brendan Duke <bduke@modular.com>
2025-07-16 07:51:34 -0400

    [Evals] Add InternVL3 14B to dataset eval scripts (#65469)
    Add a dataset eval script for InternVL3 14B.
    This was useful for generating an accuracy report for this model.
    
    ---------
    
    Co-authored-by: akirchhoff-modular <akirchhoff@modular.com>
    MAX_INTEGRATION_TESTS_REV_ID: e38cf75708ca6a75fa0886c737250129b21846e1

---
+++ max/tests/integration/pipelines/python/dataset_eval_configs/OpenGVLab/InternVL3-14B-Instruct.sh
@@ -0,0 +1,21 @@
+##===----------------------------------------------------------------------===##
+#
+# This file is Modular Inc proprietary.
+#
+##===----------------------------------------------------------------------===##
+
+batch_size=512
+max_length=8192
+
+evaluator=mistral-evals
+tasks=mathvista,chartqa,docvqa
+
+# NOTE: Change `max_dynamic_patch` here to sweep the number of patches.
+# This affects the number of input vision tokens.
+extra_pipelines_args=(
+  --trust-remote-code
+  --device-memory-utilization 0.8
+  --no-enable-prefix-caching
+  --no-enable-chunked-prefill
+  --vision-config-overrides '{"max_dynamic_patch": 12}'
+)
