108b212483555113ab1e8aba51422474beea2088
Brian Zhang <bez@modular.com>
2025-07-23 18:30:32 -0400

    [python-bindings][KVCache] Add benchmark for mojo block hasher (#64279)
    Compares with python hash fn, hashlib.blake2b ("tensor hash"), and
    xxhash.xxh64 (todo)
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 46d33de9d803fe2a531e718cbaa3bd7f2e7535c4

--- max/tests/tests/_core_mojo/test_block_hasher.py
+++ max/tests/tests/_core_mojo/test_block_hasher.py
@@ -5,8 +5,11 @@
 # ===----------------------------------------------------------------------=== #
 
 
+import hashlib
+
 import numpy as np
 from max._core_mojo import block_hasher
+from pytest_benchmark.fixture import BenchmarkFixture
 
 
 def test_block_hasher() -> None:
@@ -30,3 +33,83 @@ def test_block_hasher() -> None:
     for h in hashes:
         assert h not in seen
         seen.add(h)
+
+
+def mojo_block_hasher(tokens: np.ndarray, block_size: int) -> list[int]:
+    return block_hasher(tokens, block_size, hash("None"))
+
+
+def tensor_block_hasher(tokens: np.ndarray, block_size: int) -> list[int]:
+    num_elts = tokens.size
+    num_hashes = num_elts // block_size
+
+    # Initial hash seed value
+    prev_hash = hash("None")
+
+    results = []
+    for i in range(num_hashes):
+        block = tokens[i * block_size : (i + 1) * block_size]
+        digest = hashlib.blake2b(block.tobytes()).hexdigest()
+        pair_to_hash = (prev_hash, digest)
+        curr_hash = hash(pair_to_hash)
+
+        results.append(curr_hash)
+
+    return results
+
+
+def naive_block_hasher(tokens: np.ndarray, block_size: int) -> list[int]:
+    num_elts = tokens.size
+    num_hashes = num_elts // block_size
+
+    # Initial hash seed value
+    prev_hash = hash("None")
+
+    results = []
+    for i in range(num_hashes):
+        block = tokens[i * block_size : (i + 1) * block_size]
+        pair_to_hash = (prev_hash, tuple(block))
+        curr_hash = hash(pair_to_hash)
+        results.append(curr_hash)
+        prev_hash = curr_hash
+
+    return results
+
+
+def test_benchmark_mojo(benchmark: BenchmarkFixture) -> None:
+    block_size = 128
+    tokens = np.arange(30000)
+
+    _ = benchmark.pedantic(
+        mojo_block_hasher,
+        args=(tokens, block_size),
+        warmup_rounds=1,
+        rounds=3,
+        iterations=10,
+    )
+
+
+def test_benchmark_tensor(benchmark: BenchmarkFixture) -> None:
+    block_size = 128
+    tokens = np.arange(30000)
+
+    _ = benchmark.pedantic(
+        tensor_block_hasher,
+        args=(tokens, block_size),
+        warmup_rounds=1,
+        rounds=3,
+        iterations=10,
+    )
+
+
+def test_benchmark_naive(benchmark: BenchmarkFixture) -> None:
+    block_size = 128
+    tokens = np.arange(30000)
+
+    _ = benchmark.pedantic(
+        naive_block_hasher,
+        args=(tokens, block_size),
+        warmup_rounds=1,
+        rounds=3,
+        iterations=10,
+    )
