6a669f64b070f6c62124b59168e8af69a546f62b
Brendan Hansknecht <bhansconnect@modular.com>
2025-08-06 22:01:29 +0000

    [perf][NFC] cleanup internvl dataset eval configs (#66803)
    Just making params consistent.
    
    MAX_INTEGRATION_TESTS_REV_ID: 9cddbf3e4c929adcf95e48bdb9742c0363183f9e

--- max/tests/integration/pipelines/python/dataset_eval_configs/OpenGVLab/InternVL3-8B-Instruct.sh
+++ max/tests/integration/pipelines/python/dataset_eval_configs/OpenGVLab/InternVL3-8B-Instruct.sh
@@ -4,10 +4,19 @@
 #
 ##===----------------------------------------------------------------------===##
 
-batch_size=1
+batch_size=512
 max_length=8192
+
 evaluator=mistral-evals
 tasks=mathvista,chartqa,docvqa
 
-extra_pipelines_args=(--trust-remote-code)
+# NOTE: Change `max_dynamic_patch` here to sweep the number of patches.
+# This affects the number of input vision tokens.
+extra_pipelines_args=(
+  --trust-remote-code
+  --device-memory-utilization 0.8
+  --no-enable-prefix-caching
+  --no-enable-chunked-prefill
+  --vision-config-overrides '{"max_dynamic_patch": 12}'
+)
 
