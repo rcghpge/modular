26223fe6c5513270f7d8e9c567fd4f1d6ce9982d
akirchhoff-modular <akirchhoff@modular.com>
2025-08-07 14:16:25 -0700

    [Serve] Tighten up type safety in OpenAI router
    Fixes latent type errors hidden by schema typechecking being disabled.
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 248e2328bce425e1c2f7d8560584435468739854

--- max/tests/tests/serve/test_routes.py
+++ max/tests/tests/serve/test_routes.py
@@ -14,7 +14,7 @@ from max.serve.pipelines.echo_gen import (
     EchoTokenGenerator,
 )
 from max.serve.router import openai_routes
-from max.serve.schemas.openai import CreateCompletionRequest  # type: ignore
+from max.serve.schemas.openai import InputItem, PromptItem  # type: ignore
 
 
 class MockPipelineConfig(PipelineConfig):
@@ -53,57 +53,49 @@ def test_health_endpoint_exists(app) -> None:  # noqa: ANN001
 
 
 def test_prompts() -> None:
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt="Why is the sky blue?",
-    )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
+    prompts = openai_routes.get_prompts_from_openai_request(
+        "Why is the sky blue?"
     )
     assert len(prompts) == 1
 
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt=["Why is the sky blue?", "what time is it?"],
-    )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
+    prompts = openai_routes.get_prompts_from_openai_request(
+        ["Why is the sky blue?", "what time is it?"]
     )
     assert len(prompts) == 2
 
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt=[[1, 2, 3]],
-    )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
-    )
+    prompts = openai_routes.get_prompts_from_openai_request([[1, 2, 3]])
     assert len(prompts) == 1
 
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt=[1, 2, 3],
-    )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
-    )
+    prompts = openai_routes.get_prompts_from_openai_request([1, 2, 3])
     assert len(prompts) == 1
 
     # prompt item
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt=[[1, 2, 3]],
+    prompts = openai_routes.get_prompts_from_openai_request([[1, 2, 3]])
+    assert len(prompts) == 1
+
+    prompts = openai_routes.get_prompts_from_openai_request(
+        [[1, 2, 3], [4, 5, 6]]
     )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
+    assert len(prompts) == 2
+
+    # prompt item (explicit)
+    prompts = openai_routes.get_prompts_from_openai_request(
+        [PromptItem(root=[1, 2, 3])]
     )
     assert len(prompts) == 1
 
-    completion_req = CreateCompletionRequest(
-        model="whatev",
-        prompt=[[1, 2, 3], [4, 5, 6]],
+    prompts = openai_routes.get_prompts_from_openai_request(
+        [PromptItem(root=[1, 2, 3]), PromptItem(root=[4, 5, 6])]
     )
-    prompts = openai_routes.openai_get_prompts_from_completion_request(
-        completion_req
+    assert len(prompts) == 2
+
+    # input item (explicit)
+    prompts = openai_routes.get_prompts_from_openai_request(
+        [InputItem(root=[1, 2, 3])]
+    )
+    assert len(prompts) == 1
+
+    prompts = openai_routes.get_prompts_from_openai_request(
+        [InputItem(root=[1, 2, 3]), InputItem(root=[4, 5, 6])]
     )
     assert len(prompts) == 2
