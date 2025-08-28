22d108a505ca03cf64b6405dfb9927becf1535d6
Stef Lindall <stef@modular.com>
2025-08-28 08:39:29 -0700

    [SDK] max.nn.module_v3.Module
    The core unit of composition for modeling in MAX.
    
    Informally, a Module is a container class. It can contain other Module
    instances, Tensors (the Module's "local parameters") or other arbitrary
    Python data.
    
    A Module also has a `__call__` which applies that Module to some input.
    In the simplest case this is a function from one Tensor to another
    Tensor.
    
    Formally Modules form a tree, and subtrees of Modules can be manipulate
    directly. A Module may also be thought of as a closure, where the
    parameters form the data of the closure and `__call__` is the
    application of the closure.
    
    Terms:
    - A "child" of a Module is sub-Module stored directly on that Module.
    - A "descendent" of a Module is one of its children, or one of their
    descendents.
    - A "parameter" is a Tensor storing data on the Module or one of its
    descendents.
    - The "qualified path" of a descendent is a period-separated string of
    the names of the child module attributes which lead to that descendent
    module, for instance `child.sub.last`.
    - The "qualified path" of a parameter is the qualified path of the
    descendent directly holding that parameter, followed by a final path
    component for the attribute name of the tensor. For instance `weight`
    for a local parameter, or `child.sub.last.weight` for a descendent's
    parameter.
    
    ```python
    >>> from max.experimental.tensor import Tensor
    >>> from max.nn.module_v3 import Module, module_dataclass
    
    >>> @module_dataclass
    ... class Linear(Module):
    ...     weight: Tensor
    ...     bias: Tensor | int = 0
    ...
    ...     def __call__(self, x: Tensor) -> Tensor:
    ...     return x @ self.weight.T + self.bias
    
    >>> linear = Linear(Tensor.zeros([5, 4]))
    >>> print(linear)
    Linear(
      weight=Tensor(shape=[Dim(5), Dim(4)], dtype=DType.bfloat16, device=Device())
    )
    >>> print(linear(Tensor.constant([1, 2, 3, 4])))
    [0., 0., 0., 0., 0.]
    ```
    
    MAX_INTEGRATION_TESTS_REV_ID: aff34e02e163ba789ac87b7018f6294b32e330b0

---
+++ max/tests/integration/API/python/nn/module_v3/test_module.py
@@ -0,0 +1,135 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Tests for max.nn.Module."""
+
+from __future__ import annotations
+
+import pytest
+from max.experimental.tensor import Tensor
+from max.nn.module_v3.module import Module, module_dataclass
+
+
+@module_dataclass
+class SubModule(Module):
+    b: Tensor
+    eps: float = 1e-5
+
+    def __call__(self, x: Tensor) -> Tensor:
+        return x + self.b
+
+
+@module_dataclass
+class TestModule(Module):
+    a: Tensor
+    sub: SubModule
+
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.sub(x) + self.a
+
+
+@module_dataclass
+class SuperModule(Module):
+    mod: TestModule
+
+
+@pytest.fixture
+def test_module():
+    return TestModule(a=Tensor.constant(1), sub=SubModule(b=Tensor.constant(2)))
+
+
+@pytest.fixture
+def super_module(test_module: TestModule):
+    return SuperModule(mod=test_module)
+
+
+def test_module_dataclass():
+    @module_dataclass
+    class Test(Module):
+        a: int
+        b: int = 0
+
+    assert repr(Test(2)) == "Test(a=2)"
+    assert repr(Test(1, 3)) == "Test(a=1, b=3)"
+
+
+def test_module_repr(test_module: TestModule):
+    assert "TestModule" in repr(test_module)
+    assert "SubModule" in repr(test_module)
+    assert "a=Tensor" in repr(test_module)
+    assert "b=Tensor" in repr(test_module)
+    # eps is the default value, shouldn't be present
+    assert "eps=" not in repr(test_module)
+
+    sub = SubModule(b=Tensor.constant(2), eps=1e-6)
+
+    assert "SubModule" in repr(sub)
+    assert "b=Tensor" in repr(sub)
+    assert "eps=" in repr(sub)
+
+
+def test_module_custom_repr():
+    class Linear(Module):
+        weight: Tensor
+        bias: Tensor | int
+
+        def __init__(self, in_dim: int, out_dim: int, bias: bool = True):
+            self.weight = Tensor.zeros([out_dim, in_dim])
+            self.bias = Tensor.zeros([out_dim]) if bias else 0
+
+        def __rich_repr__(self):
+            out_dim, in_dim = self.weight.shape
+            bias = isinstance(self.bias, Tensor)
+            yield "in_dim", in_dim
+            yield "out_dim", out_dim
+            yield "bias", bias, True
+
+    l1 = Linear(2, 2)
+    assert repr(l1) == "Linear(in_dim=Dim(2), out_dim=Dim(2))"
+
+    l2 = Linear(3, 1, bias=False)
+    assert repr(l2) == "Linear(in_dim=Dim(3), out_dim=Dim(1), bias=False)"
+
+
+def test_module_decomposition(test_module: TestModule):
+    test_module_2 = TestModule(a=Tensor.constant(1), sub=test_module.sub)
+    assert test_module_2.sub is test_module.sub
+    assert dict(test_module_2.children) == dict(test_module.children)
+
+
+def test_module_decomposition_call(test_module: TestModule):
+    x = Tensor.constant(1)
+    assert test_module.sub.b.item() == 2
+    assert test_module.sub(x).item() == 3
+
+
+def test_module_local_parameters(test_module: TestModule):
+    assert dict(test_module.local_parameters) == {"a": test_module.a}
+    assert dict(test_module.sub.local_parameters) == {"b": test_module.sub.b}
+
+
+def test_module_parameters(test_module: TestModule):
+    assert dict(test_module.parameters) == {
+        "a": test_module.a,
+        "sub.b": test_module.sub.b,
+    }
+
+    assert dict(test_module.sub.parameters) == {"b": test_module.sub.b}
+
+
+def test_module_children(test_module: TestModule, super_module: SuperModule):
+    assert dict(super_module.children) == {"mod": test_module}
+    assert dict(test_module.children) == {"sub": test_module.sub}
+    assert dict(test_module.sub.children) == {}
+
+
+def test_module_descendents(test_module: TestModule, super_module: SuperModule):
+    assert super_module.mod is test_module
+    assert dict(super_module.descendents) == {
+        "mod": test_module,
+        "mod.sub": test_module.sub,
+    }
+    assert dict(super_module.mod.descendents) == {"sub": super_module.mod.sub}
+    assert dict(test_module.sub.descendents) == {}
