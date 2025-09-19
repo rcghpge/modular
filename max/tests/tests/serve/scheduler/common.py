e40aa0e3cdde7a6cdfa78cc97ce3bd672f94d6c3
Brian Zhang <bez@modular.com>
2025-09-19 14:04:02 -0700

    [Scheduler] Make most scheduler tests runnable w/out GPU + cleanup bazel configs
    KVCache offloading to host test is the only test that requires GPU now
    (I set limit to 1GiB).
    
    Most of this PR is code movement. I did not change anything meaningful.
    
    test_paged_scheduler -> {common, test_paged_scheduler,
    test_paged_scheduler_gpu}
    
    In a followup PR, I will create test_di which also uses common
    
    MAX_TESTS_TESTS_ORIG_REV_ID: a66bd390f419a2fe1e2c8221ae4b9ec08c832566

---
+++ max/tests/tests/serve/scheduler/common.py
@@ -0,0 +1,338 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from __future__ import annotations
+
+import queue
+from dataclasses import dataclass
+
+import numpy as np
+from max.driver import CPU, Device
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.interfaces import (
+    GenerationStatus,
+    MAXPushQueue,
+    Pipeline,
+    RequestID,
+    SchedulerResult,
+    TextGenerationInputs,
+    TextGenerationOutput,
+)
+from max.nn.kv_cache import KVCacheParams, KVCacheStrategy, PagedKVCacheManager
+from max.pipelines.core import TextContext
+from max.serve.scheduler.text_batch_constructor import (
+    BatchType,
+    TokenGenerationSchedulerConfig,
+)
+from max.serve.scheduler.text_generation_scheduler import (
+    TokenGenerationScheduler,
+)
+
+
+def rand(length: int) -> np.ndarray:
+    return np.random.randint(0, 256, size=length)
+
+
+def create_text_context(
+    prompt_len: int,
+    max_seq_len: int,
+    shared_prefix: np.ndarray | None = None,
+) -> TextContext:
+    if shared_prefix is None:
+        tokens = np.ones(prompt_len, dtype=np.int32)
+    else:
+        rem_tokens = prompt_len - len(shared_prefix)
+        assert rem_tokens >= 0
+        tokens = np.concatenate([shared_prefix, rand(rem_tokens)])
+
+    return TextContext(
+        max_length=max_seq_len,
+        tokens=tokens,
+    )
+
+
+def create_paged_manager(
+    num_blocks: int,
+    max_batch_size: int,
+    max_seq_len: int,
+    page_size: int,
+    enable_prefix_caching: bool = False,
+    enable_kvcache_swapping_to_host: bool = False,
+    device: Device = CPU(),
+) -> PagedKVCacheManager[TextContext]:
+    # Setting kv_heads, head_dim, and num_layers to 1 so it is easy to compute
+    # memory usage. Now we know each block is 1 byte.
+    NUM_KV_HEADS = 1
+    HEAD_DIM = 1
+    NUM_LAYERS = 1
+
+    dtype = DType.float32
+
+    cache_memory = (
+        2
+        * NUM_LAYERS
+        * NUM_KV_HEADS
+        * HEAD_DIM
+        * page_size
+        * num_blocks
+        * dtype.size_in_bytes
+    )
+
+    # CPU swap space is 100x the device cache memory
+    GiB = 1024 * 1024 * 1024
+    host_kvcache_swap_space_gb = 100 * cache_memory / GiB
+
+    kv_params = KVCacheParams(
+        dtype=dtype,
+        n_kv_heads=NUM_KV_HEADS,
+        head_dim=HEAD_DIM,
+        cache_strategy=KVCacheStrategy.PAGED,
+        page_size=page_size,
+        enable_prefix_caching=enable_prefix_caching,
+        enable_kvcache_swapping_to_host=enable_kvcache_swapping_to_host,
+        host_kvcache_swap_space_gb=host_kvcache_swap_space_gb,
+    )
+
+    session = InferenceSession(devices=[device])
+
+    kv_manager = PagedKVCacheManager[TextContext](
+        params=kv_params,
+        max_batch_size=max_batch_size,
+        max_seq_len=max_seq_len,
+        num_layers=NUM_LAYERS,
+        devices=[device],
+        session=session,
+        cache_memory=cache_memory,
+        page_size=page_size,
+        enable_runtime_checks=True,
+    )
+
+    assert kv_manager.total_num_pages == num_blocks
+    return kv_manager
+
+
+def create_paged_scheduler(
+    max_seq_len: int = 2048,
+    num_blocks: int = 9999,
+    max_batch_size: int = 512,
+    page_size: int = 128,
+    max_forward_steps_tg: int = 10,
+    target_tokens_per_batch_ce: int = 8192,
+    enable_prefix_caching: bool = False,
+    enable_in_flight_batching: bool = False,
+    enable_chunked_prefill: bool = True,
+    enable_kvcache_swapping_to_host: bool = False,
+    device: Device = CPU(),
+) -> tuple[
+    TokenGenerationScheduler,
+    MAXPushQueue[tuple[RequestID, TextContext]],
+]:
+    # Create a paged manager that has one slot
+    paged_manager = create_paged_manager(
+        num_blocks=num_blocks,
+        max_batch_size=max_batch_size,
+        max_seq_len=max_seq_len,
+        page_size=page_size,
+        enable_prefix_caching=enable_prefix_caching,
+        enable_kvcache_swapping_to_host=enable_kvcache_swapping_to_host,
+        device=device,
+    )
+
+    # Create a scheduler with a paged manager
+    scheduler_config = TokenGenerationSchedulerConfig(
+        max_batch_size_tg=max_batch_size,
+        max_forward_steps_tg=max_forward_steps_tg,
+        max_batch_size_ce=max_batch_size,
+        target_tokens_per_batch_ce=target_tokens_per_batch_ce,
+        enable_chunked_prefill=enable_chunked_prefill,
+        enable_in_flight_batching=enable_in_flight_batching,
+    )
+    token_pipeline = FakeTokenGeneratorPipeline(paged_manager)
+    request_queue: queue.Queue[tuple[RequestID, TextContext]] = queue.Queue()
+    response_queue: queue.Queue[
+        dict[RequestID, SchedulerResult[TextGenerationOutput]]
+    ] = queue.Queue()
+    cancel_queue: queue.Queue[list[RequestID]] = queue.Queue()
+    scheduler = TokenGenerationScheduler(
+        scheduler_config=scheduler_config,
+        pipeline=token_pipeline,
+        paged_manager=paged_manager,
+        request_queue=request_queue,
+        response_queue=response_queue,
+        cancel_queue=cancel_queue,
+    )
+
+    return (scheduler, request_queue)
+
+
+class FakeTokenGeneratorPipeline(
+    Pipeline[TextGenerationInputs[TextContext], TextGenerationOutput]
+):
+    def __init__(self, kv_manager: PagedKVCacheManager) -> None:
+        self.kv_manager = kv_manager
+
+    def execute(
+        self, inputs: TextGenerationInputs[TextContext]
+    ) -> dict[RequestID, TextGenerationOutput]:
+        max_seq_len = self.kv_manager.max_seq_len
+        # Truncate num steps based on the max seq len
+        for context in inputs.batch.values():
+            num_available_steps = context.compute_num_available_steps(
+                max_seq_len
+            )
+            assert num_available_steps > 0
+            num_steps = min(inputs.num_steps, num_available_steps)
+
+        # Claim cache rows for context.
+        for _, context in inputs.batch.items():
+            if not self.kv_manager.contains(context.request_id):
+                self.kv_manager.external_claim(context.request_id)
+
+        ctxs: list[TextContext] = list(inputs.batch.values())
+
+        self.kv_manager.fetch(ctxs, num_steps=num_steps)
+
+        # Generate the responses
+        responses = {}
+        for req_id, context in inputs.batch.items():
+            for _ in range(num_steps):
+                context.update(new_token=rand(1)[0])
+
+                if context.current_length == context.max_length:
+                    context.status = GenerationStatus.MAXIMUM_LENGTH
+
+                if context.is_done:
+                    break
+
+            responses[req_id] = context.to_generation_output()
+
+        # Step the kv cache manager
+        self.kv_manager.step(ctxs)
+
+        return responses
+
+    def release(self, request_id: RequestID) -> None:
+        self.kv_manager.release(request_id)
+
+
+@dataclass
+class BatchInfo:
+    batch_type: BatchType
+    batch_size: int
+    terminated: int
+    num_steps: int
+    input_tokens: int
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BatchInfo):
+            return False
+        # All empty batches are equivalent
+        if self.batch_size == 0 and other.batch_size == 0:
+            return True
+        return (
+            self.batch_type,
+            self.batch_size,
+            self.terminated,
+            self.num_steps,
+            self.input_tokens,
+        ) == (
+            other.batch_type,
+            other.batch_size,
+            other.terminated,
+            other.num_steps,
+            other.input_tokens,
+        )
+
+    @classmethod
+    def empty(cls) -> BatchInfo:
+        return BatchInfo(BatchType.TG, 0, 0, 0, 0)
+
+    def __repr__(self) -> str:
+        return (
+            f"BatchInfo("
+            f"{self.batch_type.value}, "
+            f"{self.batch_size}, "
+            f"{self.terminated}, "
+            f"{self.num_steps}, "
+            f"{self.input_tokens})"
+        )
+
+
+def create_batch_and_execute(
+    scheduler: TokenGenerationScheduler,
+) -> BatchInfo:
+    scheduler._retrieve_pending_requests()
+    batch_to_execute = scheduler.batch_constructor.construct_batch()
+    batch_size = batch_to_execute.batch_size
+    batch_type = batch_to_execute.batch_type
+    input_tokens = batch_to_execute.input_tokens
+    num_steps = batch_to_execute.inputs.num_steps
+    if batch_to_execute.batch_size == 0:
+        return BatchInfo.empty()
+
+    scheduler._schedule(batch_to_execute)
+    terminated_reqs = batch_to_execute.num_terminated
+
+    assert isinstance(scheduler.pipeline, FakeTokenGeneratorPipeline)
+
+    return BatchInfo(
+        batch_type=batch_type,
+        batch_size=batch_size,
+        terminated=terminated_reqs,
+        num_steps=num_steps,
+        input_tokens=input_tokens,
+    )
+
+
+def run_until_completion(
+    scheduler: TokenGenerationScheduler,
+    max_num_iters: int = 50,
+    output_list: list | None = None,
+) -> list[BatchInfo]:
+    if output_list is None:
+        batch_infos = []
+    else:
+        batch_infos = output_list
+
+    for _ in range(max_num_iters):
+        batch_info = create_batch_and_execute(scheduler)
+        batch_infos.append(batch_info)
+        if batch_info.batch_size == 0:
+            break
+    return batch_infos
+
+
+def enqueue_request(
+    queue: MAXPushQueue[tuple[RequestID, TextContext]],
+    prompt_len: int,
+    max_seq_len: int,
+    shared_prefix: np.ndarray | None = None,
+) -> None:
+    context = create_text_context(
+        prompt_len=prompt_len,
+        max_seq_len=max_seq_len,
+        shared_prefix=shared_prefix,
+    )
+    assert context.active_length == prompt_len
+    queue.put_nowait((context.request_id, context))
+
+
+def enqueue_request_with_prompt(
+    queue: MAXPushQueue[tuple[RequestID, TextContext]],
+    tokens: np.ndarray,
+    max_seq_len: int,
+) -> None:
+    context = TextContext(
+        max_length=max_seq_len,
+        tokens=tokens,
+    )
+
+    queue.put_nowait((context.request_id, context))
+
+
+CE = BatchType.CE
+TG = BatchType.TG
