a3a101d5eb2700d732ebb53ba542fc069949943c
Stef Lindall <stef@modular.com>
2025-10-23 12:57:32 -0700

    [SDK] Fix random.normal with non-standard dtypes
    The distribution parameter dtypes need to match the dtype of the result.
    Added an explicit test for this case.
    
    MAX_INTEGRATION_TESTS_REV_ID: 9f71581ed104043fc02d5aebdec296a82e97ea36

--- max/tests/integration/API/python/tensor/test_random.py
+++ max/tests/integration/API/python/tensor/test_random.py
@@ -29,6 +29,19 @@ def test_normal_defaults() -> None:
         assert_all_close(t1, t2)
 
 
+def test_normal_different_dtype() -> None:
+    t1 = random.normal([20], dtype=DType.float32)
+    t2 = random.normal([20], dtype=DType.float32)
+
+    with pytest.raises(AssertionError):
+        assert_all_close(t1, t2)
+
+
+def test_normal_integral() -> None:
+    t = random.normal([20], dtype=DType.uint8)
+    _ = repr(t)
+
+
 def test_uniform() -> None:
     t1 = random.uniform([20], dtype=DType.float32, device=CPU())
     t2 = random.uniform([20], dtype=DType.float32, device=CPU())
