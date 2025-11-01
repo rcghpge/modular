eed834bd79d96b79d5bd8e02b2805a62a7a1ec28
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-11-01 13:01:45 -0400

    [bazel] Fix `./bazelw run fix-fast` to ignore deleted files
    Currently, if a file gets deleted, it gets picked up in git commands we
    run, which causes most of these checks to fall over. Just filter them
    out.
    
    Also silence an internal `cd` in the markdownlint wrapper.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7ee22fb83c953a3fee06fcf88093114b979159ed

--- bazel/lint/markdownlint-wrapper.sh
+++ bazel/lint/markdownlint-wrapper.sh
@@ -20,13 +20,13 @@ readonly config=$(find $BUILD_WORKSPACE_DIRECTORY -name .markdownlint.yaml -path
 
 if [[ -n "${FAST:-}" ]]; then
     cd "$BUILD_WORKSPACE_DIRECTORY"
-    paths=$(git diff --name-only $(git merge-base origin/main HEAD) -- '*.md' '*.mdx' ':!:third-party/*')
+    paths=$(git diff --diff-filter=d --name-only $(git merge-base origin/main HEAD) -- '*.md' '*.mdx' ':!:third-party/*')
     if [ ! -n "$paths" ]; then
         # markdownlint will just print help if no input paths, short circuit here
         exit 0
     fi
     # aspect_rules_js complains if we change directories and then run the binary, so go back
-    cd -
+    cd - >/dev/null
 else
     paths="."
 fi
