793164f6ff4093646c31255de7ad2611bec3d47f
Kyle Finn <kfinn@modular.com>
2025-11-10 10:04:44 -0500

    [max serve] switch heartbeat to threading.Event
    Use Event to avoid memory / perf overhead from Queue
    
    MAX_TESTS_TESTS_ORIG_REV_ID: 9bd64b7e8618180bfa33e87fde3e0b644f63cf0c

--- max/tests/tests/serve/unit/test_process_control.py
+++ max/tests/tests/serve/unit/test_process_control.py
@@ -9,7 +9,7 @@ import sys
 import time
 from collections.abc import Awaitable, Callable
 from multiprocessing import Process
-from queue import Queue
+from threading import Event
 from typing import NoReturn, ParamSpec, TypeVar
 
 import pytest
@@ -33,29 +33,29 @@ def async_timeout(
     return decorator
 
 
-def work(health: Queue[bool], reps: int, pause: float) -> int:
+def work(alive: Event, reps: int, pause: float) -> int:
     for _ in range(reps):
         time.sleep(pause)
-        health.put(True)
+        alive.set()
     return 123
 
 
-def run_exception(health: Queue[bool]) -> NoReturn:
-    health.put(True)
+def run_exception(alive: Event) -> NoReturn:
+    alive.set()
     raise ValueError("dead!")
 
 
-def run_exit(health: Queue[bool]) -> NoReturn:
-    health.put(True)
+def run_exit(alive: Event) -> NoReturn:
+    alive.set()
     sys.exit(1)
 
 
 @async_timeout(10)
 async def test_ready_and_finish() -> None:
     async with subprocess_manager("test1") as proc:
-        health = proc.ctx.Queue()
-        task = proc.start(work, health, reps=3, pause=0)
-        await proc.ready(lambda: health.get(timeout=5))
+        alive = proc.ctx.Event()
+        task = proc.start(work, alive, reps=3, pause=0)
+        await proc.ready(alive, timeout=5)
         processes: list[Process] = list(proc.pool._processes.values())  # type: ignore[attr-defined]
         res = await asyncio.wait_for(task, timeout=10)
         assert res == 123
@@ -68,9 +68,9 @@ async def test_ready_and_finish() -> None:
 @async_timeout(15)
 async def test_cancel() -> None:
     async with subprocess_manager("test1") as proc:
-        health = proc.ctx.Queue()
-        task = proc.start(work, health, reps=5, pause=4)
-        await proc.ready(lambda: health.get(timeout=5))
+        alive = proc.ctx.Event()
+        task = proc.start(work, alive, reps=5, pause=4)
+        await proc.ready(alive, timeout=5)
         processes: list[Process] = list(proc.pool._processes.values())  # type: ignore[attr-defined]
 
         task.cancel()
@@ -86,10 +86,10 @@ async def test_cancel() -> None:
 @async_timeout(10)
 async def test_heartbeat_good() -> None:
     async with subprocess_manager("test1") as proc:
-        health = proc.ctx.Queue()
-        task = proc.start(work, health, reps=10, pause=0.2)
-        await proc.ready(lambda: health.get(timeout=10))
-        proc.watch_heartbeat(lambda: health.get(timeout=0.5))
+        alive = proc.ctx.Event()
+        task = proc.start(work, alive, reps=10, pause=0.2)
+        await proc.ready(alive, timeout=10)
+        proc.watch_heartbeat(alive, timeout=0.5)
         res = await asyncio.wait_for(task, timeout=10)
         assert res == 123
 
@@ -98,9 +98,9 @@ async def test_heartbeat_good() -> None:
 async def test_heartbeat_bad() -> None:
     with pytest.raises(TimeoutError, match="test1 failed heartbeat check"):
         async with subprocess_manager("test1") as proc:
-            health = proc.ctx.Queue()
-            task = proc.start(work, health, reps=5, pause=4)
-            proc.watch_heartbeat(lambda: health.get(timeout=0.1))
+            alive = proc.ctx.Event()
+            task = proc.start(work, alive, reps=5, pause=4)
+            proc.watch_heartbeat(alive, timeout=0.1)
             await asyncio.wait_for(task, timeout=10)
 
 
@@ -108,33 +108,33 @@ async def test_heartbeat_bad() -> None:
 async def test_exception_propagate() -> None:
     with pytest.raises(ValueError, match="dead!"):
         async with subprocess_manager("test1") as proc:
-            health = proc.ctx.Queue()
-            task = proc.start(run_exception, health)
-            await proc.ready(lambda: health.get(timeout=10))
-            await asyncio.wait_for(task, timeout=10)
+            alive = proc.ctx.Event()
+            task = proc.start(run_exception, alive)
+            await proc.ready(alive, timeout=10)
+            await asyncio.sleep(11)
 
 
 @async_timeout(10)
 async def test_hard_exit() -> None:
     with pytest.raises(RuntimeError, match="Subprocess SystemExit"):
         async with subprocess_manager("test1") as proc:
-            health = proc.ctx.Queue()
-            task = proc.start(run_exit, health)
-            await proc.ready(lambda: health.get(timeout=10))
-            await asyncio.wait_for(task, timeout=10)
+            alive = proc.ctx.Event()
+            task = proc.start(run_exit, alive)
+            await proc.ready(alive, timeout=10)
+            await asyncio.sleep(11)
 
 
 @async_timeout(10)
 async def test_nested() -> None:
     async with subprocess_manager("test1") as proc:
-        health = proc.ctx.Queue()
-        task = proc.start(work, health, reps=3, pause=0)
-        await proc.ready(lambda: health.get(timeout=10))
+        alive = proc.ctx.Event()
+        task = proc.start(work, alive, reps=3, pause=0)
+        await proc.ready(alive, timeout=10)
 
         async with subprocess_manager("test2") as proc2:
-            health2 = proc2.ctx.Queue()
+            health2 = proc2.ctx.Event()
             task2 = proc2.start(work, health2, reps=3, pause=0)
-            await proc2.ready(lambda: health2.get(timeout=10))
+            await proc2.ready(health2, timeout=10)
             await asyncio.wait_for(task2, timeout=10)
 
         await asyncio.wait_for(task, timeout=10)
