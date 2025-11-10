055ee7a61455cdd0ae560d00eb15f6f48c21cff1
Davood Mohajerani <davood@modular.com>
2025-11-10 17:54:37 -0500

    [KBench] Force `kbench` to read CSV values as string and not print in scientific notation - Force `kbench` to read CSV values as string and not print in scientific notation
    MODULAR_ORIG_COMMIT_REV_ID: 01dff56b253cae506746ec6d442a55590c00410a

--- max/kernels/benchmarks/autotune/kbench.py
+++ max/kernels/benchmarks/autotune/kbench.py
@@ -53,6 +53,7 @@ from utils import pretty_exception_handler
 CONSOLE = Console(width=80)
 CURRENT_FILE = Path(__file__).resolve()
 LINE = "\n" + 70 * "-"
+pd.set_option("display.float_format", str)
 
 
 def store_pickle(path: Path | str, data: Any) -> None:
