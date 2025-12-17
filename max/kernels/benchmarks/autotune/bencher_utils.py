ba5eb6b854b004ca32d91c70407251403a15d5ca
Davood Mohajerani <davood@modular.com>
2025-12-17 12:28:18 -0500

    [Benchmarks][Kbench] Refactor `bencher_utils:dump_report` to match Mojo Bencher format - Refactor `bencher_utils:dump_report` to match Mojo Bencher format
    MODULAR_ORIG_COMMIT_REV_ID: c75075607d9608f0f1470d002929e1aff10541da

--- max/kernels/benchmarks/autotune/bencher_utils.py
+++ max/kernels/benchmarks/autotune/bencher_utils.py
@@ -11,6 +11,7 @@
 # limitations under the License.
 # ===----------------------------------------------------------------------=== #
 
+import csv
 import sys
 from dataclasses import dataclass, field
 from pathlib import Path
@@ -51,9 +52,19 @@ class ThroughputMeasure:
 
 @dataclass
 class Bench:
+    """Constructs a Benchmark object, used for running multiple benchmarks
+    and comparing the results.
+
+    Args:
+        name: Name of benchmark entry (string).
+        met: Measured execution time for the entry in seconds (float).
+        iters: Number of iterations in the measurement (int).
+        metric_list: List of ThroughputMeasure's.
+    """
+
     name: str
-    iters: int
     met: float
+    iters: int
 
     metric_list: list[ThroughputMeasure] = field(default_factory=list)
 
@@ -63,27 +74,25 @@ class Bench:
     theoretical_flops = BenchMetric(3, "TheoreticalArithmetic", "GFLOPS/s")
 
     BENCH_LABEL = "name"
-    ITERS_LABEL = "iters"
     MET_LABEL = "met (ms)"
+    ITERS_LABEL = "iters"
 
-    def dump_report(self, output_path: Path) -> None:
-        output: list[str] = []
-
-        metrics = [
+    def dump_report(self, output_path: Path | None = None) -> None:
+        metrics = [self.BENCH_LABEL, self.MET_LABEL, self.ITERS_LABEL] + [
             f"{m.metric.name} ({m.metric.unit})" for m in self.metric_list
         ]
-        s = [self.BENCH_LABEL, self.ITERS_LABEL, self.MET_LABEL] + metrics
-        output += [",".join(s)]
+        vals = ['"' + self.name + '"', self.met, self.iters] + [
+            f"{m.compute(self.met)}" for m in self.metric_list
+        ]
+        rows = (metrics, vals)
 
-        metric_vals = [f"{m.compute(self.met)}" for m in self.metric_list]
-        vals = [self.name, self.iters, self.met] + metric_vals
-        output += [",".join([str(v) for v in vals])]
-
-        output_str = "\n".join(output)
-        with open(output_path, "w") as f:
-            f.write(output_str + "\n")
-
-        print(output_str)
+        if output_path:
+            with open(output_path, "w") as f:
+                w = csv.writer(f, delimiter=",", quotechar="'")
+                w.writerows(rows)
+        with sys.stdout as f:
+            w = csv.writer(f, delimiter=",", quotechar="'")
+            w.writerows(rows)
 
 
 def arg_parse(handle: str, default: Any = None, short_handle: str = "") -> str:
