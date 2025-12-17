25931a15cce73623451a81a501192c1a10c775e6
Feras Boulala <feras.boulala@modular.com>
2025-12-17 16:26:36 -0500

    [MAX] Parallelize tests to reduce their duration
    MODULAR_ORIG_COMMIT_REV_ID: 8277215bf19779aa33cfc7eae888480377411e2b

--- max/tests/integration/API/python/tensor/test_tensor.py
+++ max/tests/integration/API/python/tensor/test_tensor.py
@@ -26,7 +26,8 @@ from max.dtype import DType
 from max.experimental import random
 from max.experimental.tensor import Tensor, default_dtype
 
-DEVICE = Accelerator() if accelerator_count() else CPU()
+# Do not do this because this test is parallelized at pytest's level. Each test should create its own accelerator.
+ACCELERATOR = Accelerator() if accelerator_count() else CPU()
 
 
 def test_ones_defaults() -> None:
@@ -36,28 +37,44 @@ def test_ones_defaults() -> None:
 
 
 def test_abs() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = abs(tensor)
     result._sync_realize()
     assert result.real
 
 
 def test_argmax() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.argmax()
     result._sync_realize()
     assert result.real
 
 
 def test_max() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.max()
     result._sync_realize()
     assert result.real
 
 
 def test_mean() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.mean()
     result._sync_realize()
     assert result.real
@@ -71,7 +88,11 @@ def test_clip() -> None:
 
 
 def test_reshape() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.reshape([6, 4])
     result._sync_realize()
     assert result.real
@@ -79,7 +100,11 @@ def test_reshape() -> None:
 
 
 def test_cast() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.cast(DType.int64)
     result._sync_realize()
     assert result.real
@@ -87,7 +112,11 @@ def test_cast() -> None:
 
 
 def test_permute() -> None:
-    tensor = Tensor.ones([2, 3, 4], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [2, 3, 4],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.permute([2, 0, 1])
     result._sync_realize()
     assert result.real
@@ -95,7 +124,11 @@ def test_permute() -> None:
 
 
 def test_transpose() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.transpose(0, 1)
     result._sync_realize()
     assert result.real
@@ -103,7 +136,11 @@ def test_transpose() -> None:
 
 
 def test_T_property() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor.T
     result._sync_realize()
     assert result.real
@@ -111,160 +148,296 @@ def test_T_property() -> None:
 
 
 def test_getitem() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = tensor[0:2, 1:4]
     result._sync_realize()
     assert result.real
 
 
 def test_neg() -> None:
-    tensor = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    tensor = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = -tensor
     result._sync_realize()
     assert result.real
 
 
 def test_eq() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a == b
     result._sync_realize()
     assert result.real
 
 
 def test_ne() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.zeros([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.zeros(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a != b
     result._sync_realize()
     assert result.real
 
 
 def test_ge() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.zeros([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.zeros(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a >= b
     result._sync_realize()
     assert result.real
 
 
 def test_gt() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.zeros([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.zeros(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a > b
     result._sync_realize()
     assert result.real
 
 
 def test_le() -> None:
-    a = Tensor.zeros([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.zeros(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a <= b
     result._sync_realize()
     assert result.real
 
 
 def test_lt() -> None:
-    a = Tensor.zeros([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.zeros(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a < b
     result._sync_realize()
     assert result.real
 
 
 def test_add() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a + b
     result._sync_realize()
     assert result.real
 
 
 def test_radd() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 + a  # triggers __radd__
     result._sync_realize()
     assert result.real
 
 
 def test_sub() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a - b
     result._sync_realize()
     assert result.real
 
 
 def test_rsub() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 - a  # triggers __rsub__
     result._sync_realize()
     assert result.real
 
 
 def test_mul() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a * b
     result._sync_realize()
     assert result.real
 
 
 def test_rmul() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 * a  # triggers __rmul__
     result._sync_realize()
     assert result.real
 
 
 def test_truediv() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a / b
     result._sync_realize()
     assert result.real
 
 
 def test_rtruediv() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 / a  # triggers __rtruediv__
     result._sync_realize()
     assert result.real
 
 
 def test_floordiv() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a // b
     result._sync_realize()
     assert result.real
 
 
 def test_rfloordiv() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 // a  # triggers __rfloordiv__
     result._sync_realize()
     assert result.real
 
 
 def test_mod() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a % b
     result._sync_realize()
     assert result.real
 
 
 def test_rmod() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0 % a  # triggers __rmod__
     result._sync_realize()
     assert result.real
 
 
 def test_divmod() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     quotient, remainder = divmod(a, b)
     quotient._sync_realize()
     remainder._sync_realize()
@@ -273,7 +446,11 @@ def test_divmod() -> None:
 
 
 def test_rdivmod() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     # Call __rdivmod__ explicitly, divmod(2.0, a) is typed improperly
     quotient, remainder = a.__rdivmod__(2.0)
     quotient._sync_realize()
@@ -283,8 +460,16 @@ def test_rdivmod() -> None:
 
 
 def test_matmul() -> None:
-    a = Tensor.ones([4, 3], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([3, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 3],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [3, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a @ b
     result._sync_realize()
     assert result.real
@@ -292,8 +477,16 @@ def test_matmul() -> None:
 
 
 def test_rmatmul() -> None:
-    a = Tensor.ones([4, 3], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([3, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 3],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [3, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     # a @ b would call __matmul__, so call __rmatmal__ explicitly
     result = b.__rmatmul__(a)
     result._sync_realize()
@@ -301,67 +494,129 @@ def test_rmatmul() -> None:
 
 
 def test_pow() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
-    b = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a**b
     result._sync_realize()
     assert result.real
 
 
 def test_rpow() -> None:
-    a = Tensor.ones([4, 6], dtype=DType.float32, device=DEVICE)
+    a = Tensor.ones(
+        [4, 6],
+        dtype=DType.float32,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = 2.0**a  # triggers __rpow__
     result._sync_realize()
     assert result.real
 
 
 def test_and() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
-    b = Tensor.full([4, 6], False, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.full(
+        [4, 6],
+        False,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a & b
     result._sync_realize()
     assert result.real
 
 
 def test_rand() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = True & a  # triggers __rand__
     result._sync_realize()
     assert result.real
 
 
 def test_or() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
-    b = Tensor.full([4, 6], False, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.full(
+        [4, 6],
+        False,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a | b
     result._sync_realize()
     assert result.real
 
 
 def test_ror() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = False | a  # triggers __ror__
     result._sync_realize()
     assert result.real
 
 
 def test_xor() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
-    b = Tensor.full([4, 6], False, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
+    b = Tensor.full(
+        [4, 6],
+        False,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = a ^ b
     result._sync_realize()
     assert result.real
 
 
 def test_rxor() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = False ^ a  # triggers __rxor__
     result._sync_realize()
     assert result.real
 
 
 def test_invert() -> None:
-    a = Tensor.full([4, 6], True, dtype=DType.bool, device=DEVICE)
+    a = Tensor.full(
+        [4, 6],
+        True,
+        dtype=DType.bool,
+        device=Accelerator() if accelerator_count() else CPU(),
+    )
     result = ~a
     result._sync_realize()
     assert result.real
