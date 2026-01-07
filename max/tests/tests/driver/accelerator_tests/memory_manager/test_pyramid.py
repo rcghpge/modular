8fe04f0fc827f125cf77bc3298b6e5c6b20ad456
Brian Zhang <bez@modular.com>
2026-01-07 14:36:07 -0800

    [Driver] Update Memory Manager test
    MODULAR_ORIG_COMMIT_REV_ID: 7e8b18d1d19a3835ac8c67f8661304ee5e3150af

--- max/tests/tests/driver/accelerator_tests/memory_manager/test_pyramid.py
+++ max/tests/tests/driver/accelerator_tests/memory_manager/test_pyramid.py
@@ -42,7 +42,7 @@ def test_pyramid(memory_manager_config: None, mem_type: MemType) -> None:
 
         # This explicit sync is necessary to ensure that the buffers are returned
         # to the memory manager for pinned memory.
-        # Interally, this calls checkPendingWork() which polls for ready events
+        # Interally, this calls pollPendingEvents() which polls for ready events
         # and runs the above handlers we enqueued earlier. These handlers free
         # the buffers.
 
