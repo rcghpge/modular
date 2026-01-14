63daf53f1b44748a85734fe31aa133c15c9b38d3
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2026-01-14 17:20:41 -0500

    [Mojo][PythonInterop] Support relative import paths
    MODULAR_ORIG_COMMIT_REV_ID: ff90183194dbe498c26c950c7f4f6e2cbc320327

--- max/python/max/_core_mojo/__init__.py
+++ max/python/max/_core_mojo/__init__.py
@@ -15,19 +15,13 @@ import os
 import sys
 from typing import Any
 
+# Install mojo import hook
+import mojo.importer
 import numpy as np
 import numpy.typing as npt
 
-# get directory of current file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-sys.path.insert(0, current_dir)
-
-# Install mojo import hook
-import mojo.importer
-
 # Imports from 'mojo_module.mojo'
-from mojo_module import mojo_block_hasher  # type: ignore
+from .mojo_module import mojo_block_hasher  # type: ignore
 
 
 def block_hasher(
