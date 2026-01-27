e4f2d5933a46642b9fef4781bbe12374eaa3eef5
Katherine Wu <31663267+k-w-w@users.noreply.github.com>
2026-01-27 13:13:51 -0800

    [Models] Update Qwen3 Embedding's `SupportedArchitecture.config`
    Since the embedding model does not use a KVCache, the config doesn't
    really hold anything special at the moment.
    
    MODULAR_ORIG_COMMIT_REV_ID: 6de3a59000722e2f4470a509c9ffdd9b946db233

--- max/python/max/pipelines/architectures/qwen3_embedding/arch.py
+++ max/python/max/pipelines/architectures/qwen3_embedding/arch.py
@@ -22,6 +22,7 @@ from max.pipelines.lib import (
 )
 
 from .model import Qwen3EmbeddingModel
+from .model_config import Qwen3EmbeddingConfig
 
 qwen3_embedding_arch = SupportedArchitecture(
     name="Qwen3ForCausalLM_Legacy",
@@ -44,4 +45,5 @@ qwen3_embedding_arch = SupportedArchitecture(
         WeightsFormat.safetensors: weight_adapters.convert_safetensor_state_dict,
         WeightsFormat.gguf: weight_adapters.convert_gguf_state_dict,
     },
+    config=Qwen3EmbeddingConfig,
 )
