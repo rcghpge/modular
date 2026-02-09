f02b579f54d1541f758a520d89fbf1d7450e6eca
Brendan Duke <bduke@modular.com>
2026-02-09 15:45:35 -0500

    Revert "[Overlap] Automatically enable overlap scheduling for LlamaForCausalLM_Legacy, Try 2 [Internal link]
    This reverts commit 957a2d23e384daebb5271452349b01d48e89c641.
    This causes the following error running Llama-3.2-1B:
    ```python
    ValueError: At max/kernels/src/linalg/matmul/gpu/__init__.mojo:1098:65: DeviceContext.enqueue_function: Dim value grid_dim.y must be a positive number.
    ```
    
    Repro:
    ```shell
    br //max -- serve --model-path meta-llama/Llama-3.2-1B --devices 0,1
    ```
    MODULAR_ORIG_COMMIT_REV_ID: cf2b679994ea0b7a5c64c9578b4dfc2f71c7a09d

--- max/tests/integration/pipelines/text_generation_pipeline/test_overlap_text_generation_pipeline.py
+++ max/tests/integration/pipelines/text_generation_pipeline/test_overlap_text_generation_pipeline.py
@@ -37,3 +37,25 @@ def test_throws_if_num_steps_gt_1() -> None:
         match=r"Max num steps > 1 is not supported with the Overlap scheduler\.",
     ):
         pipeline.execute(inputs)
+
+
+def test_throws_if_enable_log_probs() -> None:
+    pipeline = OverlapTextGenerationPipeline.__new__(
+        OverlapTextGenerationPipeline
+    )
+    request_id = RequestID()
+    ctx = TextContext(
+        request_id=request_id,
+        max_length=1000,
+        tokens=TokenBuffer(np.array([42, 67, 21])),
+        log_probabilities=1,
+    )
+    inputs: TextGenerationInputs[TextContext] = TextGenerationInputs(
+        batches=[[ctx]],
+        num_steps=1,
+    )
+    with pytest.raises(
+        ValueError,
+        match=r"Log probabilities are not supported with overlap pipeline",
+    ):
+        pipeline.execute(inputs)
