a9d2b746486bc3d6f9a3bc9fda62c189be832a34
Scott Main <scott@modular.com>
2026-02-17 17:58:33 -0800

    [docs] Move Mojo redirects to separate file
    MODULAR_ORIG_COMMIT_REV_ID: 5a02f40f00b323c6b6a89f3d93a39a30cffcdf01

---
+++ mojo/docs/redirects.js
@@ -0,0 +1,16 @@
+const mojoRedirects = [
+  {
+    from: '/mojo/manual/gpu/gpu-basics',
+    to: '/mojo/manual/gpu/intro-tutorial',
+  },
+  {
+    from: '/mojo/manual/gpu/basics',
+    to: '/mojo/manual/gpu/intro-tutorial',
+  },
+  {
+    from: '/mojo/why-mojo',
+    to: '/mojo/vision',
+  },
+];
+
+module.exports = { mojoRedirects };
