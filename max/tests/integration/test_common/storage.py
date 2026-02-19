0f2519f7f6439d421afe52ebfc099e0f7ace7463
Reza Hussain <rhussain@modular.com>
2026-02-19 12:26:35 -0600

    [Models] Adding anonymous s3 access for public s3 bucket
    This lets boto connect to the s3://modular-bazel-artifacts-public/
    bucket when being run inside our CI/CD system
    
    MODULAR_ORIG_COMMIT_REV_ID: 418ce93ba08e19601e529caf80c08ab5ad49178a

--- max/tests/integration/test_common/storage.py
+++ max/tests/integration/test_common/storage.py
@@ -36,7 +36,11 @@ import tarfile
 import tempfile
 
 import boto3
-from botocore.exceptions import NoCredentialsError, UnauthorizedSSOTokenError
+from botocore.exceptions import (
+    NoCredentialsError,
+    SSOTokenLoadError,
+    UnauthorizedSSOTokenError,
+)
 from PIL import Image
 
 _DEFAULT_CACHE_DIR = "~/.cache/modular/testdata"
@@ -66,6 +70,19 @@ def load_from_s3(s3_path: str, cache_dir: str | None = None) -> str:
                 "AWS authentication failed. Please run 'aws sso login' to "
                 "refresh your credentials."
             ) from e
+        except SSOTokenLoadError as e:
+            print(
+                "Error loading SSO Token, attempting with anonymous credentials"
+            )
+            s3 = boto3.client(
+                "s3", aws_access_key_id="", aws_secret_access_key=""
+            )
+            s3._request_signer.sign = lambda *args, **kwargs: None
+            s3.download_file(
+                _S3_BUCKET,
+                s3_path,
+                local_path,
+            )
     return local_path
 
 
