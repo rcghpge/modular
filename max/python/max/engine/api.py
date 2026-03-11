fff56cc3f997558318f014d2201061864ae4656d
Joshua Peterson <josh@modular.com>
2026-03-11 16:46:14 -0400

    [MAX] Rename PdlLevel to PDLLevel in the engine API
    This aligns with our Python naming conventions. PDL is an acronym, so we
    should captialize it.
    
    MODULAR_ORIG_COMMIT_REV_ID: ce678563d67afb46b63f93c283625a7e85bfc731

--- max/python/max/engine/api.py
+++ max/python/max/engine/api.py
@@ -304,7 +304,7 @@ class SplitKReductionPrecision(IntEnum):
     OUTPUT = auto()
 
 
-class PdlLevel(IntEnum):
+class PDLLevel(IntEnum):
     """Internal use."""
 
     # No PDL
@@ -748,12 +748,12 @@ class InferenceSession:
 
         self._set_mojo_define("MODULE_USE_VENDOR_BLAS", 1)
 
-    def _pdl_level(self, level: str | PdlLevel) -> None:
+    def _pdl_level(self, level: str | PDLLevel) -> None:
         """Level of overlap of kernel launch."""
-        if not isinstance(level, PdlLevel):
+        if not isinstance(level, PDLLevel):
             if level not in {"0", "1", "2"}:
                 raise TypeError(
-                    f"Invalid pdl level ({level}). Please use one of: {[0, 1, 2]} corresponding to {[x.name for x in PdlLevel]}"
+                    f"Invalid pdl level ({level}). Please use one of: {[0, 1, 2]} corresponding to {[x.name for x in PDLLevel]}"
                 )
 
         self._set_mojo_define("PDL_LEVEL", int(level))
