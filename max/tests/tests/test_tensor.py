12d13536a747900f99d7bb6306e1cdd2851438fc
Reza Hussain <rhussain@modular.com>
2026-04-21 12:41:47 -0500

    [CI] QUA-260 disable flaky test (#84000)
    MODULAR_ORIG_COMMIT_REV_ID: 6a441ea6e52e61ffe8f7cb1dcc161cc4a8bcecb3

--- max/tests/tests/test_tensor.py
+++ max/tests/tests/test_tensor.py
@@ -297,6 +297,7 @@ def test_mutation_op_order() -> None:
     assert d.item() == 2.0
 
 
+@pytest.mark.skip(reason="flaky - QUA-260, MXF-275")
 def test_mutation_op_order_lazy() -> None:
     with F.lazy():
         a = Tensor.zeros([1])
